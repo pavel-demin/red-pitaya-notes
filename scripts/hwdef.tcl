5aa75ff55320a7cfdd8c07aa0b13716fda85a353
Pavel Demin <pavel.demin@cern.ch>
2014-12-25 21:47:41 +0100

    add catch to hwdef.tcl

--- scripts/hwdef.tcl
+++ scripts/hwdef.tcl
@@ -1,15 +1,22 @@
-set project_name [lindex $argv 0]
+if {[catch {
 
-open_project $project_name.xpr
+  set project_name [lindex $argv 0]
 
-if {[get_property PROGRESS [get_runs synth_1]] != "100%"} {
-  launch_runs synth_1
-  wait_on_run synth_1
+  open_project $project_name.xpr
+
+  if {[get_property PROGRESS [get_runs synth_1]] != "100%"} {
+    launch_runs synth_1
+    wait_on_run synth_1
+  }
+
+  write_hwdef -force -file $project_name.hwdef
+
+  close_project
+
+} result]} {
+  puts "** ERROR: $result"
+  exit 1
 }
-
-write_hwdef -force -file $project_name.hwdef
-
-close_project
 
 exit
 
