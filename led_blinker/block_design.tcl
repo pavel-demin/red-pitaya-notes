5cec30ce684622ea60e9b6143e465016aeda3209
Pavel Demin <pavel.demin@cern.ch>
2014-12-26 20:45:05 +0100

    use shorter names for block design cells

--- led_blinker/block_design.tcl
+++ led_blinker/block_design.tcl
@@ -22,17 +22,17 @@ create_bd_cell -type ip -vlnv xilinx.com:ip:c_counter_binary:12.0 cntr_0
 set_property -dict [list CONFIG.Output_Width {32}] [get_bd_cells cntr_0]
 
 # Connect axis_red_pitaya_adc to c_counter_binary
-connect_bd_net [get_bd_pins adc_0/adc_clk] [get_bd_pins c_counter_binary_0/CLK]
+connect_bd_net [get_bd_pins adc_0/adc_clk] [get_bd_pins cntr_0/CLK]
 
 # Create xlslice
 create_bd_cell -type ip -vlnv xilinx.com:ip:xlslice:1.0 slice_0
 set_property -dict [list CONFIG.DIN_FROM {26} CONFIG.DIN_TO {26}] [get_bd_cells slice_0]
 
 # Connect c_counter_binary to xlslice
-connect_bd_net [get_bd_pins c_counter_binary_0/Q] [get_bd_pins xlslice_0/Din]
+connect_bd_net [get_bd_pins cntr_0/Q] [get_bd_pins slice_0/Din]
 
 # Create interface port for xlslice
 create_bd_port -dir O -from 0 -to 0 led
 
 # Connect xlslice to interface port
-connect_bd_net [get_bd_ports led] [get_bd_pins xlslice_0/Dout]
+connect_bd_net [get_bd_ports led] [get_bd_pins slice_0/Dout]
