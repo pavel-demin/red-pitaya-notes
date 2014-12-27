3408df147ebcb32bd8e35930bdb871a6bf454eaf
Pavel Demin <pavel.demin@cern.ch>
2014-12-27 15:42:24 +0100

    fix dac_dat_o direction

--- cfg/ports.tcl
+++ cfg/ports.tcl
@@ -14,12 +14,12 @@ create_bd_port -dir O adc_csn_o
 
 ### DAC
 
-create_bd_port -dir I -from 13 -to 0 dac_dat_o
+create_bd_port -dir O -from 13 -to 0 dac_dat_o
 
-create_bd_port -dir O dac_wrt_o
-create_bd_port -dir O dac_sel_o
 create_bd_port -dir O dac_clk_o
 create_bd_port -dir O dac_rst_o
+create_bd_port -dir O dac_sel_o
+create_bd_port -dir O dac_wrt_o
 
 ### PWM
 
