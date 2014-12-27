b612075d214753b3a60f14c706309724551eced7
Pavel Demin <pavel.demin@cern.ch>
2014-12-27 15:14:36 +0100

    add hierarchical modules

--- adc_dump/block_design.tcl
+++ adc_dump/block_design.tcl
@@ -8,26 +8,26 @@ set_property -dict [list CONFIG.PCW_USE_FABRIC_INTERRUPT {1}] [get_bd_cells ps_0
 set_property -dict [list CONFIG.PCW_IRQ_F2P_INTR {1}] [get_bd_cells ps_0]
 
 # Create axis_clock_converter
-create_bd_cell -type ip -vlnv xilinx.com:ip:axis_clock_converter:1.1 fifo_0
+create_bd_cell -vlnv xilinx.com:ip:axis_clock_converter:1.1 fifo_0
 
 # Connect axis_red_pitaya_adc to axis_clock_converter
 connect_bd_net [get_bd_pins adc_0/adc_clk] [get_bd_pins fifo_0/s_axis_aclk]
 connect_bd_intf_net [get_bd_intf_pins adc_0/M_AXIS] [get_bd_intf_pins fifo_0/S_AXIS]
 
 # Create axis_packetizer
-create_bd_cell -type ip -vlnv pavel-demin:user:axis_packetizer:1.0 pktzr_0
+create_bd_cell -vlnv pavel-demin:user:axis_packetizer:1.0 pktzr_0
 
 # Connect axis_clock_converter to axis_packetizer
 connect_bd_intf_net [get_bd_intf_pins fifo_0/M_AXIS] [get_bd_intf_pins pktzr_0/S_AXIS]
 
 # Create axi_dma
-create_bd_cell -type ip -vlnv xilinx.com:ip:axi_dma:7.1 dma_0
+create_bd_cell -vlnv xilinx.com:ip:axi_dma:7.1 dma_0
 
 # Connect axis_packetizer to axi_dma
 connect_bd_intf_net [get_bd_intf_pins pktzr_0/M_AXIS] [get_bd_intf_pins dma_0/S_AXIS_S2MM]
 
 # Create axi_interconnect for GP0
-create_bd_cell -type ip -vlnv xilinx.com:ip:axi_interconnect:2.1 gp_0
+create_bd_cell -vlnv xilinx.com:ip:axi_interconnect:2.1 gp_0
 set_property -dict [list CONFIG.NUM_MI {1}] [get_bd_cells gp_0]
 
 # Connect processing_system7 to axi_interconnect
@@ -37,7 +37,7 @@ connect_bd_intf_net [get_bd_intf_pins ps_0/M_AXI_GP0] [get_bd_intf_pins gp_0/S00
 connect_bd_intf_net [get_bd_intf_pins gp_0/M00_AXI] [get_bd_intf_pins dma_0/S_AXI_LITE]
 
 # Create axi_interconnect for HP0
-create_bd_cell -type ip -vlnv xilinx.com:ip:axi_interconnect:2.1 hp_0
+create_bd_cell -vlnv xilinx.com:ip:axi_interconnect:2.1 hp_0
 set_property -dict [list CONFIG.NUM_MI {1} CONFIG.NUM_SI {3}]  [get_bd_cells hp_0]
 
 # Connect axi_dma to axi_interconnect
@@ -49,7 +49,7 @@ connect_bd_intf_net [get_bd_intf_pins dma_0/M_AXI_SG] [get_bd_intf_pins hp_0/S00
 connect_bd_intf_net [get_bd_intf_pins hp_0/M00_AXI] [get_bd_intf_pins ps_0/S_AXI_HP0]
 
 # Create xlconcat
-create_bd_cell -type ip -vlnv xilinx.com:ip:xlconcat:2.1 concat_0
+create_bd_cell -vlnv xilinx.com:ip:xlconcat:2.1 concat_0
 
 # Connect axi_dma to xlconcat
 connect_bd_net [get_bd_pins dma_0/mm2s_introut] [get_bd_pins concat_0/In0]
@@ -59,7 +59,7 @@ connect_bd_net [get_bd_pins dma_0/s2mm_introut] [get_bd_pins concat_0/In1]
 connect_bd_net [get_bd_pins concat_0/dout] [get_bd_pins ps_0/IRQ_F2P]
 
 # Create proc_sys_reset
-create_bd_cell -type ip -vlnv xilinx.com:ip:proc_sys_reset:5.0 rst_0
+create_bd_cell -vlnv xilinx.com:ip:proc_sys_reset:5.0 rst_0
 
 # Connect processing_system7 to proc_sys_reset
 connect_bd_net [get_bd_pins ps_0/FCLK_RESET0_N] [get_bd_pins rst_0/ext_reset_in]
@@ -70,6 +70,12 @@ connect_bd_net [get_bd_pins rst_0/peripheral_aresetn] [get_bd_pins dma_0/axi_res
 
 # Connect FCLK_CLK0 to all other cells
 connect_bd_net [get_bd_pins ps_0/FCLK_CLK0] [get_bd_pins dma_0/m_axi_mm2s_aclk] [get_bd_pins dma_0/m_axi_s2mm_aclk] [get_bd_pins dma_0/m_axi_sg_aclk] [get_bd_pins dma_0/s_axi_lite_aclk] [get_bd_pins fifo_0/m_axis_aclk] [get_bd_pins gp_0/ACLK] [get_bd_pins gp_0/M00_ACLK] [get_bd_pins gp_0/S00_ACLK] [get_bd_pins hp_0/ACLK] [get_bd_pins hp_0/M00_ACLK] [get_bd_pins hp_0/S00_ACLK] [get_bd_pins hp_0/S01_ACLK] [get_bd_pins hp_0/S02_ACLK] [get_bd_pins pktzr_0/aclk] [get_bd_pins ps_0/S_AXI_HP0_ACLK] [get_bd_pins rst_0/slowest_sync_clk]
+
+# Create hierarchical module
+create_bd_cell -type hier hpdma_0
+
+# Move dma_0, gp_0, hp_0, concat_0 and rst_0 to hpdma_0
+move_bd_cells hpdma_0 [get_bd_cells dma_0] [get_bd_cells gp_0] [get_bd_cells hp_0] [get_bd_cells concat_0] [get_bd_cells rst_0]
 
 # Configure addresses
 create_bd_addr_seg -range 0x20000000 -offset 0x0 [get_bd_addr_spaces dma_0/Data_SG] [get_bd_addr_segs ps_0/S_AXI_HP0/HP0_DDR_LOWOCM] SEG_ps_0_HP0_DDR_LOWOCM
--- led_blinker/block_design.tcl
+++ led_blinker/block_design.tcl
@@ -1,7 +1,7 @@
 source base_system/block_design.tcl
 
 # Create axis_red_pitaya_adc
-create_bd_cell -type ip -vlnv pavel-demin:user:axis_red_pitaya_adc:1.0 adc_0
+create_bd_cell -vlnv pavel-demin:user:axis_red_pitaya_adc:1.0 adc_0
 
 # Connect axis_red_pitaya_adc to interface ports
 connect_bd_net [get_bd_pins adc_0/adc_clk_p] [get_bd_ports adc_clk_p_i]
@@ -10,25 +10,25 @@ connect_bd_net [get_bd_pins adc_0/adc_dat_a] [get_bd_ports adc_dat_a_i]
 connect_bd_net [get_bd_pins adc_0/adc_dat_b] [get_bd_ports adc_dat_b_i]
 connect_bd_net [get_bd_pins adc_0/adc_csn] [get_bd_ports adc_csn_o]
 
-# Create hierarchical module
-create_bd_cell -type hier blinker_0
-
 # Create c_counter_binary
-create_bd_cell -type ip -vlnv xilinx.com:ip:c_counter_binary:12.0 cntr_0
+create_bd_cell -vlnv xilinx.com:ip:c_counter_binary:12.0 cntr_0
 set_property -dict [list CONFIG.Output_Width {32}] [get_bd_cells cntr_0]
 
 # Connect axis_red_pitaya_adc to c_counter_binary
 connect_bd_net [get_bd_pins adc_0/adc_clk] [get_bd_pins cntr_0/CLK]
 
 # Create xlslice
-create_bd_cell -type ip -vlnv xilinx.com:ip:xlslice:1.0 slice_0
+create_bd_cell -vlnv xilinx.com:ip:xlslice:1.0 slice_0
 set_property -dict [list CONFIG.DIN_FROM {26} CONFIG.DIN_TO {26}] [get_bd_cells slice_0]
 
 # Connect c_counter_binary to xlslice
 connect_bd_net [get_bd_pins cntr_0/Q] [get_bd_pins slice_0/Din]
 
 # Connect xlslice to interface port
-connect_bd_net [get_bd_ports {led_o[0]}] [get_bd_pins slice_0/Dout]
+connect_bd_net [get_bd_ports led_o] [get_bd_pins slice_0/Dout]
 
-# End of hierarchical module
-current_bd_instance
+# Create hierarchical module
+create_bd_cell -type hier blinker_0
+
+# Move cntr_0 and slice_0 to blinker_0
+move_bd_cells blinker_0 [get_bd_cells cntr_0] [get_bd_cells slice_0]
