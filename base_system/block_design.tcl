d857b5a07ae02de66837e819c2a44168de4be260
Pavel Demin <pavel.demin@cern.ch>
2014-12-28 17:46:12 +0100

    rename adc_dump to adc_reader and use cell command

--- base_system/block_design.tcl
+++ base_system/block_design.tcl
@@ -1,12 +1,18 @@
 
 # Create processing_system7
-create_bd_cell -type ip -vlnv xilinx.com:ip:processing_system7:5.5 ps_0
-
-# Import Red Pitaya configuration
-set_property CONFIG.PCW_IMPORT_BOARD_PRESET cfg/red_pitaya.xml [get_bd_cells ps_0]
+cell xilinx.com:ip:processing_system7:5.5 ps_0 {
+  PCW_IMPORT_BOARD_PRESET cfg/red_pitaya.xml
+  PCW_USE_S_AXI_HP0 1
+  PCW_USE_FABRIC_INTERRUPT 1
+  PCW_IRQ_F2P_INTR 1
+} {
+  M_AXI_GP0_ACLK ps_0/FCLK_CLK0
+  S_AXI_HP0_ACLK ps_0/FCLK_CLK0
+}
 
 # Create all required interconnections
-apply_bd_automation -rule xilinx.com:bd_rule:processing_system7 -config {make_external "FIXED_IO, DDR" Master Disable Slave Disable} [get_bd_cells ps_0]
-
-# Connect FCLK_CLK0 to M_AXI_GP0_ACLK
-connect_bd_net [get_bd_pins ps_0/FCLK_CLK0] [get_bd_pins ps_0/M_AXI_GP0_ACLK]
+apply_bd_automation -rule xilinx.com:bd_rule:processing_system7 -config {
+  make_external {FIXED_IO, DDR}
+  Master Disable
+  Slave Disable
+} [get_bd_cells ps_0]
--- led_blinker/block_design.tcl
+++ led_blinker/block_design.tcl
@@ -1,35 +1,40 @@
 source base_system/block_design.tcl
 
 # Create axis_red_pitaya_adc
-create_bd_cell -vlnv pavel-demin:user:axis_red_pitaya_adc:1.0 adc_0
-
-# Connect axis_red_pitaya_adc to interface ports
-connect_bd_net [get_bd_pins adc_0/adc_clk_p] [get_bd_ports adc_clk_p_i]
-connect_bd_net [get_bd_pins adc_0/adc_clk_n] [get_bd_ports adc_clk_n_i]
-connect_bd_net [get_bd_pins adc_0/adc_dat_a] [get_bd_ports adc_dat_a_i]
-connect_bd_net [get_bd_pins adc_0/adc_dat_b] [get_bd_ports adc_dat_b_i]
-connect_bd_net [get_bd_pins adc_0/adc_csn] [get_bd_ports adc_csn_o]
+cell pavel-demin:user:axis_red_pitaya_adc:1.0 adc_0 {} {
+  adc_clk_p adc_clk_p_i
+  adc_clk_n adc_clk_n_i
+  adc_dat_a adc_dat_a_i
+  adc_dat_b adc_dat_b_i
+  adc_csn   adc_csn_o
+}
 
 # Create c_counter_binary
-create_bd_cell -vlnv xilinx.com:ip:c_counter_binary:12.0 cntr_0
-set_property CONFIG.Output_Width 32 [get_bd_cells cntr_0]
-
-# Connect axis_red_pitaya_adc to c_counter_binary
-connect_bd_net [get_bd_pins adc_0/adc_clk] [get_bd_pins cntr_0/CLK]
+cell xilinx.com:ip:c_counter_binary:12.0 cntr_0 {
+  Output_Width 32
+} {
+  CLK adc_0/adc_clk
+}
 
 # Create xlslice
-create_bd_cell -vlnv xilinx.com:ip:xlslice:1.0 slice_0
-set_property CONFIG.DIN_FROM 26 [get_bd_cells slice_0]
-set_property CONFIG.DIN_TO 26 [get_bd_cells slice_0]
+cell xilinx.com:ip:xlslice:1.0 slice_0 {
+  DIN_FROM 26
+  DIN_TO 26
+} {
+  Din cntr_0/Q
+}
 
-# Connect c_counter_binary to xlslice
-connect_bd_net [get_bd_pins cntr_0/Q] [get_bd_pins slice_0/Din]
+# Create xlconstant
+cell xilinx.com:ip:xlconstant:1.1 const_0 {
+  CONST_WIDTH 7
+  CONST_VAL 0
+}
 
-# Connect xlslice to interface port
-connect_bd_net [get_bd_ports led_o] [get_bd_pins slice_0/Dout]
-
-# Create hierarchical module
-create_bd_cell -type hier blinker_0
-
-# Move cntr_0 and slice_0 to blinker_0
-move_bd_cells [get_bd_cells blinker_0] [get_bd_cells cntr_0] [get_bd_cells slice_0]
+# Create xlconcat
+cell xilinx.com:ip:xlconcat:2.1 concat_0 {
+  IN1_WIDTH 7
+} {
+  In0 slice_0/Dout
+  In1 const_0/dout
+  dout led_o
+}
