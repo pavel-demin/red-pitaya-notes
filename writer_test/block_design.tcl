1585fa597190aaeffe0fe32561e63128f062ea1c
Pavel Demin <pavel.demin@cern.ch>
2015-01-09 00:35:26 +0100

    add address for axis_ram_writer

--- writer_test/block_design.tcl
+++ writer_test/block_design.tcl
@@ -6,3 +6,5 @@ cell pavel-demin:user:axis_ram_writer:1.0 writer_0 {} {
   aclk ps_0/FCLK_CLK0
   aresetn rst_0/peripheral_aresetn 
 }
+
+create_bd_addr_seg -range 8M -offset 0x1E000000 [get_bd_addr_spaces writer_0/M_AXI] [get_bd_addr_segs ps_0/S_AXI_HP0/HP0_DDR_LOWOCM] SEG_ps_0_HP0_DDR_LOWOCM
