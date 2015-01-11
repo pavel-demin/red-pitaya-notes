d715c8b2d1f534a7383f93f11c667bff03dc28a7
Pavel Demin <pavel.demin@cern.ch>
2015-01-11 18:46:22 +0100

    add axis_counter

---
+++ cores/axis_counter_v1_0/axis_counter.v
@@ -0,0 +1,41 @@
+
+`timescale 1 ns / 1 ps
+
+module axis_counter #
+(
+  parameter integer CNTR_WIDTH = 20,
+  parameter integer AXIS_TDATA_WIDTH = 32
+)
+(
+  // System signals
+  input  wire                        aclk,
+  input  wire                        aresetn,
+
+  // Master side
+  output wire                        m_axis_tvalid,
+  output wire [AXIS_TDATA_WIDTH-1:0] m_axis_tdata
+);
+
+  reg [CNTR_WIDTH-1:0] int_cntr_reg, int_cntr_next;
+
+  always @(posedge aclk)
+  begin
+    if(~aresetn)
+    begin
+      int_cntr_reg <= {(CNTR_WIDTH){1'b0}};
+    end
+    else
+    begin
+      int_cntr_reg <= int_cntr_next;
+    end
+  end
+
+  always @*
+  begin
+    int_cntr_next = int_cntr_reg + 1'b1;
+  end
+
+  assign m_axis_tvalid = 1'b1;
+  assign m_axis_tdata = {{(AXIS_TDATA_WIDTH-CNTR_WIDTH){1'b0}}, int_cntr_reg};
+
+endmodule
