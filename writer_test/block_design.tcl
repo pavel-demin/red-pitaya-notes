69d05439d01f2bbd193c266d3d97b116699bb9e3
Pavel Demin <pavel.demin@cern.ch>
2015-01-12 15:48:45 +0100

    add configurable axis_counter and reset ports

--- writer_test/block_design.tcl
+++ writer_test/block_design.tcl
@@ -1,10 +1,39 @@
 source cfg_test/block_design.tcl
 
+# Create xlslice
+cell xilinx.com:ip:xlslice:1.0 slice_2 {
+  DIN_WIDTH 1024 DIN_FROM 0 DIN_TO 0 DOUT_WIDTH 1
+} {
+  Din cfg_0/cfg_data
+}
+
+# Create xlslice
+cell xilinx.com:ip:xlslice:1.0 slice_3 {
+  DIN_WIDTH 1024 DIN_FROM 1 DIN_TO 1 DOUT_WIDTH 1
+} {
+  Din cfg_0/cfg_data
+}
+
+# Create xlslice
+cell xilinx.com:ip:xlslice:1.0 slice_4 {
+  DIN_WIDTH 1024 DIN_FROM 51 DIN_TO 32 DOUT_WIDTH 20
+} {
+  Din cfg_0/cfg_data
+}
+
+# Create axis_counter
+cell pavel-demin:user:axis_counter:1.0 cntr_1 {} {
+  cfg_data slice_4/Dout
+  aclk ps_0/FCLK_CLK0
+  aresetn slice_2/Dout
+}
+
 # Create axis_ram_writer
 cell pavel-demin:user:axis_ram_writer:1.0 writer_0 {} {
+  S_AXIS cntr_1/M_AXIS
   M_AXI ps_0/S_AXI_HP0
   aclk ps_0/FCLK_CLK0
-  aresetn rst_0/peripheral_aresetn 
+  aresetn slice_3/Dout
 }
 
 create_bd_addr_seg -range 8M -offset 0x1E000000 [get_bd_addr_spaces writer_0/M_AXI] [get_bd_addr_segs ps_0/S_AXI_HP0/HP0_DDR_LOWOCM] SEG_ps_0_HP0_DDR_LOWOCM
