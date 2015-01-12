7a3e5dc37efe32e053231d60dd6a44dd4ecf5f0c
Pavel Demin <pavel.demin@cern.ch>
2015-01-12 15:46:39 +0100

    disable interrupts

--- base_system/block_design.tcl
+++ base_system/block_design.tcl
@@ -3,8 +3,6 @@
 cell xilinx.com:ip:processing_system7:5.5 ps_0 {
   PCW_IMPORT_BOARD_PRESET cfg/red_pitaya.xml
   PCW_USE_S_AXI_HP0 1
-  PCW_USE_FABRIC_INTERRUPT 1
-  PCW_IRQ_F2P_INTR 1
 } {
   M_AXI_GP0_ACLK ps_0/FCLK_CLK0
   S_AXI_HP0_ACLK ps_0/FCLK_CLK0
