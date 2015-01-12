7ccf2f232c8ce3f1e5c5906e9af0c901913bbc8f
Pavel Demin <pavel.demin@cern.ch>
2015-01-12 21:05:07 +0100

    remove duplicate of proc_sys_reset and set fifo_0/s_axis_aresetn to 1

--- packetizer_test/block_design.tcl
+++ packetizer_test/block_design.tcl
@@ -21,13 +21,14 @@ cell xilinx.com:ip:xlslice:1.0 slice_4 {
   Din cfg_0/cfg_data
 }
 
-# Create proc_sys_reset
-cell xilinx.com:ip:proc_sys_reset:5.0 rst_0
+# Create xlconstant
+cell xilinx.com:ip:xlconstant:1.1 const_1
 
 # Create axis_clock_converter
 cell xilinx.com:ip:axis_clock_converter:1.1 fifo_0 {} {
   S_AXIS adc_0/M_AXIS
   s_axis_aclk adc_0/adc_clk
+  s_axis_aresetn const_1/dout
   m_axis_aclk ps_0/FCLK_CLK0
   m_axis_aresetn rst_0/peripheral_aresetn
 }
