5b8f7cce0a44dff77dab19093eb4fade184bf96d
Pavel Demin <pavel.demin@cern.ch>
2015-01-27 20:50:54 +0100

    add tready and tlast to axis_packetizer

--- cores/axis_packetizer_v1_0/axis_packetizer.v
+++ cores/axis_packetizer_v1_0/axis_packetizer.v
@@ -19,63 +19,59 @@ module axis_packetizer #
   input  wire                        s_axis_tvalid,
 
   // Master side
+  output wire                        s_axis_tready,
   output wire [AXIS_TDATA_WIDTH-1:0] m_axis_tdata,
-  output wire                        m_axis_tvalid
+  output wire                        m_axis_tvalid,
+  output wire                        m_axis_tlast
 );
 
   reg [CNTR_WIDTH-1:0] int_cntr_reg, int_cntr_next;
-  reg [AXIS_TDATA_WIDTH-1:0] int_tdata_reg, int_tdata_next;
-  reg int_tready_reg, int_tready_next;
-  reg int_tvalid_reg, int_tvalid_next;
+  reg int_enbl_reg, int_enbl_next;
 
-  wire int_comp_wire, int_tvalid_wire;
+  wire int_comp_wire, int_tvalid_wire, int_tlast_wire;
 
   always @(posedge aclk)
   begin
     if(~aresetn)
     begin
       int_cntr_reg <= {(CNTR_WIDTH){1'b0}};
-      int_tdata_reg <= {(AXIS_TDATA_WIDTH){1'b0}};
-      int_tready_reg <= 1'b0;
-      int_tvalid_reg <= 1'b0;
+      int_enbl_reg <= 1'b0;
     end
     else
     begin
       int_cntr_reg <= int_cntr_next;
-      int_tdata_reg <= int_tdata_next;
-      int_tready_reg <= int_tready_next;
-      int_tvalid_reg <= int_tvalid_next;
+      int_enbl_reg <= int_enbl_next;
     end
   end
 
   assign int_comp_wire = int_cntr_reg < cfg_data;
-  assign int_tvalid_wire = s_axis_tvalid & int_tready_reg;
+  assign int_tvalid_wire = int_enbl_reg & s_axis_tvalid;
+  assign int_tlast_wire = int_cntr_reg == cfg_data;
 
   always @*
   begin
     int_cntr_next = int_cntr_reg;
-    int_tdata_next = s_axis_tdata;
-    int_tready_next = int_tready_reg;
-    int_tvalid_next = int_tvalid_wire;
+    int_enbl_next = int_enbl_reg;
 
-    if(~int_tready_reg & int_comp_wire)
+    if(~int_enbl_reg & int_comp_wire)
     begin
-      int_tready_next = 1'b1;
+      int_enbl_next = 1'b1;
     end
 
-    if(int_tvalid_wire & int_comp_wire)
+    if(m_axis_tready & int_tvalid_wire & int_comp_wire)
     begin
       int_cntr_next = int_cntr_reg + 1'b1;
     end
 
-    if(int_tvalid_wire & (int_cntr_reg == cfg_data))
+    if(m_axis_tready & int_tvalid_wire & int_tlast_wire)
     begin
-      int_tready_next = 1'b0;
+      int_enbl_next = 1'b0;
     end
   end
 
-  assign s_axis_tready = int_tready_reg;
-  assign m_axis_tdata = int_tdata_reg;
-  assign m_axis_tvalid = int_tvalid_reg;
+  assign s_axis_tready = int_enbl_reg & m_axis_tready;
+  assign m_axis_tdata = s_axis_tdata;
+  assign m_axis_tvalid = int_tvalid_wire;
+  assign m_axis_tlast = int_enbl_reg & int_tlast_wire;
 
 endmodule
