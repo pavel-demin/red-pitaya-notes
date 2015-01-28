46d4d592dec32245c8456b368020364e88a655a7
Pavel Demin <pavel.demin@cern.ch>
2015-01-28 01:12:51 +0100

    add switch_test

---
+++ projects/switch_test/block_design.tcl
@@ -0,0 +1,188 @@
+source projects/cfg_test/block_design.tcl
+
+# Create xlslice
+cell xilinx.com:ip:xlslice:1.0 slice_2 {
+  DIN_WIDTH 1024 DIN_FROM 0 DIN_TO 0 DOUT_WIDTH 1
+} {
+  Din cfg_0/cfg_data
+}
+
+# Create xlslice
+cell xilinx.com:ip:xlslice:1.0 slice_3 {
+  DIN_WIDTH 1024 DIN_FROM 1 DIN_TO 1 DOUT_WIDTH 1
+} {
+  Din cfg_0/cfg_data
+}
+
+# Create xlslice
+cell xilinx.com:ip:xlslice:1.0 slice_4 {
+  DIN_WIDTH 1024 DIN_FROM 2 DIN_TO 2 DOUT_WIDTH 1
+} {
+  Din cfg_0/cfg_data
+}
+
+# Create xlslice
+cell xilinx.com:ip:xlslice:1.0 slice_5 {
+  DIN_WIDTH 1024 DIN_FROM 3 DIN_TO 3 DOUT_WIDTH 1
+} {
+  Din cfg_0/cfg_data
+}
+
+# Create xlslice
+cell xilinx.com:ip:xlslice:1.0 slice_6 {
+  DIN_WIDTH 1024 DIN_FROM 4 DIN_TO 4 DOUT_WIDTH 1
+} {
+  Din cfg_0/cfg_data
+}
+
+# Create xlslice
+cell xilinx.com:ip:xlslice:1.0 slice_7 {
+  DIN_WIDTH 1024 DIN_FROM 5 DIN_TO 5 DOUT_WIDTH 1
+} {
+  Din cfg_0/cfg_data
+}
+
+# Create xlslice
+cell xilinx.com:ip:xlslice:1.0 slice_8 {
+  DIN_WIDTH 1024 DIN_FROM 6 DIN_TO 6 DOUT_WIDTH 1
+} {
+  Din cfg_0/cfg_data
+}
+
+# Create xlslice
+cell xilinx.com:ip:xlslice:1.0 slice_9 {
+  DIN_WIDTH 1024 DIN_FROM 63 DIN_TO 32 DOUT_WIDTH 32
+} {
+  Din cfg_0/cfg_data
+}
+
+# Create xlslice
+cell xilinx.com:ip:xlslice:1.0 slice_10 {
+  DIN_WIDTH 1024 DIN_FROM 95 DIN_TO 64 DOUT_WIDTH 32
+} {
+  Din cfg_0/cfg_data
+}
+
+# Create xlslice
+cell xilinx.com:ip:xlslice:1.0 slice_11 {
+  DIN_WIDTH 1024 DIN_FROM 127 DIN_TO 96 DOUT_WIDTH 32
+} {
+  Din cfg_0/cfg_data
+}
+
+# Create xlslice
+cell xilinx.com:ip:xlslice:1.0 slice_12 {
+  DIN_WIDTH 1024 DIN_FROM 159 DIN_TO 128 DOUT_WIDTH 32
+} {
+  Din cfg_0/cfg_data
+}
+
+# Create axis_counter
+cell pavel-demin:user:axis_counter:1.0 cntr_1 {} {
+  cfg_data slice_9/Dout
+  aclk ps_0/FCLK_CLK0
+  aresetn slice_2/Dout
+}
+
+# Create blk_mem_gen
+cell xilinx.com:ip:blk_mem_gen:8.2 bram_0 {
+  MEMORY_TYPE True_Dual_Port_RAM
+  USE_BRAM_BLOCK Stand_Alone
+  WRITE_WIDTH_A 32
+  WRITE_DEPTH_A 16384
+  ENABLE_A Always_Enabled
+  ENABLE_B Always_Enabled
+  REGISTER_PORTB_OUTPUT_OF_MEMORY_PRIMITIVES false
+}
+
+# Create axis_histogram
+cell pavel-demin:user:axis_histogram:1.0 hist_0 {
+  BRAM_ADDR_WIDTH 14
+  S_AXIS_TDATA_WIDTH 16
+  M_AXIS_TDATA_WIDTH 32
+} {
+  S_AXIS cntr_1/M_AXIS
+  BRAM_PORTA bram_0/BRAM_PORTA
+  BRAM_PORTB bram_0/BRAM_PORTB
+  aclk ps_0/FCLK_CLK0
+  aresetn slice_3/Dout
+}
+
+# Create axis_packetizer
+cell pavel-demin:user:axis_packetizer:1.0 pktzr_0 {} {
+  S_AXIS hist_0/M_AXIS
+  cfg_data slice_10/Dout
+  aclk ps_0/FCLK_CLK0
+  aresetn slice_4/Dout
+}
+
+# Create axis_counter
+cell pavel-demin:user:axis_counter:1.0 cntr_2 {} {
+  cfg_data slice_11/Dout
+  aclk ps_0/FCLK_CLK0
+  aresetn slice_5/Dout
+}
+
+# Create blk_mem_gen
+cell xilinx.com:ip:blk_mem_gen:8.2 bram_1 {
+  MEMORY_TYPE True_Dual_Port_RAM
+  USE_BRAM_BLOCK Stand_Alone
+  WRITE_WIDTH_A 32
+  WRITE_DEPTH_A 16384
+  ENABLE_A Always_Enabled
+  ENABLE_B Always_Enabled
+  REGISTER_PORTB_OUTPUT_OF_MEMORY_PRIMITIVES false
+}
+
+# Create axis_histogram
+cell pavel-demin:user:axis_histogram:1.0 hist_1 {
+  BRAM_ADDR_WIDTH 14
+  S_AXIS_TDATA_WIDTH 16
+  M_AXIS_TDATA_WIDTH 32
+} {
+  S_AXIS cntr_2/M_AXIS
+  BRAM_PORTA bram_1/BRAM_PORTA
+  BRAM_PORTB bram_1/BRAM_PORTB
+  aclk ps_0/FCLK_CLK0
+  aresetn slice_6/Dout
+}
+
+# Create axis_packetizer
+cell pavel-demin:user:axis_packetizer:1.0 pktzr_1 {} {
+  S_AXIS hist_1/M_AXIS
+  cfg_data slice_12/Dout
+  aclk ps_0/FCLK_CLK0
+  aresetn slice_7/Dout
+}
+
+# Create axis_switch
+cell xilinx.com:ip:axis_switch:1.1 switch_0 {
+  NUM_SI 2
+  TDATA_NUM_BYTES 4
+  HAS_TLAST 1
+  ARB_ON_MAX_XFERS 0
+  ARB_ON_TLAST 1
+} {
+  S00_AXIS pktzr_0/M_AXIS
+  S01_AXIS pktzr_1/M_AXIS
+  aclk ps_0/FCLK_CLK0
+  aresetn rst_0/peripheral_aresetn
+}
+
+# Create xlconstant
+cell xilinx.com:ip:xlconstant:1.1 const_2 {
+  CONST_WIDTH 32
+  CONST_VAL 503316480
+}
+
+# Create axis_ram_writer
+cell pavel-demin:user:axis_ram_writer:1.0 writer_0 {} {
+  S_AXIS switch_0/M00_AXIS
+  M_AXI ps_0/S_AXI_HP0
+  cfg_data const_2/dout
+  aclk ps_0/FCLK_CLK0
+  aresetn slice_8/Dout
+}
+
+assign_bd_address [get_bd_addr_segs ps_0/S_AXI_HP0/HP0_DDR_LOWOCM]
+
