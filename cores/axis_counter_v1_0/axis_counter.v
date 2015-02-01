b7cee4e965ae7fa8a980c2b4b73f1f81e3254d74
Pavel Demin <pavel.demin@cern.ch>
2015-02-01 01:16:51 +0100

    use the same register names as in axis_packetizer

--- cores/axis_counter_v1_0/axis_counter.v
+++ cores/axis_counter_v1_0/axis_counter.v
@@ -3,8 +3,8 @@
 
 module axis_counter #
 (
-  parameter integer CNTR_WIDTH = 32,
-  parameter integer AXIS_TDATA_WIDTH = 32
+  parameter integer AXIS_TDATA_WIDTH = 32,
+  parameter integer CNTR_WIDTH = 32
 )
 (
   // System signals
@@ -19,7 +19,7 @@ module axis_counter #
 );
 
   reg [CNTR_WIDTH-1:0] int_cntr_reg, int_cntr_next;
-  reg int_tvalid_reg, int_tvalid_next;
+  reg int_enbl_reg, int_enbl_next;
 
   wire int_comp_wire;
 
@@ -28,12 +28,12 @@ module axis_counter #
     if(~aresetn)
     begin
       int_cntr_reg <= {(CNTR_WIDTH){1'b0}};
-      int_tvalid_reg <= 1'b0;
+      int_enbl_reg <= 1'b0;
     end
     else
     begin
       int_cntr_reg <= int_cntr_next;
-      int_tvalid_reg <= int_tvalid_next;
+      int_enbl_reg <= int_enbl_next;
     end
   end
 
@@ -42,25 +42,25 @@ module axis_counter #
   always @*
   begin
     int_cntr_next = int_cntr_reg;
-    int_tvalid_next = int_tvalid_reg;
+    int_enbl_next = int_enbl_reg;
 
-    if(~int_tvalid_reg & int_comp_wire)
+    if(~int_enbl_reg & int_comp_wire)
     begin
-      int_tvalid_next = 1'b1;
+      int_enbl_next = 1'b1;
     end
 
-    if(int_tvalid_reg & int_comp_wire)
+    if(int_enbl_reg & int_comp_wire)
     begin
       int_cntr_next = int_cntr_reg + 1'b1;
     end
 
-    if(int_tvalid_reg & (int_cntr_reg == cfg_data))
+    if(int_enbl_reg & (int_cntr_reg == cfg_data))
     begin
-      int_tvalid_next = 1'b0;
+      int_enbl_next = 1'b0;
     end
   end
 
   assign m_axis_tdata = {{(AXIS_TDATA_WIDTH-CNTR_WIDTH){1'b0}}, int_cntr_reg};
-  assign m_axis_tvalid = int_tvalid_reg;
+  assign m_axis_tvalid = int_enbl_reg;
 
 endmodule
