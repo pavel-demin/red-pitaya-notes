11c8179e10a6540741af3581f3efefba6fdfe21a
Pavel Demin <pavel.demin@cern.ch>
2015-02-02 22:00:16 +0100

    fix dds_compiler parameters and remove unneeded cores

--- projects/dac_test/block_design.tcl
+++ projects/dac_test/block_design.tcl
@@ -16,31 +16,13 @@ cell xilinx.com:ip:clk_wiz:5.1 pll_0 {
 # Create dds_compiler
 cell xilinx.com:ip:dds_compiler:6.0 dds_0 {
   DDS_CLOCK_RATE 125
-  SPURIOUS_FREE_DYNAMIC_RANGE 83
+  SPURIOUS_FREE_DYNAMIC_RANGE 84
   FREQUENCY_RESOLUTION 0.5
-  OUTPUT_SELECTION Sine
+  AMPLITUDE_MODE Unit_Circle
   HAS_PHASE_OUT false
   OUTPUT_FREQUENCY1 0.9765625
 } {
   aclk pll_0/clk_out1
-}
-
-# Create xlconstant
-cell xilinx.com:ip:xlconstant:1.1 const_3
-
-# Create axis_broadcaster
-cell xilinx.com:ip:axis_broadcaster:1.1 bcast_0 {} {
-  aclk pll_0/clk_out1
-  aresetn const_3/dout
-  S_AXIS dds_0/M_AXIS_DATA
-}
-
-# Create axis_combiner
-cell xilinx.com:ip:axis_combiner:1.1 comb_0 {} {
-  aclk pll_0/clk_out1
-  aresetn const_3/dout
-  S00_AXIS bcast_0/M00_AXIS
-  S01_AXIS bcast_0/M01_AXIS
 }
 
 # Create axis_red_pitaya_dac
@@ -48,7 +30,7 @@ cell pavel-demin:user:axis_red_pitaya_dac:1.0 dac_0 {} {
   aclk pll_0/clk_out1
   ddr_clk pll_0/clk_out2
   locked pll_0/locked
-  S_AXIS comb_0/M_AXIS
+  S_AXIS dds_0/M_AXIS_DATA
   dac_clk dac_clk_o
   dac_rst dac_rst_o
   dac_sel dac_sel_o
