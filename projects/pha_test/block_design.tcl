9f951d63550a16e2b6b6da34ce37223689e77c09
Pavel Demin <pavel.demin@cern.ch>
2015-02-03 17:51:40 +0100

    add pha_test

---
+++ projects/pha_test/block_design.tcl
@@ -0,0 +1,71 @@
+source projects/cfg_test/block_design.tcl
+
+# Create xlslice
+cell xilinx.com:ip:xlslice:1.0 slice_2 {
+  DIN_WIDTH 1024 DIN_FROM 0 DIN_TO 0 DOUT_WIDTH 1
+} {
+  Din cfg_0/cfg_data
+}
+
+# Create xlslice
+cell xilinx.com:ip:xlslice:1.0 slice_3 {
+  DIN_WIDTH 1024 DIN_FROM 1 DIN_TO 1 DOUT_WIDTH 1
+} {
+  Din cfg_0/cfg_data
+}
+
+# Create xlslice
+cell xilinx.com:ip:xlslice:1.0 slice_4 {
+  DIN_WIDTH 1024 DIN_FROM 2 DIN_TO 2 DOUT_WIDTH 1
+} {
+  Din cfg_0/cfg_data
+}
+
+# Create xlslice
+cell xilinx.com:ip:xlslice:1.0 slice_5 {
+  DIN_WIDTH 1024 DIN_FROM 63 DIN_TO 32 DOUT_WIDTH 32
+} {
+  Din cfg_0/cfg_data
+}
+
+# Create xlslice
+cell xilinx.com:ip:xlslice:1.0 slice_6 {
+  DIN_WIDTH 1024 DIN_FROM 143 DIN_TO 64 DOUT_WIDTH 48
+} {
+  Din cfg_0/cfg_data
+}
+
+# Create axis_counter
+cell pavel-demin:user:axis_counter:1.0 cntr_1 {} {
+  cfg_data slice_5/Dout
+  aclk ps_0/FCLK_CLK0
+  aresetn slice_2/Dout
+}
+
+# Create axis_pulse_height_analyzer
+cell pavel-demin:user:axis_pulse_height_analyzer:1.0 pha_0 {
+  AXIS_TDATA_WIDTH 32
+  AXIS_TDATA_SIGNED FALSE
+} {
+  S_AXIS cntr_1/M_AXIS
+  cfg_data slice_6/Dout
+  aclk ps_0/FCLK_CLK0
+  aresetn slice_3/Dout
+}
+
+# Create xlconstant
+cell xilinx.com:ip:xlconstant:1.1 const_1 {
+  CONST_WIDTH 32
+  CONST_VAL 503316480
+}
+
+# Create axis_ram_writer
+cell pavel-demin:user:axis_ram_writer:1.0 writer_0 {} {
+  S_AXIS pha_0/M_AXIS
+  M_AXI ps_0/S_AXI_HP0
+  cfg_data const_1/dout
+  aclk ps_0/FCLK_CLK0
+  aresetn slice_4/Dout
+}
+
+assign_bd_address [get_bd_addr_segs ps_0/S_AXI_HP0/HP0_DDR_LOWOCM]
