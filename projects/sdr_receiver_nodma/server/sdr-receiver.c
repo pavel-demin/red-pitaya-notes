5671a0e40e8284add4c50bd1fc676dd89bdc2ac4
Pavel Demin <pavel.demin@cern.ch>
2015-03-20 09:25:25 +0100

    use TCP for data add more sample rates

--- projects/sdr_receiver_nodma/server/sdr-receiver.c
+++ projects/sdr_receiver_nodma/server/sdr-receiver.c
@@ -13,7 +13,6 @@
 #include <arpa/inet.h>
 
 #define TCP_PORT 1001
-#define UDP_PORT 1002
 
 int interrupted = 0;
 
@@ -37,7 +36,7 @@ int main(int argc, char *argv[])
   uint32_t freqMax = 50000000;
   int yes = 1;
 
-  if((file = open(name, O_RDWR)) < 0)
+  if((file = open(name, O_RDWR)) < 1)
   {
     perror("open");
     return 1;
@@ -47,7 +46,7 @@ int main(int argc, char *argv[])
   sts = mmap(NULL, sysconf(_SC_PAGESIZE), PROT_READ|PROT_WRITE, MAP_SHARED, file, 0x40001000);
   ram = mmap(NULL, sysconf(_SC_PAGESIZE), PROT_READ|PROT_WRITE, MAP_SHARED, file, 0x40002000);
 
-  if((sockServer = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+  if((sockServer = socket(AF_INET, SOCK_STREAM, 0)) < 1)
   {
     perror("socket");
     return 1;
@@ -76,77 +75,24 @@ int main(int argc, char *argv[])
   while(!interrupted)
   {
     /* enter reset mode */
-    *((uint32_t *)(cfg + 0)) &= ~7;
+    *((uint32_t *)(cfg + 0)) &= ~15;
     /* set default phase increment */
     *((uint32_t *)(cfg + 4)) = (uint32_t)floor(600000/125.0e6*(1<<30)+0.5);
-    /* set default bandwidth */
-    *((uint8_t *)(cfg + 8)) = 255;
-    *((uint8_t *)(cfg + 9)) = 0;
+    /* set default sample rate */
+    *((uint32_t *)(cfg + 8)) = 625;
 
-    if((sockClient = accept(sockServer, (struct sockaddr *)&addrClient, &lenClient)) < 0)
+    sockClient = accept(sockServer, (struct sockaddr *)&addrClient, &lenClient);
+
+    /* enter normal operating mode */
+    *((uint32_t *)(cfg + 0)) |= 15;
+
+    while(!interrupted)
     {
-      perror("accept");
-      return 1;
-    }
+      ioctl(sockClient, FIONREAD, &size);
 
-    pid = fork();
-    if(pid == 0)
-    {
-      /* child process */
-
-      int sockData;
-      struct sockaddr_in addrData;
-      int pos, limit, start;
-
-      if((sockData = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+      if(size >= 4)
       {
-        perror("socket");
-        return 1;
-      }
-
-      /* set up destination address */
-      memset(&addrData, 0, sizeof(addrData));
-      addrData.sin_family = AF_INET;
-      addrData.sin_addr = addrClient.sin_addr;
-      addrData.sin_port = htons(UDP_PORT);
-
-      connect(sockData, (struct sockaddr *)&addrData, sizeof(addrData));
-
-      signal(SIGINT, signal_handler);
-
-      limit = 128;
-
-      while(!interrupted)
-      {
-        /* read ram writer position */
-        pos = *((uint32_t *)(sts + 0));
-
-        /* send 1024 bytes if ready, otherwise sleep 0.5 ms */
-        if((limit > 0 && pos > limit) || (limit == 0 && pos < 384))
-        {
-          start = limit > 0 ? limit*8 - 1024 : 3072;
-          send(sockData, ram + start, 1024, 0);
-          limit += 128;
-          if(limit == 512) limit = 0;
-        }
-        else
-        {
-          usleep(100);
-        }
-      }
-
-      close(sockData);
-    }
-    else if(pid > 0)
-    {
-      /* parent process */
-
-      /* enter normal operating mode */
-      *((uint32_t *)(cfg + 0)) |= 7;
-
-      while(!interrupted)
-      {
-        if(recv(sockClient, (char *)&command, 4, 0) == 0) break;
+        recv(sockClient, (char *)&command, 4, 0);
         switch(command >> 31)
         {
           case 0:
@@ -155,22 +101,60 @@ int main(int argc, char *argv[])
             *((uint32_t *)(cfg + 4)) = (uint32_t)floor(command/125.0e6*(1<<30)+0.5);
             break;
           case 1:
-            /* set bandwidth */
-            switch(command & 1)
+            /* set sample rate */
+            switch(command & 3)
             {
-              case 0: freqMin = 100000; *((uint8_t *)(cfg + 8)) = 255; *((uint8_t *)(cfg + 9)) = 0; break;
-              case 1: freqMin = 300000; *((uint8_t *)(cfg + 8)) = 0; *((uint8_t *)(cfg + 9)) = 255; break;
+              case 0:
+                freqMin = 75000;
+                *((uint32_t *)(cfg + 0)) &= ~8;
+                *((uint32_t *)(cfg + 8)) = 1250;
+                *((uint32_t *)(cfg + 0)) |= 8;
+                break;
+              case 1:
+                freqMin = 100000;
+                *((uint32_t *)(cfg + 0)) &= ~8;
+                *((uint32_t *)(cfg + 8)) = 625;
+                *((uint32_t *)(cfg + 0)) |= 8;
+                break;
+              case 2:
+                freqMin = 175000;
+                *((uint32_t *)(cfg + 0)) &= ~8;
+                *((uint32_t *)(cfg + 8)) = 250;
+                *((uint32_t *)(cfg + 0)) |= 8;
+                break;
+              case 3:
+                freqMin = 300000;
+                *((uint32_t *)(cfg + 0)) &= ~8;
+                *((uint32_t *)(cfg + 8)) = 125;
+                *((uint32_t *)(cfg + 0)) |= 8;
+                break;
             }
             break;
         }
       }
 
-      kill(pid, SIGTERM);
-      waitpid(pid, NULL, 0);
+      /* read ram writer position */
+      pos = *((uint32_t *)(sts + 0));
 
-      close(sockClient);
+      /* send 1024 bytes if ready, otherwise sleep 0.5 ms */
+      if((limit > 0 && pos > limit) || (limit == 0 && pos < 384))
+      {
+        start = limit > 0 ? limit*8 - 1024 : 3072;
+        if(send(sockClient, ram + start, 1024, 0) < 0) break;
+        limit += 128;
+        if(limit == 512) limit = 0;
+      }
+      else
+      {
+        usleep(100);
+      }
     }
+
+    close(sockClient);
   }
+
+  /* enter reset mode */
+  *((uint32_t *)(cfg + 0)) &= ~15;
 
   return 0;
 }
