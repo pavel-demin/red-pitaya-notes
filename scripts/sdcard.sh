dbd4832679d5eb252c25fd89ef3a1c993511cd46
Pavel Demin <pavel.demin@cern.ch>
2015-05-07 09:13:18 +0200

    automatically start and stop hostapd and isc-dhcp-server

--- scripts/sdcard.sh
+++ scripts/sdcard.sh
@@ -108,6 +108,10 @@ allow-hotplug wlan0
 iface wlan0 inet static
   address 192.168.42.1
   netmask 255.255.255.0
+  post-up service hostapd restart
+  post-up service isc-dhcp-server restart
+  pre-down service isc-dhcp-server stop
+  pre-down service hostapd stop
 EOF_CAT
 
 cat <<- EOF_CAT > etc/hostapd/hostapd.conf
