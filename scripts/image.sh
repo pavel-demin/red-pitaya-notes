a1ba96e9ceb8195e4c5e57b9b2e9690820d3e95f
Pavel Demin <pavel.demin@cern.ch>
2015-05-25 00:03:45 +0200

    add optional size parameter

--- scripts/image.sh
+++ scripts/image.sh
@@ -1,7 +1,14 @@
 script=$1
 image=$2
 
-dd if=/dev/zero of=$image bs=1M count=512
+size=512
+
+if [ $# -eq 3 ]
+then
+  size=$3
+fi
+
+dd if=/dev/zero of=$image bs=1M count=$size
 
 device=`losetup -f`
 
