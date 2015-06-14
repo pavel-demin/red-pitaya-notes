d216d4164c8ab36057c8573914d1878d72087487
Pavel Demin <pavel.demin@cern.ch>
2015-06-15 00:15:17 +0200

    add m_axis_tready to axis_phase_generator

--- cores/axis_phase_generator_v1_0/axis_phase_generator.v
+++ cores/axis_phase_generator_v1_0/axis_phase_generator.v
@@ -13,21 +13,14 @@ module axis_phase_generator #
 
   input  wire [PHASE_WIDTH-1:0]      cfg_data,
 
-  // Slave side
-  output wire                        s_axis_tready,
-  input  wire                        s_axis_tvalid,
-
   // Master side
+  input  wire                        m_axis_tready,
   output wire [AXIS_TDATA_WIDTH-1:0] m_axis_tdata,
   output wire                        m_axis_tvalid
 );
 
   reg [PHASE_WIDTH-1:0] int_cntr_reg, int_cntr_next;
   reg int_enbl_reg, int_enbl_next;
-
-  wire int_tvalid_wire;
-
-  assign int_tvalid_wire = int_enbl_reg & s_axis_tvalid;
 
   always @(posedge aclk)
   begin
@@ -53,14 +46,13 @@ module axis_phase_generator #
       int_enbl_next = 1'b1;
     end
 
-    if(int_tvalid_wire)
+    if(int_enbl_reg & m_axis_tready)
     begin
       int_cntr_next = int_cntr_reg + cfg_data;
     end
   end
 
-  assign s_axis_tready = int_enbl_reg;
   assign m_axis_tdata = {{(AXIS_TDATA_WIDTH-PHASE_WIDTH){int_cntr_reg[PHASE_WIDTH-1]}}, int_cntr_reg};
-  assign m_axis_tvalid = int_tvalid_wire;
+  assign m_axis_tvalid = int_enbl_reg;
 
 endmodule
