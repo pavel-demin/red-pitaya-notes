0e2b70e5fae6290ae041a2903cc5ce131b726947
Pavel Demin <demin@eleclap1.fynu.ucl.ac.be>
2015-09-05 21:37:54 +0200

    working version of GNU Radio blocks

--- projects/sdr_transceiver/gnuradio/gr_redpitaya.py
+++ projects/sdr_transceiver/gnuradio/gr_redpitaya.py
@@ -30,19 +30,20 @@ class sdr_transceiver_rx(gr.sync_block):
       self,
       name = "sdr_transceiver_rx",
       in_sig = [],
-      out_sig = [numpy.complex64],
+      out_sig = [numpy.complex64]
     )
     self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     self.sock.connect((addr,port))
-    if rx_rate in sdr_transceiver_rx.rx_rates:
-      self.set_rate(sdr_transceiver_rx.rx_rates[rx_rate])
-    else:
-      raise ValueError("acceptable sample rates are 50k, 100k, 250k, 500k")
+    self.set_rx_rate(rx_rate)
     self.set_rx_freq(rx_freq, corr)
     self.set_tx_freq(tx_freq, corr)
 
-  def set_rx_rate(self, ratecode):
-    self.sock.send(struct.pack('<I', 1<<28 | ratecode))
+  def set_rx_rate(self, rate):
+    if rate in sdr_transceiver_rx.rx_rates:
+      code = sdr_transceiver_rx.rx_rates[rate]
+      self.sock.send(struct.pack('<I', 1<<28 | code))
+    else:
+      raise ValueError("acceptable sample rates are 50k, 100k, 250k, 500k")
 
   def set_rx_freq(self, freq, corr):
     self.sock.send(struct.pack('<I', 0<<28 | int((1.0 + 1e-6 * corr) * freq)))
@@ -51,12 +52,12 @@ class sdr_transceiver_rx(gr.sync_block):
     self.sock.send(struct.pack('<I', 2<<28 | int((1.0 + 1e-6 * corr) * freq)))
 
   def set_ptt(self, on):
-    self.sock.send(struct.pack('<I', (3<<28, 4<<28)[on == False])
+    self.sock.send(struct.pack('<I', (3<<28, 4<<28)[on == False]))
 
   def work(self, input_items, output_items):
-    data = self.sock.recv(2048, socket.MSG_WAITALL)
-    output_items[0] = numpy.fromstring(data, numpy.complex64)
-    return 256
+    data = self.sock.recv(len(output_items[0]) * 8, socket.MSG_WAITALL)
+    output_items[0][:] = numpy.fromstring(data, numpy.complex64)
+    return len(output_items[0])
 
 sdr_transceiver_rx.rx_rates={50000:0, 100000:1, 250000:2, 500000:3}
 
@@ -67,11 +68,11 @@ class sdr_transceiver_tx(gr.sync_block):
       self,
       name = "sdr_transceiver_rx",
       in_sig = [numpy.complex64],
-      out_sig = [],
+      out_sig = []
     )
     self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     self.sock.connect((addr,port))
 
   def work(self, input_items, output_items):
-    self.sock.send(input_items.tostring(), socket.MSG_NOSIGNAL)
-    return len(input_items)
+    self.sock.send(input_items[0].tostring())
+    return len(input_items[0])
