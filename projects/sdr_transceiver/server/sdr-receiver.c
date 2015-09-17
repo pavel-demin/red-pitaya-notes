a9aedd5bf8a8b8de3fc4bbe4234a99455e014987
Pavel Demin <pavel.demin@cern.ch>
2015-09-17 11:02:31 +0200

    move set_tx_freq and set_tx_rate to red_pitaya_sink

--- projects/sdr_transceiver/server/sdr-receiver.c
+++ projects/sdr_transceiver/server/sdr-receiver.c
@@ -5,37 +5,30 @@
 #include <stdint.h>
 #include <string.h>
 #include <unistd.h>
-#include <signal.h>
 #include <fcntl.h>
 #include <math.h>
+#include <pthread.h>
 #include <sys/mman.h>
-#include <sys/types.h>
 #include <sys/ioctl.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-int interrupted = 0;
+void *cfg, *sts, *ram;
+int sockThread[2] = {0, 0};
 
-void signal_handler(int sig)
-{
-  interrupted = 1;
-}
+void *ctrl_handler(void *arg);
+void *data_handler(void *arg);
 
 int main(int argc, char *argv[])
 {
   int fd, sockServer, sockClient;
-  int position, limit, offset;
-  pid_t pid;
-  void *cfg, *sts, *ram;
+  pthread_t thread;
+  void *(*handler[2])(void *) = {ctrl_handler, data_handler};
   char *end, *name = "/dev/mem";
-  char buf[4096];
-  int size = 0;
   struct sockaddr_in addr;
   uint16_t port;
-  uint32_t command, freq;
-  uint32_t freqMin = 50000;
-  uint32_t freqMax = 60000000;
+  uint32_t command;
   int yes = 1;
   long number;
 
@@ -43,7 +36,7 @@ int main(int argc, char *argv[])
   number = (argc == 2) ? strtol(argv[1], &end, 10) : -1;
   if(errno != 0 || end == argv[1] || number < 0 || number > 1)
   {
-    printf("Usage: sdr-receiver 0|1\n");
+    printf("Usage: sdr-controller 0|1\n");
     return EXIT_FAILURE;
   }
 
@@ -69,6 +62,11 @@ int main(int argc, char *argv[])
       break;
   }
 
+  /* set default rx phase increment */
+  *((uint32_t *)(cfg + 4)) = (uint32_t)floor(600000/125.0e6*(1<<30)+0.5);
+  /* set default rx sample rate */
+  *((uint32_t *)(cfg + 8)) = 625;
+
   if((sockServer = socket(AF_INET, SOCK_STREAM, 0)) < 0)
   {
     perror("socket");
@@ -91,125 +89,129 @@ int main(int argc, char *argv[])
 
   listen(sockServer, 1024);
 
-  while(!interrupted)
+  while(1)
   {
-    /* set default rx phase increment */
-    *((uint32_t *)(cfg + 4)) = (uint32_t)floor(600000/125.0e6*(1<<30)+0.5);
-    /* set default rx sample rate */
-    *((uint32_t *)(cfg + 8)) = 625;
-    /* set default tx phase increment */
-    *((uint32_t *)(cfg + 12)) = (uint32_t)floor(600000/125.0e6*(1<<30)+0.5);
-    /* set default tx sample rate */
-    *((uint32_t *)(cfg + 16)) = 625;
-
     if((sockClient = accept(sockServer, NULL, NULL)) < 0)
     {
       perror("accept");
       return EXIT_FAILURE;
     }
 
-    signal(SIGINT, signal_handler);
+    if(recv(sockClient, (char *)&command, 4, MSG_WAITALL) < 0) continue;
 
-    limit = 512;
-
-    while(!interrupted)
+    if(command > 1 || sockThread[command])
     {
-      if(ioctl(sockClient, FIONREAD, &size) < 0) break;
-
-      if(size >= 4)
-      {
-        if(recv(sockClient, (char *)&command, 4, MSG_WAITALL) < 0) break;
-        switch(command >> 28)
-        {
-          case 0:
-            /* set rx phase increment */
-            freq = command & 0xfffffff;
-            if(freq < freqMin || freq > freqMax) continue;
-            *((uint32_t *)(cfg + 4)) = (uint32_t)floor(freq/125.0e6*(1<<30)+0.5);
-            break;
-          case 1:
-            /* set rx sample rate */
-            switch(command & 7)
-            {
-              case 0:
-                freqMin = 10000;
-                *((uint32_t *)(cfg + 8)) = 3125;
-                break;
-              case 1:
-                freqMin = 25000;
-                *((uint32_t *)(cfg + 8)) = 1250;
-                break;
-              case 2:
-                freqMin = 50000;
-                *((uint32_t *)(cfg + 8)) = 625;
-                break;
-              case 3:
-                freqMin = 125000;
-                *((uint32_t *)(cfg + 8)) = 250;
-                break;
-              case 4:
-                freqMin = 250000;
-                *((uint32_t *)(cfg + 8)) = 125;
-                break;
-            }
-            break;
-          case 2:
-            /* set tx phase increment */
-            freq = command & 0xfffffff;
-            if(freq < freqMin || freq > freqMax) continue;
-            *((uint32_t *)(cfg + 12)) = (uint32_t)floor(freq/125.0e6*(1<<30)+0.5);
-            break;
-          case 3:
-            /* set tx sample rate */
-            switch(command & 7)
-            {
-              case 0:
-                freqMin = 10000;
-                *((uint32_t *)(cfg + 16)) = 3125;
-                break;
-              case 1:
-                freqMin = 25000;
-                *((uint32_t *)(cfg + 16)) = 1250;
-                break;
-              case 2:
-                freqMin = 50000;
-                *((uint32_t *)(cfg + 16)) = 625;
-                break;
-              case 3:
-                freqMin = 125000;
-                *((uint32_t *)(cfg + 16)) = 250;
-                break;
-              case 4:
-                freqMin = 250000;
-                *((uint32_t *)(cfg + 16)) = 125;
-                break;
-            }
-            break;
-        }
-      }
-
-      /* read ram writer position */
-      position = *((uint16_t *)(sts + 0));
-
-      /* send 4096 bytes if ready, otherwise sleep 0.5 ms */
-      if((limit > 0 && position > limit) || (limit == 0 && position < 512))
-      {
-        offset = limit > 0 ? 0 : 4096;
-        limit = limit > 0 ? 0 : 512;
-        memcpy(buf, ram + offset, 4096);
-        if(send(sockClient, buf, 4096, MSG_NOSIGNAL) < 0) break;
-      }
-      else
-      {
-        usleep(500);
-      }
+      close(sockClient);
+      continue;
     }
 
-    signal(SIGINT, SIG_DFL);
-    close(sockClient);
+    sockThread[command] = sockClient;
+
+    if(pthread_create(&thread, NULL, handler[command], NULL) < 0)
+    {
+      perror("pthread_create");
+      return EXIT_FAILURE;
+    }
+    pthread_detach(thread);
   }
 
   close(sockServer);
 
   return EXIT_SUCCESS;
+}
+
+void *ctrl_handler(void *arg)
+{
+  int sockClient = sockThread[0];
+  uint32_t command, freq;
+  uint32_t freqMin = 50000;
+  uint32_t freqMax = 60000000;
+
+  /* set default rx phase increment */
+  *((uint32_t *)(cfg + 4)) = (uint32_t)floor(600000/125.0e6*(1<<30)+0.5);
+  /* set default rx sample rate */
+  *((uint32_t *)(cfg + 8)) = 625;
+
+  while(1)
+  {
+    if(recv(sockClient, (char *)&command, 4, MSG_WAITALL) < 0) break;
+    switch(command >> 28)
+    {
+      case 0:
+        /* set rx phase increment */
+        freq = command & 0xfffffff;
+        if(freq < freqMin || freq > freqMax) continue;
+        *((uint32_t *)(cfg + 4)) = (uint32_t)floor(freq/125.0e6*(1<<30)+0.5);
+        break;
+      case 1:
+        /* set rx sample rate */
+        switch(command & 7)
+        {
+          case 0:
+            freqMin = 10000;
+            *((uint32_t *)(cfg + 8)) = 3125;
+            break;
+          case 1:
+            freqMin = 25000;
+            *((uint32_t *)(cfg + 8)) = 1250;
+            break;
+          case 2:
+            freqMin = 50000;
+            *((uint32_t *)(cfg + 8)) = 625;
+            break;
+          case 3:
+            freqMin = 125000;
+            *((uint32_t *)(cfg + 8)) = 250;
+            break;
+          case 4:
+            freqMin = 250000;
+            *((uint32_t *)(cfg + 8)) = 125;
+            break;
+        }
+        break;
+    }
+  }
+
+  /* set default rx phase increment */
+  *((uint32_t *)(cfg + 4)) = (uint32_t)floor(600000/125.0e6*(1<<30)+0.5);
+  /* set default rx sample rate */
+  *((uint32_t *)(cfg + 8)) = 625;
+
+  close(sockClient);
+  sockThread[0] = 0;
+
+  return NULL;
+}
+
+void *data_handler(void *arg)
+{
+  int sockClient = sockThread[1];
+  int position, limit, offset;
+  char buf[4096];
+
+  limit = 512;
+
+  while(1)
+  {
+    /* read ram writer position */
+    position = *((uint16_t *)(sts + 0));
+
+    /* send 4096 bytes if ready, otherwise sleep 0.5 ms */
+    if((limit > 0 && position > limit) || (limit == 0 && position < 512))
+    {
+      offset = limit > 0 ? 0 : 4096;
+      limit = limit > 0 ? 0 : 512;
+      memcpy(buf, ram + offset, 4096);
+      if(send(sockClient, buf, 4096, MSG_NOSIGNAL) < 0) break;
+    }
+    else
+    {
+      usleep(500);
+    }
+  }
+
+  close(sockClient);
+  sockThread[1] = 0;
+
+  return NULL;
 }
--- projects/sdr_transceiver/server/sdr-transmitter.c
+++ projects/sdr_transceiver/server/sdr-transmitter.c
@@ -5,32 +5,30 @@
 #include <stdint.h>
 #include <string.h>
 #include <unistd.h>
-#include <signal.h>
 #include <fcntl.h>
 #include <math.h>
+#include <pthread.h>
 #include <sys/mman.h>
-#include <sys/types.h>
 #include <sys/ioctl.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-int interrupted = 0;
+void *cfg, *sts, *ram;
+int sockThread[2] = {0, 0};
 
-void signal_handler(int sig)
-{
-  interrupted = 1;
-}
+void *ctrl_handler(void *arg);
+void *data_handler(void *arg);
 
 int main(int argc, char *argv[])
 {
   int fd, sockServer, sockClient;
-  int position, limit, offset;
-  void *cfg, *sts, *ram;
+  pthread_t thread;
+  void *(*handler[2])(void *) = {ctrl_handler, data_handler};
   char *end, *name = "/dev/mem";
-  char buf[4096];
   struct sockaddr_in addr;
   uint16_t port;
+  uint32_t command;
   int yes = 1;
   long number;
 
@@ -38,7 +36,7 @@ int main(int argc, char *argv[])
   number = (argc == 2) ? strtol(argv[1], &end, 10) : -1;
   if(errno != 0 || end == argv[1] || number < 0 || number > 1)
   {
-    printf("Usage: sdr-transmitter 0|1\n");
+    printf("Usage: sdr-controller 0|1\n");
     return EXIT_FAILURE;
   }
 
@@ -64,6 +62,15 @@ int main(int argc, char *argv[])
       break;
   }
 
+  /* set PTT pin to low */
+  *((uint32_t *)(cfg + 0)) = 0;
+  /* set default tx phase increment */
+  *((uint32_t *)(cfg + 12)) = (uint32_t)floor(600000/125.0e6*(1<<30)+0.5);
+  /* set default tx sample rate */
+  *((uint32_t *)(cfg + 16)) = 625;
+
+  memset(ram, 0, 8192);
+
   if((sockServer = socket(AF_INET, SOCK_STREAM, 0)) < 0)
   {
     perror("socket");
@@ -86,50 +93,159 @@ int main(int argc, char *argv[])
 
   listen(sockServer, 1024);
 
-  while(!interrupted)
+  while(1)
   {
-    /* set PTT pin to low */
-    *((uint32_t *)(cfg + 0)) = 0;
-
-    memset(ram, 0, 8192);
-
     if((sockClient = accept(sockServer, NULL, NULL)) < 0)
     {
       perror("accept");
       return EXIT_FAILURE;
     }
-
-    signal(SIGINT, signal_handler);
-
-    /* set PTT pin to high */
-    *((uint32_t *)(cfg + 0)) = 1;
-
-    limit = 512;
-
-    while(!interrupted)
+    if(recv(sockClient, (char *)&command, 4, MSG_WAITALL) < 0) continue;
+    if(command > 1 || sockThread[command])
     {
-      /* read ram reader position */
-      position = *((uint16_t *)(sts + 2));
-
-      /* receive 4096 bytes if ready, otherwise sleep 0.5 ms */
-      if((limit > 0 && position > limit) || (limit == 0 && position < 512))
-      {
-        offset = limit > 0 ? 0 : 4096;
-        limit = limit > 0 ? 0 : 512;
-        if(recv(sockClient, buf, 4096, MSG_WAITALL) <= 0) break;
-        memcpy(ram + offset, buf, 4096);
-      }
-      else
-      {
-        usleep(500);
-      }
+      close(sockClient);
+      continue;
     }
 
-    signal(SIGINT, SIG_DFL);
-    close(sockClient);
+    sockThread[command] = sockClient;
+
+    if(pthread_create(&thread, NULL, handler[command], NULL) < 0)
+    {
+      perror("pthread_create");
+      return EXIT_FAILURE;
+    }
+    pthread_detach(thread);
   }
 
   close(sockServer);
 
   return EXIT_SUCCESS;
+}
+
+void *ctrl_handler(void *arg)
+{
+  int sockClient = sockThread[0];
+  uint32_t command, freq;
+  uint32_t freqMin = 50000;
+  uint32_t freqMax = 60000000;
+
+  /* set PTT pin to low */
+  *((uint32_t *)(cfg + 0)) = 0;
+  /* set default tx phase increment */
+  *((uint32_t *)(cfg + 12)) = (uint32_t)floor(600000/125.0e6*(1<<30)+0.5);
+  /* set default tx sample rate */
+  *((uint32_t *)(cfg + 16)) = 625;
+
+  memset(ram, 0, 8192);
+
+  while(1)
+  {
+    if(recv(sockClient, (char *)&command, 4, MSG_WAITALL) < 0) break;
+    switch(command >> 28)
+    {
+      case 0:
+        /* set tx phase increment */
+        freq = command & 0xfffffff;
+        if(freq < freqMin || freq > freqMax) continue;
+        *((uint32_t *)(cfg + 12)) = (uint32_t)floor(freq/125.0e6*(1<<30)+0.5);
+        break;
+      case 1:
+        /* set tx sample rate */
+        switch(command & 7)
+        {
+          case 0:
+            freqMin = 10000;
+            *((uint32_t *)(cfg + 16)) = 3125;
+            break;
+          case 1:
+            freqMin = 25000;
+            *((uint32_t *)(cfg + 16)) = 1250;
+            break;
+          case 2:
+            freqMin = 50000;
+            *((uint32_t *)(cfg + 16)) = 625;
+            break;
+          case 3:
+            freqMin = 125000;
+            *((uint32_t *)(cfg + 16)) = 250;
+            break;
+          case 4:
+            freqMin = 250000;
+            *((uint32_t *)(cfg + 16)) = 125;
+            break;
+        }
+        break;
+      case 2:
+        /* set PTT pin to high */
+        *((uint32_t *)(cfg + 0)) = 1;
+        break;
+      case 3:
+        /* set PTT pin to low */
+        *((uint32_t *)(cfg + 0)) = 0;
+        break;
+    }
+  }
+
+  /* set PTT pin to low */
+  *((uint32_t *)(cfg + 0)) = 0;
+  /* set default tx phase increment */
+  *((uint32_t *)(cfg + 12)) = (uint32_t)floor(600000/125.0e6*(1<<30)+0.5);
+  /* set default tx sample rate */
+  *((uint32_t *)(cfg + 16)) = 625;
+
+  memset(ram, 0, 8192);
+
+  close(sockClient);
+  sockThread[0] = 0;
+
+  return NULL;
+}
+
+void *data_handler(void *arg)
+{
+  int sockClient = sockThread[1];
+  int position, limit, offset;
+  int size = 0;
+  char buf[4096];
+
+  limit = 512;
+
+  while(1)
+  {
+    /* read ram reader position */
+    position = *((uint16_t *)(sts + 2));
+
+    /* receive 4096 bytes if ready, otherwise sleep 0.5 ms */
+    if((limit > 0 && position > limit) || (limit == 0 && position < 512))
+    {
+      offset = limit > 0 ? 0 : 4096;
+      limit = limit > 0 ? 0 : 512;
+      if(ioctl(sockClient, FIONREAD, &size) < 0) break;
+      if(size >= 4096)
+      {
+        if(recv(sockClient, buf, 4096, MSG_WAITALL) <= 0) break;
+        if(*((uint32_t *)(cfg + 0)))
+        {
+          memcpy(ram + offset, buf, 4096);
+        }
+        else
+        {
+          memset(ram + offset, 0, 4096);
+        }
+      }
+      else
+      {
+        memset(ram + offset, 0, 4096);
+      }
+    }
+    else
+    {
+      usleep(500);
+    }
+  }
+
+  close(sockClient);
+  sockThread[1] = 0;
+
+  return NULL;
 }
