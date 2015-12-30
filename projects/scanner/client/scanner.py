16138556f6022c96e45f97bb8b15662d5839ceeb
Pavel Demin <pavel.demin@cern.ch>
2015-12-30 23:48:30 +0100

    adapt scanner server and client to new scanner/block_design.tcl

--- projects/scanner/client/scanner.py
+++ projects/scanner/client/scanner.py
@@ -26,6 +26,7 @@ matplotlib.use('Qt5Agg')
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 from matplotlib.figure import Figure
+import matplotlib.cm as cm
 
 from PyQt5.uic import loadUiType
 from PyQt5.QtCore import QRegExp, QTimer
@@ -36,28 +37,31 @@ from PyQt5.QtNetwork import QAbstractSocket, QTcpSocket
 Ui_Scanner, QMainWindow = loadUiType('scanner.ui')
 
 class Scanner(QMainWindow, Ui_Scanner):
-  rates = {0:25.0e3, 1:50.0e3, 2:250.0e3, 3:500.0e3, 4:2500.0e3}
   def __init__(self):
     super(Scanner, self).__init__()
     self.setupUi(self)
-    self.rateValue.addItems(['25', '50', '250', '500', '2500'])
     # IP address validator
     rx = QRegExp('^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$')
     self.addrValue.setValidator(QRegExpValidator(rx, self.addrValue))
     # state variable
     self.idle = True
     # number of samples to show on the plot
-    self.size = 50000
+    self.size = 512 * 512
+    self.freq = 143.0
     # buffer and offset for the incoming samples
     self.buffer = bytearray(8 * self.size)
     self.offset = 0
-    self.data = np.frombuffer(self.buffer, np.complex64)
+    self.data = np.frombuffer(self.buffer, np.int32)
     # create figure
     figure = Figure()
     figure.set_facecolor('none')
     self.axes = figure.add_subplot(111)
     self.canvas = FigureCanvas(figure)
     self.plotLayout.addWidget(self.canvas)
+    self.axes.axis((0.0, 512.0, 0.0, 512.0))
+    x, y = np.meshgrid(np.linspace(0.0, 512.0, 513), np.linspace(0.0, 512.0, 513))
+    z = x / 512.0 + y * 0.0
+    self.mesh = self.axes.pcolormesh(x, y, z, cmap = cm.plasma)
     # create navigation toolbar
     self.toolbar = NavigationToolbar(self.canvas, self.plotWidget, False)
     # remove subplots action
@@ -70,38 +74,52 @@ class Scanner(QMainWindow, Ui_Scanner):
     self.socket.readyRead.connect(self.read_data)
     self.socket.error.connect(self.display_error)
     # connect signals from buttons and boxes
-    self.startButton.clicked.connect(self.start)
-    self.freqValue.valueChanged.connect(self.set_freq)
-    self.awidthValue.valueChanged.connect(self.set_awidth)
-    self.deltaValue.valueChanged.connect(self.set_delta)
-    self.rateValue.currentIndexChanged.connect(self.set_rate)
-    # set rate
-    self.rateValue.setCurrentIndex(2)
-    # create timer for the repetitions
-    self.timer = QTimer(self)
-    self.timer.timeout.connect(self.fire)
+    self.connectButton.clicked.connect(self.start)
+    self.scanButton.clicked.connect(self.scan)
+    self.periodValue.valueChanged.connect(self.set_period)
+    self.trgtimeValue.valueChanged.connect(self.set_trgtime)
+    self.shdelayValue.valueChanged.connect(self.set_shdelay)
+    self.shtimeValue.valueChanged.connect(self.set_shtime)
+    self.acqdelayValue.valueChanged.connect(self.set_acqdelay)
+    self.samplesValue.valueChanged.connect(self.set_samples)
+    # create timers
+    self.startTimer = QTimer(self)
+    self.startTimer.timeout.connect(self.timeout)
+    self.meshTimer = QTimer(self)
+    self.meshTimer.timeout.connect(self.update_mesh)
 
   def start(self):
     if self.idle:
-      self.startButton.setEnabled(False)
+      self.connectButton.setEnabled(False)
       self.socket.connectToHost(self.addrValue.text(), 1001)
+      self.startTimer.start(5000)
     else:
-      self.idle = True
-      self.timer.stop()
-      self.socket.close()
-      self.offset = 0
-      self.startButton.setText('Start')
-      self.startButton.setEnabled(True)
+      self.stop()
+
+  def stop(self):
+    self.idle = True
+    self.socket.abort()
+    self.offset = 0
+    self.connectButton.setText('Connect')
+    self.connectButton.setEnabled(True)
+    self.scanButton.setEnabled(True)
+
+  def timeout(self):
+    self.startTimer.stop()
+    self.display_error('timeout')
 
   def connected(self):
+    self.startTimer.stop()
     self.idle = False
-    self.set_freq(self.freqValue.value())
-    self.set_rate(self.rateValue.currentIndex())
-    self.set_awidth(self.awidthValue.value())
-    self.fire()
-    self.timer.start(self.deltaValue.value())
-    self.startButton.setText('Stop')
-    self.startButton.setEnabled(True)
+    self.set_period(self.periodValue.value())
+    self.set_trgtime(self.trgtimeValue.value())
+    self.set_shdelay(self.shdelayValue.value())
+    self.set_shtime(self.shtimeValue.value())
+    self.set_acqdelay(self.acqdelayValue.value())
+    self.set_samples(self.samplesValue.value())
+    self.connectButton.setText('Disconnect')
+    self.connectButton.setEnabled(True)
+    self.scanButton.setEnabled(True)
 
   def read_data(self):
     size = self.socket.bytesAvailable()
@@ -109,62 +127,54 @@ class Scanner(QMainWindow, Ui_Scanner):
       self.buffer[self.offset:self.offset + size] = self.socket.read(size)
       self.offset += size
     else:
+      self.meshTimer.stop()
       self.buffer[self.offset:8 * self.size] = self.socket.read(8 * self.size - self.offset)
       self.offset = 0
-      # plot the signal envelope
-      self.curve.set_ydata(np.abs(self.data))
-      self.canvas.draw()
+      self.update_mesh()
+      self.scanButton.setEnabled(True)
 
   def display_error(self, socketError):
-    if socketError == QAbstractSocket.RemoteHostClosedError:
-      pass
+    if socketError == 'timeout':
+      QMessageBox.information(self, 'Scanner', 'Error: connection timeout.')
     else:
       QMessageBox.information(self, 'Scanner', 'Error: %s.' % self.socket.errorString())
-    self.startButton.setText('Start')
-    self.startButton.setEnabled(True)
+    self.stop()
 
-  def set_freq(self, value):
+  def set_period(self, value):
     if self.idle: return
-    self.socket.write(struct.pack('<I', 0<<28 | int(1.0e6 * value)))
+    self.socket.write(struct.pack('<I', 0<<28 | int(value * self.freq)))
 
-  def set_rate(self, index):
-    # time axis
-    rate = float(Scanner.rates[index])
-    time = np.linspace(0.0, (self.size - 1) * 1000.0 / rate, self.size)
-    # reset toolbar
-    self.toolbar.home()
-    self.toolbar._views.clear()
-    self.toolbar._positions.clear()
-    # reset plot
-    self.axes.clear()
-    self.axes.grid()
-    # plot zeros and get store the returned Line2D object
-    self.curve, = self.axes.plot(time, np.zeros(self.size))
-    x1, x2, y1, y2 = self.axes.axis()
-    # set y axis limits
-    self.axes.axis((x1, x2, -0.1, 0.4))
-    self.axes.set_xlabel('time, ms')
+  def set_trgtime(self, value):
+    if self.idle: return
+    self.socket.write(struct.pack('<I', 1<<28 | int(value * self.freq)))
+
+  def set_shdelay(self, value):
+    if self.idle: return
+    self.socket.write(struct.pack('<I', 2<<28 | int(value * self.freq)))
+
+  def set_shtime(self, value):
+    if self.idle: return
+    self.socket.write(struct.pack('<I', 3<<28 | int(value * self.freq)))
+
+  def set_acqdelay(self, value):
+    if self.idle: return
+    self.socket.write(struct.pack('<I', 4<<28 | int(value * self.freq)))
+
+  def set_samples(self, value):
+    if self.idle: return
+    self.socket.write(struct.pack('<I', 5<<28 | int(value)))
+
+  def scan(self):
+    if self.idle: return
+    self.scanButton.setEnabled(False)
+    self.data[:] = np.zeros(2 * 512 * 512, np.int32)
+    self.update_mesh()
+    self.socket.write(struct.pack('<I', 6<<28))
+    self.meshTimer.start(1000)
+
+  def update_mesh(self):
+    self.mesh.set_array(self.data[0::2]/self.samplesValue.value()/8192.0)
     self.canvas.draw()
-    # set repetition time
-    minimum = self.size / rate * 2000.0
-    if minimum < 100.0: minimum = 100.0
-    self.deltaValue.setMinimum(minimum)
-    self.deltaValue.setValue(minimum)
-    if self.idle: return
-    self.socket.write(struct.pack('<I', 1<<28 | index))
-
-  def set_awidth(self, value):
-    if self.idle: return
-    self.socket.write(struct.pack('<I', 2<<28 | int(1.0e1 * value)))
-
-  def set_delta(self, value):
-    if self.idle: return
-    self.timer.stop()
-    self.timer.start(value)
-
-  def fire(self):
-    if self.idle: return
-    self.socket.write(struct.pack('<I', 3<<28))
 
 app = QApplication(sys.argv)
 window = Scanner()
