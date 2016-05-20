bbd226fa6a21cf702ce7a254ff8a4aaa6407827b
Pavel Demin <pavel.demin@cern.ch>
2016-05-20 22:34:24 +0200

    add dna_reader to led_blinker

--- projects/led_blinker/block_design.tcl
+++ projects/led_blinker/block_design.tcl
@@ -1,13 +1,20 @@
 source projects/base_system/block_design.tcl
 
+# Create proc_sys_reset
+cell xilinx.com:ip:proc_sys_reset:5.0 rst_0
+
+# ADC
+
 # Create axis_red_pitaya_adc
 cell pavel-demin:user:axis_red_pitaya_adc:1.0 adc_0 {} {
   adc_clk_p adc_clk_p_i
   adc_clk_n adc_clk_n_i
   adc_dat_a adc_dat_a_i
   adc_dat_b adc_dat_b_i
-  adc_csn   adc_csn_o
+  adc_csn adc_csn_o
 }
+
+# LED
 
 # Create c_counter_binary
 cell xilinx.com:ip:c_counter_binary:12.0 cntr_0 {
@@ -18,23 +25,47 @@ cell xilinx.com:ip:c_counter_binary:12.0 cntr_0 {
 
 # Create xlslice
 cell xilinx.com:ip:xlslice:1.0 slice_0 {
-  DIN_FROM 26
-  DIN_TO 26
+  DIN_WIDTH 32 DIN_FROM 26 DIN_TO 26 DOUT_WIDTH 1
 } {
   Din cntr_0/Q
+  Dout led_o
 }
 
+# STS
+
 # Create xlconstant
-cell xilinx.com:ip:xlconstant:1.1 const_0 {
-  CONST_WIDTH 7
-  CONST_VAL 0
+cell xilinx.com:ip:xlconstant:1.1 const_0
+
+# Create dna_reader
+cell pavel-demin:user:dna_reader:1.0 dna_0 {} {
+  aclk ps_0/FCLK_CLK0
+  aresetn rst_0/peripheral_aresetn
 }
 
 # Create xlconcat
 cell xilinx.com:ip:xlconcat:2.1 concat_0 {
-  IN1_WIDTH 7
+  NUM_PORTS 2
+  IN0_WIDTH 32
+  IN1_WIDTH 64
 } {
-  In0 slice_0/Dout
-  In1 const_0/dout
-  dout led_o
+  In0 const_0/dout
+  In1 dna_0/dna_data
 }
+
+# Create axi_sts_register
+cell pavel-demin:user:axi_sts_register:1.0 sts_0 {
+  STS_DATA_WIDTH 96
+  AXI_ADDR_WIDTH 32
+  AXI_DATA_WIDTH 32
+} {
+  sts_data concat_0/dout
+}
+
+# Create all required interconnections
+apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {
+  Master /ps_0/M_AXI_GP0
+  Clk Auto
+} [get_bd_intf_pins sts_0/S_AXI]
+
+set_property RANGE 4K [get_bd_addr_segs ps_0/Data/SEG_sts_0_reg0]
+set_property OFFSET 0x40000000 [get_bd_addr_segs ps_0/Data/SEG_sts_0_reg0]
