7990fa297a653778c3586c765c41d61a3660b002
Pavel Demin <pavel.demin@cern.ch>
2016-06-09 16:57:27 +0200

    add aplay to sdr-transceiver-hpsdr-ecosystem.sh

--- scripts/sdr-transceiver-hpsdr-ecosystem.sh
+++ scripts/sdr-transceiver-hpsdr-ecosystem.sh
@@ -23,8 +23,14 @@ echo 143000000 > \$devcfg/fclk/fclk0/set_rate
 
 cat /opt/redpitaya/sdr_transceiver_hpsdr.bit > /dev/xdevcfg
 
-/opt/redpitaya/bin/sdr-transceiver-hpsdr &
+server=/opt/redpitaya/bin/sdr-transceiver-hpsdr
 
+if aplay --list-devices | grep 'card 0:'
+then
+  \$server | aplay --format=S16_BE --channels=2 --rate=48000 &
+else
+  \$server > /dev/null &
+fi
 EOF_CAT
 
 cd ${ecosystem}-sdr-transceiver-hpsdr
