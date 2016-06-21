30945bd862cf090fb7a0406c5c7700f3fec75ca8
Pavel Demin <pavel.demin@cern.ch>
2016-06-21 09:11:40 +0200

    update Debian version

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -5,21 +5,21 @@ source /opt/Xilinx/SDK/2016.2/settings64.sh
 
 make NAME=led_blinker all
 
-sudo sh scripts/image.sh scripts/debian.sh red-pitaya-debian-8.4-armhf-$DATE.img
-zip red-pitaya-debian-8.4-armhf-$DATE.zip red-pitaya-debian-8.4-armhf-$DATE.img
+sudo sh scripts/image.sh scripts/debian.sh red-pitaya-debian-8.5-armhf-$DATE.img
+zip red-pitaya-debian-8.5-armhf-$DATE.zip red-pitaya-debian-8.5-armhf-$DATE.img
 
-sudo sh scripts/image.sh scripts/debian-ecosystem.sh red-pitaya-ecosystem-0.95-debian-8.4-armhf-$DATE.img 1024
-zip red-pitaya-ecosystem-0.95-debian-8.4-armhf-$DATE.zip red-pitaya-ecosystem-0.95-debian-8.4-armhf-$DATE.img
+sudo sh scripts/image.sh scripts/debian-ecosystem.sh red-pitaya-ecosystem-0.95-debian-8.5-armhf-$DATE.img 1024
+zip red-pitaya-ecosystem-0.95-debian-8.5-armhf-$DATE.zip red-pitaya-ecosystem-0.95-debian-8.5-armhf-$DATE.img
 
 make NAME=sdr_transceiver_emb all
 
-sudo sh scripts/image.sh scripts/debian-gnuradio.sh red-pitaya-gnuradio-debian-8.4-armhf-$DATE.img 1024
-zip red-pitaya-gnuradio-debian-8.4-armhf-$DATE.zip red-pitaya-gnuradio-debian-8.4-armhf-$DATE.img
+sudo sh scripts/image.sh scripts/debian-gnuradio.sh red-pitaya-gnuradio-debian-8.5-armhf-$DATE.img 1024
+zip red-pitaya-gnuradio-debian-8.5-armhf-$DATE.zip red-pitaya-gnuradio-debian-8.5-armhf-$DATE.img
 
 make NAME=sdr_transceiver_wspr all
 
-sudo sh scripts/image.sh scripts/debian-wspr.sh red-pitaya-wspr-debian-8.4-armhf-$DATE.img 1024
-zip red-pitaya-wspr-debian-8.4-armhf-$DATE.zip red-pitaya-wspr-debian-8.4-armhf-$DATE.img
+sudo sh scripts/image.sh scripts/debian-wspr.sh red-pitaya-wspr-debian-8.5-armhf-$DATE.img 1024
+zip red-pitaya-wspr-debian-8.5-armhf-$DATE.zip red-pitaya-wspr-debian-8.5-armhf-$DATE.img
 
 make NAME=sdr_transceiver tmp/sdr_transceiver.bit
 make NAME=sdr_transceiver_hpsdr tmp/sdr_transceiver_hpsdr.bit
