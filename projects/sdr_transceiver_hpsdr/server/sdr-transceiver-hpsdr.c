6702ab854765afa866e93e1a01a961226bb1e87d
Pavel Demin <pavel.demin@cern.ch>
2016-08-19 22:29:14 +0200

    replace recvfrom with recvmmsg and add CW related parameters

--- projects/sdr_transceiver_hpsdr/server/sdr-transceiver-hpsdr.c
+++ projects/sdr_transceiver_hpsdr/server/sdr-transceiver-hpsdr.c
@@ -143,16 +143,17 @@ void alex_write()
 
 int main(int argc, char *argv[])
 {
-  int fd, i;
-  ssize_t size;
+  int fd, i, j, size;
   pthread_t thread;
   volatile void *cfg, *sts;
   char *name = "/dev/mem";
-  uint8_t buffer[1032];
   uint8_t reply[11] = {0xef, 0xfe, 2, 0, 0, 0, 0, 0, 0, 21, 0};
   struct ifreq hwaddr;
-  struct sockaddr_in addr_ep2, addr_from;
-  socklen_t size_from;
+  struct sockaddr_in addr_ep2, addr_from[10];
+  uint8_t buffer[10][1032];
+  struct iovec iovec[10][1];
+  struct mmsghdr datagram[10];
+  struct timespec timeout;
   int yes = 1;
 
   if((fd = open(name, O_RDWR)) < 0)
@@ -260,64 +261,82 @@ int main(int argc, char *argv[])
 
   while(1)
   {
-    size_from = sizeof(addr_from);
-    size = recvfrom(sock_ep2, buffer, 1032, 0, (struct sockaddr *)&addr_from, &size_from);
+    memset(iovec, 0, sizeof(iovec));
+    memset(datagram, 0, sizeof(datagram));
+
+    for(i = 0; i < 10; ++i)
+    {
+      *(uint32_t *)(buffer[i] + 0) = 0x0601feef;
+      iovec[i][0].iov_base = buffer[i];
+      iovec[i][0].iov_len = 1032;
+      datagram[i].msg_hdr.msg_iov = iovec[i];
+      datagram[i].msg_hdr.msg_iovlen = 1;
+      datagram[i].msg_hdr.msg_name = &addr_from[i];
+      datagram[i].msg_hdr.msg_namelen = sizeof(addr_from[i]);
+    }
+
+    timeout.tv_sec = 0;
+    timeout.tv_nsec = 1000000;
+
+    size = recvmmsg(sock_ep2, datagram, 10, 0, &timeout);
     if(size < 0)
     {
       perror("recvfrom");
       return EXIT_FAILURE;
     }
 
-    switch(*(uint32_t *)buffer)
+    for(i = 0; i < size; ++i)
     {
-      case 0x0201feef:
-        while(*tx_cntr > 16258) usleep(1000);
-        if(*tx_cntr == 0) for(i = 0; i < 16258; ++i) *tx_data = 0;
-        if((*gpio_out & 1) | (*gpio_in & 1))
-        {
-          for(i = 0; i < 504; i += 8) *tx_data = *(uint32_t *)(buffer + 20 + i);
-          for(i = 0; i < 504; i += 8) *tx_data = *(uint32_t *)(buffer + 532 + i);
-        }
-        else
-        {
-          for(i = 0; i < 126; ++i) *tx_data = 0;
-        }
-        for(i = 0; i < 504; i += 8) jack_ringbuffer_write(playback_data, buffer + 16 + i, 4);
-        for(i = 0; i < 504; i += 8) jack_ringbuffer_write(playback_data, buffer + 528 + i, 4);
-        process_ep2(buffer + 11);
-        process_ep2(buffer + 523);
-        break;
-      case 0x0002feef:
-        reply[2] = 2 + active_thread;
-        memset(buffer, 0, 60);
-        memcpy(buffer, reply, 11);
-        sendto(sock_ep2, buffer, 60, 0, (struct sockaddr *)&addr_from, size_from);
-        break;
-      case 0x0004feef:
-        enable_thread = 0;
-        while(active_thread) usleep(1000);
-        break;
-      case 0x0104feef:
-      case 0x0204feef:
-      case 0x0304feef:
-        enable_thread = 0;
-        while(active_thread) usleep(1000);
-        memset(&addr_ep6, 0, sizeof(addr_ep6));
-        addr_ep6.sin_family = AF_INET;
-        addr_ep6.sin_addr.s_addr = addr_from.sin_addr.s_addr;
-        addr_ep6.sin_port = addr_from.sin_port;
-        enable_thread = 1;
-        active_thread = 1;
-        if(pthread_create(&thread, NULL, handler_ep6, NULL) < 0)
-        {
-          perror("pthread_create");
-          return EXIT_FAILURE;
-        }
-        pthread_detach(thread);
-        break;
+      switch(*(uint32_t *)buffer[i])
+      {
+        case 0x0201feef:
+          while(*tx_cntr > 16258) usleep(1000);
+          if(*tx_cntr == 0) for(j = 0; j < 1260; ++j) *tx_data = 0;
+          if((*gpio_out & 1) | (*gpio_in & 1))
+          {
+            for(j = 0; j < 504; j += 8) *tx_data = *(uint32_t *)(buffer[i] + 20 + j);
+            for(j = 0; j < 504; j += 8) *tx_data = *(uint32_t *)(buffer[i] + 532 + j);
+          }
+          else
+          {
+            for(j = 0; j < 126; ++j) *tx_data = 0;
+          }
+          for(j = 0; j < 504; j += 8) jack_ringbuffer_write(playback_data, buffer[i] + 16 + j, 4);
+          for(j = 0; j < 504; j += 8) jack_ringbuffer_write(playback_data, buffer[i] + 528 + j, 4);
+          process_ep2(buffer[i] + 11);
+          process_ep2(buffer[i] + 523);
+          break;
+        case 0x0002feef:
+          reply[2] = 2 + active_thread;
+          memset(buffer[i], 0, 60);
+          memcpy(buffer[i], reply, 11);
+          sendto(sock_ep2, buffer[i], 60, 0, (struct sockaddr *)&addr_from[i], sizeof(addr_from[i]));
+          break;
+        case 0x0004feef:
+          enable_thread = 0;
+          while(active_thread) usleep(1000);
+          break;
+        case 0x0104feef:
+        case 0x0204feef:
+        case 0x0304feef:
+          enable_thread = 0;
+          while(active_thread) usleep(1000);
+          memset(&addr_ep6, 0, sizeof(addr_ep6));
+          addr_ep6.sin_family = AF_INET;
+          addr_ep6.sin_addr.s_addr = addr_from[i].sin_addr.s_addr;
+          addr_ep6.sin_port = addr_from[i].sin_port;
+          enable_thread = 1;
+          active_thread = 1;
+          if(pthread_create(&thread, NULL, handler_ep6, NULL) < 0)
+          {
+            perror("pthread_create");
+            return EXIT_FAILURE;
+          }
+          pthread_detach(thread);
+          break;
+      }
     }
   }
-
   close(sock_ep2);
 
   return EXIT_SUCCESS;
@@ -327,6 +346,10 @@ void process_ep2(uint8_t *frame)
 {
   uint32_t freq;
   uint16_t data;
+  uint8_t drive;
+  uint8_t cw_reversed, cw_speed, cw_mode, cw_weight, cw_spacing;
+  uint8_t cw_internal, cw_volume, cw_delay;
+  uint16_t cw_hang, cw_freq;
 
   switch(frame[0])
   {
@@ -428,6 +451,7 @@ void process_ep2(uint8_t *frame)
       break;
     case 18:
     case 19:
+      drive = frame[1];
       data = (frame[2] & 0x40) << 9 | frame[4] << 8 | frame[3];
       if(alex_data_4 != data)
       {
@@ -446,6 +470,25 @@ void process_ep2(uint8_t *frame)
           i2c_write(i2c_fd, 0x02, data);
         }
       }
+      break;
+    case 22:
+    case 23:
+      cw_reversed = (frame[2] >> 6) & 1;
+      cw_speed = frame[3] & 63;
+      cw_mode = (frame[3] >> 6) & 3;
+      cw_weight = frame[4] & 127;
+      cw_spacing = (frame[4] >> 7) & 1;
+      break;
+    case 30:
+    case 31:
+      cw_internal = frame[1] & 1;
+      cw_volume = frame[2];
+      cw_delay = frame[3];
+      break;
+    case 32:
+    case 33:
+      cw_hang = (frame[1] << 2) | (frame[2] & 3);
+      cw_freq = (frame[3] << 4) | (frame[4] & 15);
       break;
   }
 }
