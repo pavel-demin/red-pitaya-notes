a60c9c3be5c20f9af016fa710d00331ea5661700
Pavel Demin <pavel.demin@cern.ch>
2016-08-26 23:49:51 +0200

    remove S_AXIS from  axis_keyer

--- cores/axis_keyer_v1_0/axis_keyer.v
+++ cores/axis_keyer_v1_0/axis_keyer.v
@@ -3,28 +3,20 @@
 
 module axis_keyer #
 (
-  parameter integer AXIS_TDATA_WIDTH = 32
+  parameter integer AXIS_TDATA_WIDTH = 16
 )
 (
   // System signals
   input  wire                        aclk,
 
   input  wire                        key_flag,
-  input  wire [AXIS_TDATA_WIDTH-1:0] key_data,
-
-  // Slave side
-  output wire                        s_axis_tready,
-  input  wire [AXIS_TDATA_WIDTH-1:0] s_axis_tdata,
-  input  wire                        s_axis_tvalid,
 
   // Master side
-  input  wire                        m_axis_tready,
   output wire [AXIS_TDATA_WIDTH-1:0] m_axis_tdata,
   output wire                        m_axis_tvalid
 );
 
-  assign s_axis_tready =  key_flag | m_axis_tready;
-  assign m_axis_tdata = key_flag ? key_data : s_axis_tdata;
-  assign m_axis_tvalid = key_flag | s_axis_tvalid;
+  assign m_axis_tdata = key_flag ? {1'b0, {(AXIS_TDATA_WIDTH-1){1'b1}}} : {(AXIS_TDATA_WIDTH){1'b0}};
+  assign m_axis_tvalid = 1'b1;
 
 endmodule
