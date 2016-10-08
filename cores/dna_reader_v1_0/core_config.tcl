89fbc1076afd24d663572c0c196c33995515e379
Pavel Demin <pavel.demin@cern.ch>
2016-10-08 17:48:07 +0200

    add missing properties and parameters to dna_reader, gpio_debouncer and pulse_generator

--- cores/dna_reader_v1_0/core_config.tcl
+++ cores/dna_reader_v1_0/core_config.tcl
@@ -1,3 +1,6 @@
 set display_name {DNA Reader}
 
 set core [ipx::current_core]
+
+set_property DISPLAY_NAME $display_name $core
+set_property DESCRIPTION $display_name $core
--- cores/pulse_generator_v1_0/core_config.tcl
+++ cores/pulse_generator_v1_0/core_config.tcl
@@ -1,3 +1,6 @@
 set display_name {Pulse Generator}
 
 set core [ipx::current_core]
+
+set_property DISPLAY_NAME $display_name $core
+set_property DESCRIPTION $display_name $core
