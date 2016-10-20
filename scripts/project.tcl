d0acff94726850215899a3b7f7f9be31576e0a63
Pavel Demin <pavel.demin@cern.ch>
2016-10-20 13:36:13 +0200

    replace Performance_NetDelay_high with Performance_ExtraTimingOpt in project.tcl

--- scripts/project.tcl
+++ scripts/project.tcl
@@ -86,6 +86,6 @@ if {[llength $files] > 0} {
 set_property VERILOG_DEFINE {TOOL_VIVADO} [current_fileset]
 
 set_property STRATEGY Flow_PerfOptimized_High [get_runs synth_1]
-set_property STRATEGY Performance_NetDelay_high [get_runs impl_1]
+set_property STRATEGY Performance_ExtraTimingOpt [get_runs impl_1]
 
 close_project
