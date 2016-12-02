d44b9ba588224d1c78a497f134a6ce4333d77347
Pavel Demin <pavel-demin@outlook.com>
2016-12-02 14:57:02 +0100

    remove cfg_flag from axis_timer

--- cores/axis_timer_v1_0/axis_timer.v
+++ cores/axis_timer_v1_0/axis_timer.v
@@ -7,53 +7,63 @@ module axis_timer #
 )
 (
   // System signals
-  input  wire                        aclk,
-  input  wire                        aresetn,
+  input  wire                  aclk,
+  input  wire                  aresetn,
 
-  input  wire                        run_flag,
-  input  wire                        cfg_flag,
-  input  wire [CNTR_WIDTH-1:0]       cfg_data,
+  input  wire                  run_flag,
+  input  wire [CNTR_WIDTH-1:0] cfg_data,
 
-  output wire                        trg_flag,
-  output wire [CNTR_WIDTH-1:0]       sts_data,
+  output wire                  trg_flag,
+  output wire [CNTR_WIDTH-1:0] sts_data,
 
   // Slave side
-  output wire                        s_axis_tready,
-  input  wire                        s_axis_tvalid
+  output wire                  s_axis_tready,
+  input  wire                  s_axis_tvalid
 );
 
   reg [CNTR_WIDTH-1:0] int_cntr_reg, int_cntr_next;
-  wire int_enbl_wire;
+  reg int_enbl_reg, int_enbl_next;
+
+  wire int_comp_wire;
 
   always @(posedge aclk)
   begin
     if(~aresetn)
     begin
       int_cntr_reg <= {(CNTR_WIDTH){1'b0}};
+      int_enbl_reg <= 1'b0;
     end
     else
     begin
       int_cntr_reg <= int_cntr_next;
+      int_enbl_reg <= int_enbl_next;
     end
   end
 
-  assign int_enbl_wire = run_flag & (int_cntr_reg > {(CNTR_WIDTH){1'b0}});
+  assign int_comp_wire = run_flag & (int_cntr_reg < cfg_data);
 
   always @*
   begin
     int_cntr_next = int_cntr_reg;
+    int_enbl_next = int_enbl_reg;
 
-    if(cfg_flag)
+    if(~int_enbl_reg & int_comp_wire & s_axis_tvalid)
     begin
-      int_cntr_next = cfg_data;
+      int_enbl_next = 1'b1;
     end
-    else if(int_enbl_wire & s_axis_tvalid)
+
+    if(int_enbl_reg & int_comp_wire & s_axis_tvalid)
     begin
-      int_cntr_next = int_cntr_reg - 1'b1;
+      int_cntr_next = int_cntr_reg + 1'b1;
+    end
+
+    if(int_enbl_reg & ~int_comp_wire & s_axis_tvalid)
+    begin
+      int_enbl_next = 1'b0;
     end
   end
 
-  assign trg_flag = int_enbl_wire;
+  assign trg_flag = int_enbl_reg;
   assign sts_data = int_cntr_reg;
 
   assign s_axis_tready = 1'b1;
