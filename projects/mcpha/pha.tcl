dd5f5f5145f959ab80656ff520c13bbd586146d0
Pavel Demin <pavel-demin@outlook.com>
2016-12-02 15:02:28 +0100

    connect pha_2 and pha_3 to fifo_1 in mcpha

--- projects/mcpha/pha.tcl
+++ projects/mcpha/pha.tcl
@@ -26,45 +26,38 @@ cell xilinx.com:ip:xlslice:1.0 slice_3 {
   DIN_WIDTH 8 DIN_FROM 3 DIN_TO 3 DOUT_WIDTH 1
 }
 
-# timer_0/cfg_flag
-
-# Create xlslice
-cell xilinx.com:ip:xlslice:1.0 slice_4 {
-  DIN_WIDTH 8 DIN_FROM 4 DIN_TO 4 DOUT_WIDTH 1
-}
-
 # timer_0/cfg_data
 
 # Create xlslice
-cell xilinx.com:ip:xlslice:1.0 slice_5 {
+cell xilinx.com:ip:xlslice:1.0 slice_4 {
   DIN_WIDTH 128 DIN_FROM 63 DIN_TO 0 DOUT_WIDTH 64
 }
 
 # pha_0/bln_data
 
 # Create xlslice
-cell xilinx.com:ip:xlslice:1.0 slice_6 {
+cell xilinx.com:ip:xlslice:1.0 slice_5 {
   DIN_WIDTH 128 DIN_FROM 79 DIN_TO 64 DOUT_WIDTH 16
 }
 
 # pha_0/cfg_data
 
 # Create xlslice
-cell xilinx.com:ip:xlslice:1.0 slice_7 {
+cell xilinx.com:ip:xlslice:1.0 slice_6 {
   DIN_WIDTH 128 DIN_FROM 95 DIN_TO 80 DOUT_WIDTH 16
 }
 
 # pha_0/min_data
 
 # Create xlslice
-cell xilinx.com:ip:xlslice:1.0 slice_8 {
+cell xilinx.com:ip:xlslice:1.0 slice_7 {
   DIN_WIDTH 128 DIN_FROM 111 DIN_TO 96 DOUT_WIDTH 16
 }
 
 # pha_0/max_data
 
 # Create xlslice
-cell xilinx.com:ip:xlslice:1.0 slice_9 {
+cell xilinx.com:ip:xlslice:1.0 slice_8 {
   DIN_WIDTH 128 DIN_FROM 127 DIN_TO 112 DOUT_WIDTH 16
 }
 
@@ -73,8 +66,7 @@ cell pavel-demin:user:axis_timer:1.0 timer_0 {
   CNTR_WIDTH 64
 } {
   run_flag slice_3/Dout
-  cfg_flag slice_4/Dout
-  cfg_data slice_5/Dout
+  cfg_data slice_4/Dout
   aclk /ps_0/FCLK_CLK0
   aresetn slice_1/Dout
 }
@@ -85,10 +77,10 @@ cell pavel-demin:user:axis_pulse_height_analyzer:1.0 pha_0 {
   CNTR_WIDTH 16
 } {
   bln_flag slice_2/Dout
-  bln_data slice_6/Dout
-  cfg_data slice_7/Dout
-  min_data slice_8/Dout
-  max_data slice_9/Dout
+  bln_data slice_5/Dout
+  cfg_data slice_6/Dout
+  min_data slice_7/Dout
+  max_data slice_8/Dout
   aclk /ps_0/FCLK_CLK0
   aresetn slice_0/Dout
 }
