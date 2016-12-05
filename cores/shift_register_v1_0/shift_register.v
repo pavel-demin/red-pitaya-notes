9cde0cd9f08080ee5035d68015772cdc6dcbf801
Pavel Demin <pavel-demin@outlook.com>
2016-12-05 08:31:36 +0100

    add shift_register

---
+++ cores/shift_register_v1_0/shift_register.v
@@ -0,0 +1,26 @@
+
+`timescale 1 ns / 1 ps
+
+module shift_register #
+(
+  parameter integer DATA_WIDTH = 8
+)
+(
+  input  wire                  aclk,
+
+  inout  wire [DATA_WIDTH-1:0] din,
+
+  output wire [DATA_WIDTH-1:0] dout
+);
+
+  reg [DATA_WIDTH-1:0] int_data_reg [1:0];
+
+  always @(posedge aclk)
+  begin
+    int_data_reg[0] <= din;
+    int_data_reg[1] <= int_data_reg[0];
+  end
+
+  assign dout = int_data_reg[1];
+
+endmodule
