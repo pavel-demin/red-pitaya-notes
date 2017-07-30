f73ce727949f25aaa6242e031288734fc086d7f8
Pavel Demin <pavel-demin@outlook.com>
2017-07-30 19:16:27 +0200

    remove FCLK_CLK0 from sdr_transceiver_wspr

--- projects/sdr_transceiver_wspr/tx.tcl
+++ projects/sdr_transceiver_wspr/tx.tcl
@@ -7,7 +7,7 @@ cell xilinx.com:ip:xlslice:1.0 slice_0 {
 cell pavel-demin:user:axi_axis_writer:1.0 writer_0 {
   AXI_DATA_WIDTH 32
 } {
-  aclk /ps_0/FCLK_CLK0
+  aclk /pll_0/clk_out1
   aresetn /rst_0/peripheral_aresetn
 }
 
@@ -18,7 +18,7 @@ cell xilinx.com:ip:axis_data_fifo:1.1 fifo_0 {
   FIFO_DEPTH 256
 } {
   S_AXIS writer_0/M_AXIS
-  s_axis_aclk /ps_0/FCLK_CLK0
+  s_axis_aclk /pll_0/clk_out1
   s_axis_aresetn slice_0/Dout
 }
 
@@ -35,7 +35,7 @@ cell pavel-demin:user:axis_interpolator:1.0 inter_0 {
 } {
   S_AXIS fifo_0/M_AXIS
   cfg_data const_0/dout
-  aclk /ps_0/FCLK_CLK0
+  aclk /pll_0/clk_out1
   aresetn slice_0/Dout
 }
 
@@ -45,7 +45,6 @@ cell xilinx.com:ip:dds_compiler:6.0 dds_0 {
   SPURIOUS_FREE_DYNAMIC_RANGE 84
   FREQUENCY_RESOLUTION 0.2
   PHASE_INCREMENT Streaming
-  HAS_TREADY true
   HAS_ARESETN true
   HAS_PHASE_OUT false
   PHASE_WIDTH 30
@@ -54,7 +53,7 @@ cell xilinx.com:ip:dds_compiler:6.0 dds_0 {
   OUTPUT_SELECTION Sine
 } {
   S_AXIS_PHASE inter_0/M_AXIS
-  aclk /ps_0/FCLK_CLK0
+  aclk /pll_0/clk_out1
   aresetn slice_0/Dout
 }
 
@@ -62,7 +61,7 @@ cell xilinx.com:ip:dds_compiler:6.0 dds_0 {
 cell pavel-demin:user:axis_constant:1.0 zero_0 {
   AXIS_TDATA_WIDTH 16
 } {
-  aclk /ps_0/FCLK_CLK0
+  aclk /pll_0/clk_out1
 }
 
 # Create axis_switch
@@ -75,7 +74,7 @@ cell xilinx.com:ip:axis_switch:1.1 switch_0 {
 } {
   S00_AXIS dds_0/M_AXIS_DATA
   S01_AXIS zero_0/M_AXIS
-  aclk /ps_0/FCLK_CLK0
+  aclk /pll_0/clk_out1
   aresetn /rst_0/peripheral_aresetn
 }
 
@@ -86,16 +85,6 @@ cell  xilinx.com:ip:axis_combiner:1.1 comb_0 {
 } {
   S00_AXIS switch_0/M00_AXIS
   S01_AXIS switch_0/M01_AXIS
-  aclk /ps_0/FCLK_CLK0
+  aclk /pll_0/clk_out1
   aresetn /rst_0/peripheral_aresetn
-}
-
-# Create axis_clock_converter
-cell xilinx.com:ip:axis_clock_converter:1.1 fifo_1 {
-  TDATA_NUM_BYTES.VALUE_SRC USER
-  TDATA_NUM_BYTES 4
-} {
-  S_AXIS comb_0/M_AXIS
-  s_axis_aclk /ps_0/FCLK_CLK0
-  s_axis_aresetn slice_0/Dout
 }
