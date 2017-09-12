32c9db65b55f7962607c3c2d23e1d2c000f6a789
Pavel Demin <pavel-demin@outlook.com>
2017-09-12 21:10:11 +0200

    add *-bazaar.sh to build-all.sh

--- helpers/build-all.sh
+++ helpers/build-all.sh
@@ -2,9 +2,15 @@ source /opt/Xilinx/Vivado/2016.4/settings64.sh
 
 make NAME=led_blinker all
 
-for project in sdr_receiver_hpsdr sdr_transceiver sdr_transceiver_hpsdr sdr_transceiver_wspr mcpha vna
+for project in sdr_receiver_hpsdr sdr_transceiver sdr_transceiver_hpsdr sdr_transceiver_wspr mcpha pulsed_nmr scanner vna
 do
   make NAME=$project bit
 done
 
 sudo sh scripts/alpine.sh
+
+source helpers/sdr-receiver-hpsdr-bazaar.sh
+source helpers/sdr-transceiver-bazaar.sh
+source helpers/sdr-transceiver-hpsdr-bazaar.sh
+source helpers/mcpha-bazaar.sh
+source helpers/vna-bazaar.sh
