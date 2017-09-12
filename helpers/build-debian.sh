668078c66245ed2bb2ba509e9c85e62f9760c6a4
Pavel Demin <pavel-demin@outlook.com>
2017-09-12 21:10:54 +0200

    add build-debian.sh

---
+++ helpers/build-debian.sh
@@ -0,0 +1,21 @@
+DATE=`date +%Y%m%d`
+
+source /opt/Xilinx/Vivado/2016.4/settings64.sh
+
+make NAME=led_blinker all
+
+sudo sh scripts/image.sh scripts/debian.sh red-pitaya-debian-8.8-armhf-$DATE.img 1024
+zip red-pitaya-debian-8.8-armhf-$DATE.zip red-pitaya-debian-8.8-armhf-$DATE.img
+
+sudo sh scripts/image.sh scripts/debian-ecosystem.sh red-pitaya-ecosystem-0.95-debian-8.8-armhf-$DATE.img 1024
+zip red-pitaya-ecosystem-0.95-debian-8.8-armhf-$DATE.zip red-pitaya-ecosystem-0.95-debian-8.8-armhf-$DATE.img
+
+make NAME=sdr_transceiver_emb all
+
+sudo sh scripts/image.sh scripts/debian-gnuradio.sh red-pitaya-gnuradio-debian-8.8-armhf-$DATE.img 2048
+zip red-pitaya-gnuradio-debian-8.8-armhf-$DATE.zip red-pitaya-gnuradio-debian-8.8-armhf-$DATE.img
+
+make NAME=sdr_transceiver_wspr all
+
+sudo sh scripts/image.sh scripts/debian-wspr.sh red-pitaya-wspr-debian-9.0-armhf-$DATE.img 1024
+zip red-pitaya-wspr-debian-9.0-armhf-$DATE.zip red-pitaya-wspr-debian-9.0-armhf-$DATE.img
