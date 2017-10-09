718ee4afcbbccc8333835b55abf0d95bfcd2bea0
Pavel Demin <pavel-demin@outlook.com>
2017-10-09 14:01:49 +0200

    move bazaar scripts from build-all.sh to build-bazaar.sh

--- helpers/build-all.sh
+++ helpers/build-all.sh
@@ -8,9 +8,3 @@ do
 done
 
 sudo sh scripts/alpine.sh
-
-source helpers/sdr-receiver-hpsdr-bazaar.sh
-source helpers/sdr-transceiver-bazaar.sh
-source helpers/sdr-transceiver-hpsdr-bazaar.sh
-source helpers/mcpha-bazaar.sh
-source helpers/vna-bazaar.sh
