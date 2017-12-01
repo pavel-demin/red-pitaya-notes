f3f7f4853be4cd64cb699d7033f9741d4d59e656
Pavel Demin <pavel-demin@outlook.com>
2017-12-01 08:50:43 +0100

    replace int32 with uint32 in vna.m

--- projects/vna/matlab/vna.m
+++ projects/vna/matlab/vna.m
@@ -3,36 +3,36 @@ t = tcpclient('192.168.1.100', 1001);
 
 % set start frequency in Hz
 start = 100;
-write(t, int32(0 * 2^28 + start));
+write(t, uint32(0 * 2^28 + start));
 
 % set stop frequency in Hz
 stop = 100000;
-write(t, int32(1 * 2^28 + stop));
+write(t, uint32(1 * 2^28 + stop));
 
 % set sweep size
 size = 1000;
-write(t, int32(2 * 2^28 + size));
+write(t, uint32(2 * 2^28 + size));
 
 % set decimation rate
 rate = 1000;
-write(t, int32(3 * 2^28 + rate));
+write(t, uint32(3 * 2^28 + rate));
 
 % set frequency correction
 corr = 0;
-write(t, int32(4 * 2^28 + corr));
+write(t, uint32(4 * 2^28 + corr));
 
 % set output levels
 level = 32766;
-write(t, int32(5 * 2^28 + level));
+write(t, uint32(5 * 2^28 + level));
 level = 0;
-write(t, int32(6 * 2^28 + level));
+write(t, uint32(6 * 2^28 + level));
 
 % set gpio pins
 gpio = 1;
-write(t, int32(7 * 2^28 + gpio));
+write(t, uint32(7 * 2^28 + gpio));
 
 % sweep
-write(t, int32(8 * 2^28));
+write(t, uint32(8 * 2^28));
 
 % read data
 data = read(t, 4 * size, 'single');
