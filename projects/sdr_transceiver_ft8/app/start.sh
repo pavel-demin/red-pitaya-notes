62eb38fb24416fffed12744932d44489efdcd5fd
Pavel Demin <pavel-demin@outlook.com>
2018-03-31 11:23:22 +0200

    add app to sdr_transceiver_ft8

---
+++ projects/sdr_transceiver_ft8/app/start.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+apps_dir=/media/mmcblk0p1/apps
+
+source $apps_dir/stop.sh
+
+cat $apps_dir/sdr_transceiver_ft8/sdr_transceiver_ft8.bit > /dev/xdevcfg
+
+ln -sf $apps_dir/sdr_transceiver_ft8/ft8.cron /etc/periodic/ft8
+
+service dcron restart
