f7c1b261197efb71b0e2361f38a87d42d946fb89
Pavel Demin <pavel-demin@outlook.com>
2018-10-20 22:45:34 +0000

    cleanup /dev/shm before starting sdr_transceiver_ft8 and sdr_transceiver_wspr

--- projects/sdr_transceiver_ft8/app/start.sh
+++ projects/sdr_transceiver_ft8/app/start.sh
@@ -4,6 +4,8 @@ apps_dir=/media/mmcblk0p1/apps
 
 source $apps_dir/stop.sh
 
+rm -rf /dev/shm/*
+
 cat $apps_dir/sdr_transceiver_ft8/sdr_transceiver_ft8.bit > /dev/xdevcfg
 
 ln -sf $apps_dir/sdr_transceiver_ft8/ft8.cron /etc/periodic/ft8
--- projects/sdr_transceiver_wspr/app/start.sh
+++ projects/sdr_transceiver_wspr/app/start.sh
@@ -4,6 +4,8 @@ apps_dir=/media/mmcblk0p1/apps
 
 source $apps_dir/stop.sh
 
+rm -rf /dev/shm/*
+
 cat $apps_dir/sdr_transceiver_wspr/sdr_transceiver_wspr.bit > /dev/xdevcfg
 
 ln -sf $apps_dir/sdr_transceiver_wspr/wspr.cron /etc/periodic/wspr
