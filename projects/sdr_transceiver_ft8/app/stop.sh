0ddf293b91f35f4064be0684185131be9e718a34
Pavel Demin <pavel-demin@outlook.com>
2019-02-23 10:24:50 +0000

    fix sdr_transceiver_ft8/app/stop.sh

--- projects/sdr_transceiver_ft8/app/stop.sh
+++ projects/sdr_transceiver_ft8/app/stop.sh
@@ -1,7 +1,8 @@
 #! /bin/sh
 
-rm -f /etc/periodic/ft8
-
-service dcron restart
-
-killall -q decode-ft8.sh write-c2-files ft8d
+if test -e /etc/periodic/ft8
+then
+  rm -f /etc/periodic/ft8
+  service dcron restart
+  killall -q decode-ft8.sh write-c2-files ft8d
+fi
