52ce46e8039f442d9cd2f19e6c74f6523d5526ea
Pavel Demin <pavel-demin@outlook.com>
2019-03-07 20:49:04 +0000

    add sleep-to-59 and measure-corr to stop scripts

--- projects/sdr_transceiver_ft8/app/stop.sh
+++ projects/sdr_transceiver_ft8/app/stop.sh
@@ -4,5 +4,6 @@ if test -e /etc/periodic/ft8
 then
   rm -f /etc/periodic/ft8
   service dcron restart
-  killall -q decode-ft8.sh write-c2-files ft8d
+  killall -q decode-ft8.sh sleep-to-59 write-c2-files ft8d
+  killall -q update-corr.sh measure-corr
 fi
