bfefaaf3725ab558aff7992089b7ad4d1cfd7e8f
Pavel Demin <pavel-demin@outlook.com>
2019-03-18 15:35:47 +0000

    fix mcpha/filters/fir_0.r

--- projects/mcpha/filters/fir_0.r
+++ projects/mcpha/filters/fir_0.r
@@ -4,12 +4,12 @@ a0 <- 0.35875
 a1 <- 0.48829
 a2 <- 0.14128
 a3 <- 0.01168
-x <- seq(0.0, 1.0, by=1/40)
+x <- seq(0.0, 1.0, by = 1 / 40)
 h <- a0 - a1 * cos(2 * pi * x) + a2 * cos(4 * pi * x) - a3 * cos(6 * pi * x)
-h <- B/sum(B)
+h <- h / sum(h)
 
 # Print filter coefficients
-paste(sprintf("%.10e", h), collapse=", ")
+paste(sprintf("%.10e", h), collapse = ", ")
 
 fh <- freqz(h)
 
@@ -18,6 +18,5 @@ op <- par(mfrow = c(1, 2))
 plot(fh$f / pi, abs(fh$h), type = "l", ylab = "magnitude", xlab = "Frequency", xlim = c(0, 0.2))
 
 plot(fh$f / pi, 20*log10(abs(fh$h)+1e-5), type = "l", ylab = "dB", xlab = "Frequency", xlim = c(0, 0.2))
-
 
 par(op)
