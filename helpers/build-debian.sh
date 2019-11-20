b571899b80515a63277ba39ef991e112fe522153
Pavel Demin <pavel-demin@outlook.com>
2019-11-20 22:51:14 +0000

    replace 9.9 with 9.11 in build-debian.sh

--- helpers/build-debian.sh
+++ helpers/build-debian.sh
@@ -4,13 +4,13 @@ source /opt/Xilinx/Vitis/2019.2/settings64.sh
 
 make NAME=led_blinker all
 
-sudo sh scripts/image.sh scripts/debian.sh red-pitaya-debian-9.9-armhf-$DATE.img 1024
-zip red-pitaya-debian-9.9-armhf-$DATE.zip red-pitaya-debian-9.9-armhf-$DATE.img
+sudo sh scripts/image.sh scripts/debian.sh red-pitaya-debian-9.11-armhf-$DATE.img 1024
+zip red-pitaya-debian-9.11-armhf-$DATE.zip red-pitaya-debian-9.11-armhf-$DATE.img
 
-sudo sh scripts/image.sh scripts/debian-ecosystem.sh red-pitaya-ecosystem-0.95-debian-9.9-armhf-$DATE.img 1024
-zip red-pitaya-ecosystem-0.95-debian-9.9-armhf-$DATE.zip red-pitaya-ecosystem-0.95-debian-9.9-armhf-$DATE.img
+sudo sh scripts/image.sh scripts/debian-ecosystem.sh red-pitaya-ecosystem-0.95-debian-9.11-armhf-$DATE.img 1024
+zip red-pitaya-ecosystem-0.95-debian-9.11-armhf-$DATE.zip red-pitaya-ecosystem-0.95-debian-9.11-armhf-$DATE.img
 
 make NAME=sdr_transceiver_emb all
 
-sudo sh scripts/image.sh scripts/debian-gnuradio.sh red-pitaya-gnuradio-debian-9.9-armhf-$DATE.img 2048
-zip red-pitaya-gnuradio-debian-9.9-armhf-$DATE.zip red-pitaya-gnuradio-debian-9.9-armhf-$DATE.img
+sudo sh scripts/image.sh scripts/debian-gnuradio.sh red-pitaya-gnuradio-debian-9.11-armhf-$DATE.img 2048
+zip red-pitaya-gnuradio-debian-9.11-armhf-$DATE.zip red-pitaya-gnuradio-debian-9.11-armhf-$DATE.img
