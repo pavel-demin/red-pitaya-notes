03901416811ce89798ed75e8efa06feb4a39463e
Pavel Demin <pavel-demin@outlook.com>
2020-04-19 13:12:00 +0000

    add gate_test

---
+++ projects/gate_test/block_design.tcl
@@ -0,0 +1,281 @@
+# Create clk_wiz
+cell xilinx.com:ip:clk_wiz pll_0 {
+  PRIMITIVE PLL
+  PRIM_IN_FREQ.VALUE_SRC USER
+  PRIM_IN_FREQ 125.0
+  PRIM_SOURCE Differential_clock_capable_pin
+  CLKOUT1_USED true
+  CLKOUT1_REQUESTED_OUT_FREQ 125.0
+  USE_RESET false
+} {
+  clk_in1_p adc_clk_p_i
+  clk_in1_n adc_clk_n_i
+}
+
+# Create processing_system7
+cell xilinx.com:ip:processing_system7 ps_0 {
+  PCW_IMPORT_BOARD_PRESET cfg/red_pitaya.xml
+} {
+  M_AXI_GP0_ACLK /pll_0/clk_out1
+}
+
+# Create all required interconnections
+apply_bd_automation -rule xilinx.com:bd_rule:processing_system7 -config {
+  make_external {FIXED_IO, DDR}
+  Master Disable
+  Slave Disable
+} [get_bd_cells ps_0]
+
+# Create xlconstant
+cell xilinx.com:ip:xlconstant const_0
+
+# Create proc_sys_reset
+cell xilinx.com:ip:proc_sys_reset rst_0 {} {
+  ext_reset_in const_0/dout
+}
+
+# CFG
+
+# Create axi_cfg_register
+cell pavel-demin:user:axi_cfg_register cfg_0 {
+  CFG_DATA_WIDTH 64
+  AXI_ADDR_WIDTH 32
+  AXI_DATA_WIDTH 32
+}
+
+# Create port_slicer
+cell pavel-demin:user:port_slicer slice_0 {
+  DIN_WIDTH 64 DIN_FROM 0 DIN_TO 0
+} {
+  din cfg_0/cfg_data
+}
+
+# Create port_slicer
+cell pavel-demin:user:port_slicer slice_1 {
+  DIN_WIDTH 64 DIN_FROM 1 DIN_TO 1
+} {
+  din cfg_0/cfg_data
+}
+
+# Create port_slicer
+cell pavel-demin:user:port_slicer slice_2 {
+  DIN_WIDTH 64 DIN_FROM 2 DIN_TO 2
+} {
+  din cfg_0/cfg_data
+}
+
+# Create port_slicer
+cell pavel-demin:user:port_slicer slice_3 {
+  DIN_WIDTH 64 DIN_FROM 63 DIN_TO 32
+} {
+  din cfg_0/cfg_data
+}
+
+# FIFO
+
+# Create axi_axis_writer
+cell pavel-demin:user:axi_axis_writer writer_0 {
+  AXI_DATA_WIDTH 32
+} {
+  aclk /pll_0/clk_out1
+  aresetn /rst_0/peripheral_aresetn
+}
+
+# Create fifo_generator
+cell xilinx.com:ip:fifo_generator fifo_generator_0 {
+  PERFORMANCE_OPTIONS First_Word_Fall_Through
+  INPUT_DATA_WIDTH 32
+  INPUT_DEPTH 1024
+  OUTPUT_DATA_WIDTH 128
+  OUTPUT_DEPTH 256
+} {
+  clk /pll_0/clk_out1
+  srst slice_0/dout
+}
+
+# Create axis_fifo
+cell pavel-demin:user:axis_fifo fifo_0 {
+  S_AXIS_TDATA_WIDTH 32
+  M_AXIS_TDATA_WIDTH 128
+} {
+  S_AXIS writer_0/M_AXIS
+  FIFO_READ fifo_generator_0/FIFO_READ
+  FIFO_WRITE fifo_generator_0/FIFO_WRITE
+  aclk /pll_0/clk_out1
+}
+
+# GATE
+
+# Create axis_gate_controller
+cell pavel-demin:user:axis_gate_controller gate_0 {} {
+  S_AXIS fifo_0/M_AXIS
+  aclk /pll_0/clk_out1
+  aresetn slice_1/dout
+}
+
+# Create xlconcat
+cell xilinx.com:ip:xlconcat concat_0 {
+  NUM_PORTS 2
+  IN0_WIDTH 32
+  IN1_WIDTH 32
+} {
+  In0 slice_3/dout
+  In1 gate_0/poff
+}
+
+# Create axis_constant
+cell pavel-demin:user:axis_constant phase_0 {
+  AXIS_TDATA_WIDTH 64
+} {
+  cfg_data concat_0/dout
+  aclk /pll_0/clk_out1
+}
+
+# Create dds_compiler
+cell xilinx.com:ip:dds_compiler dds_0 {
+  DDS_CLOCK_RATE 125
+  SPURIOUS_FREE_DYNAMIC_RANGE 138
+  FREQUENCY_RESOLUTION 0.2
+  PHASE_INCREMENT Streaming
+  PHASE_OFFSET Streaming
+  HAS_ARESETN true
+  HAS_PHASE_OUT false
+  PHASE_WIDTH 30
+  OUTPUT_WIDTH 24
+  DSP48_USE Minimal
+  OUTPUT_SELECTION Sine
+} {
+  S_AXIS_PHASE phase_0/M_AXIS
+  aclk /pll_0/clk_out1
+  aresetn slice_1/dout
+}
+
+# Create axis_lfsr
+cell pavel-demin:user:axis_lfsr lfsr_0 {} {
+  aclk /pll_0/clk_out1
+  aresetn /rst_0/peripheral_aresetn
+}
+
+# Create xbip_dsp48_macro
+cell xilinx.com:ip:xbip_dsp48_macro mult_0 {
+  INSTRUCTION1 RNDSIMPLE(A*B+CARRYIN)
+  A_WIDTH.VALUE_SRC USER
+  B_WIDTH.VALUE_SRC USER
+  OUTPUT_PROPERTIES User_Defined
+  A_WIDTH 24
+  B_WIDTH 16
+  P_WIDTH 16
+} {
+  A dds_0/m_axis_data_tdata
+  B gate_0/level
+  CARRYIN lfsr_0/m_axis_tdata
+  CLK /pll_0/clk_out1
+}
+
+# Create c_shift_ram
+cell xilinx.com:ip:c_shift_ram delay_0 {
+  WIDTH.VALUE_SRC USER
+  WIDTH 1
+  DEPTH 15
+} {
+  D gate_0/dout
+  CLK /pll_0/clk_out1
+}
+
+# Create xlconcat
+cell xilinx.com:ip:xlconcat concat_1 {
+  NUM_PORTS 3
+  IN0_WIDTH 16
+  IN1_WIDTH 8
+  IN2_WIDTH 8
+} {
+  In0 mult_0/P
+  In1 delay_0/Q
+  In2 gate_0/dout
+}
+
+# Create axis_constant
+cell pavel-demin:user:axis_constant output_0 {
+  AXIS_TDATA_WIDTH 32
+} {
+  cfg_data concat_1/dout
+  aclk /pll_0/clk_out1
+}
+
+# FIFO
+
+# Create axis_data_fifo
+cell xilinx.com:ip:axis_data_fifo fifo_1 {
+  TDATA_NUM_BYTES.VALUE_SRC USER
+  TDATA_NUM_BYTES 4
+  FIFO_DEPTH 1024
+  HAS_RD_DATA_COUNT true
+} {
+  S_AXIS output_0/M_AXIS
+  s_axis_aclk /pll_0/clk_out1
+  s_axis_aresetn slice_2/dout
+}
+
+# Create axi_axis_reader
+cell pavel-demin:user:axi_axis_reader reader_0 {
+  AXI_DATA_WIDTH 32
+} {
+  S_AXIS fifo_1/M_AXIS
+  aclk /pll_0/clk_out1
+  aresetn /rst_0/peripheral_aresetn
+}
+
+# STS
+
+# Create xlconcat
+cell xilinx.com:ip:xlconcat concat_2 {
+  NUM_PORTS 1
+  IN0_WIDTH 32
+} {
+  In0 fifo_1/axis_rd_data_count
+}
+
+# Create axi_sts_register
+cell pavel-demin:user:axi_sts_register sts_0 {
+  STS_DATA_WIDTH 32
+  AXI_ADDR_WIDTH 32
+  AXI_DATA_WIDTH 32
+} {
+  sts_data concat_2/dout
+}
+
+# Create all required interconnections
+apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {
+  Master /ps_0/M_AXI_GP0
+  Clk Auto
+} [get_bd_intf_pins sts_0/S_AXI]
+
+set_property RANGE 4K [get_bd_addr_segs ps_0/Data/SEG_sts_0_reg0]
+set_property OFFSET 0x40000000 [get_bd_addr_segs ps_0/Data/SEG_sts_0_reg0]
+
+# Create all required interconnections
+apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {
+  Master /ps_0/M_AXI_GP0
+  Clk Auto
+} [get_bd_intf_pins cfg_0/S_AXI]
+
+set_property RANGE 4K [get_bd_addr_segs ps_0/Data/SEG_cfg_0_reg0]
+set_property OFFSET 0x40001000 [get_bd_addr_segs ps_0/Data/SEG_cfg_0_reg0]
+
+# Create all required interconnections
+apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {
+  Master /ps_0/M_AXI_GP0
+  Clk Auto
+} [get_bd_intf_pins writer_0/S_AXI]
+
+set_property RANGE 4K [get_bd_addr_segs ps_0/Data/SEG_writer_0_reg0]
+set_property OFFSET 0x40002000 [get_bd_addr_segs ps_0/Data/SEG_writer_0_reg0]
+
+# Create all required interconnections
+apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {
+  Master /ps_0/M_AXI_GP0
+  Clk Auto
+} [get_bd_intf_pins reader_0/S_AXI]
+
+set_property RANGE 4K [get_bd_addr_segs ps_0/Data/SEG_reader_0_reg0]
+set_property OFFSET 0x40003000 [get_bd_addr_segs ps_0/Data/SEG_reader_0_reg0]
