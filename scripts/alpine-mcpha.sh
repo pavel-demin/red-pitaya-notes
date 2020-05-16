e763c0750d103da72cd14e41d2309aec2a28970e
Pavel Demin <pavel-demin@outlook.com>
2020-05-16 19:01:59 +0000

    switch to Alpine 3.11

--- scripts/alpine-mcpha.sh
+++ scripts/alpine-mcpha.sh
@@ -1,16 +1,16 @@
-alpine_url=http://dl-cdn.alpinelinux.org/alpine/v3.9
+alpine_url=http://dl-cdn.alpinelinux.org/alpine/v3.11
 
-uboot_tar=alpine-uboot-3.9.0-armv7.tar.gz
+uboot_tar=alpine-uboot-3.11.0-armv7.tar.gz
 uboot_url=$alpine_url/releases/armv7/$uboot_tar
 
-tools_tar=apk-tools-static-2.10.3-r1.apk
+tools_tar=apk-tools-static-2.10.5-r0.apk
 tools_url=$alpine_url/main/armv7/$tools_tar
 
-firmware_tar=linux-firmware-other-20190322-r0.apk
+firmware_tar=linux-firmware-other-20191215-r0.apk
 firmware_url=$alpine_url/main/armv7/$firmware_tar
 
 linux_dir=tmp/linux-4.19
-linux_ver=4.19.84-xilinx
+linux_ver=4.19.123-xilinx
 
 modules_dir=alpine-modloop/lib/modules/$linux_ver
 
@@ -21,7 +21,7 @@ test -f $tools_tar || curl -L $tools_url -o $tools_tar
 
 test -f $firmware_tar || curl -L $firmware_url -o $firmware_tar
 
-for tar in linux-firmware-ath9k_htc-20190322-r0.apk linux-firmware-brcm-20190322-r0.apk linux-firmware-rtlwifi-20190322-r0.apk
+for tar in linux-firmware-ath9k_htc-20191215-r0.apk linux-firmware-brcm-20191215-r0.apk linux-firmware-rtlwifi-20191215-r0.apk
 do
   url=$alpine_url/main/armv7/$tar
   test -f $tar || curl -L $url -o $tar
@@ -36,7 +36,7 @@ tar -zxf $tools_tar --directory=alpine-apk --warning=no-unknown-keyword
 mkdir alpine-initramfs
 cd alpine-initramfs
 
-gzip -dc ../alpine-uboot/boot/initramfs-vanilla | cpio -id
+gzip -dc ../alpine-uboot/boot/initramfs-lts | cpio -id
 rm -rf etc/modprobe.d
 rm -rf lib/firmware
 rm -rf lib/modules
@@ -57,7 +57,7 @@ depmod -a -b alpine-modloop $linux_ver
 
 tar -zxf $firmware_tar --directory=alpine-modloop/lib/modules --warning=no-unknown-keyword --strip-components=1 --wildcards lib/firmware/ar* lib/firmware/rt*
 
-for tar in linux-firmware-ath9k_htc-20190322-r0.apk linux-firmware-brcm-20190322-r0.apk linux-firmware-rtlwifi-20190322-r0.apk
+for tar in linux-firmware-ath9k_htc-20191215-r0.apk linux-firmware-brcm-20191215-r0.apk linux-firmware-rtlwifi-20191215-r0.apk
 do
   tar -zxf $tar --directory=alpine-modloop/lib/modules --warning=no-unknown-keyword --strip-components=1
 done
@@ -183,6 +183,6 @@ hostname -F /etc/hostname
 
 rm -rf $root_dir alpine-apk
 
-zip -r red-pitaya-alpine-3.9-armv7-`date +%Y%m%d`-mcpha.zip apps boot.bin cache devicetree.dtb modloop red-pitaya.apkovl.tar.gz start.sh uEnv.txt uImage uInitrd wifi
+zip -r red-pitaya-alpine-3.11-armv7-`date +%Y%m%d`-mcpha.zip apps boot.bin cache devicetree.dtb modloop red-pitaya.apkovl.tar.gz start.sh uEnv.txt uImage uInitrd wifi
 
 rm -rf apps cache modloop red-pitaya.apkovl.tar.gz start.sh uInitrd wifi
