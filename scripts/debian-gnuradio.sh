72be62a83bcb2ee55e1d19e28421f7028bf6cfd6
Pavel Demin <pavel-demin@outlook.com>
2020-05-16 18:56:41 +0000

    set Linux version to 4.19.123

--- scripts/debian-gnuradio.sh
+++ scripts/debian-gnuradio.sh
@@ -4,7 +4,7 @@ boot_dir=`mktemp -d /tmp/BOOT.XXXXXXXXXX`
 root_dir=`mktemp -d /tmp/ROOT.XXXXXXXXXX`
 
 linux_dir=tmp/linux-4.19
-linux_ver=4.19.84-xilinx
+linux_ver=4.19.123-xilinx
 
 # Choose mirror automatically, depending the geographic and network location
 mirror=http://deb.debian.org/debian
