321e9f719b35251d51342d8ce59d6d8ef79e6745
Pavel Demin <pavel-demin@outlook.com>
2021-04-01 18:16:59 +0000

    replace HP0 with ACP in base_system and writer_test

--- projects/base_system/block_design.tcl
+++ projects/base_system/block_design.tcl
@@ -15,10 +15,11 @@ cell xilinx.com:ip:clk_wiz pll_0 {
 # Create processing_system7
 cell xilinx.com:ip:processing_system7 ps_0 {
   PCW_IMPORT_BOARD_PRESET cfg/red_pitaya.xml
-  PCW_USE_S_AXI_HP0 1
+  PCW_USE_S_AXI_ACP 1
+  PCW_USE_DEFAULT_ACP_USER_VAL 1
 } {
   M_AXI_GP0_ACLK pll_0/clk_out1
-  S_AXI_HP0_ACLK pll_0/clk_out1
+  S_AXI_ACP_ACLK pll_0/clk_out1
 }
 
 # Create all required interconnections
--- projects/writer_test/block_design.tcl
+++ projects/writer_test/block_design.tcl
@@ -21,9 +21,16 @@ cell pavel-demin:user:port_slicer slice_4 {
   din cfg_0/cfg_data
 }
 
+# Create port_slicer
+cell pavel-demin:user:port_slicer slice_5 {
+  DIN_WIDTH 1024 DIN_FROM 95 DIN_TO 64
+} {
+  din cfg_0/cfg_data
+}
+
 # Create axis_counter
 cell pavel-demin:user:axis_counter cntr_1 {} {
-  cfg_data slice_4/dout
+  cfg_data slice_5/dout
   aclk pll_0/clk_out1
   aresetn slice_2/dout
 }
@@ -39,19 +46,16 @@ cell xilinx.com:ip:axis_dwidth_converter conv_0 {
   aresetn slice_3/dout
 }
 
-# Create xlconstant
-cell xilinx.com:ip:xlconstant const_1 {
-  CONST_WIDTH 32
-  CONST_VAL 503316480
-}
-
 # Create axis_ram_writer
-cell pavel-demin:user:axis_ram_writer writer_0 {} {
+cell pavel-demin:user:axis_ram_writer writer_0 {
+  ADDR_WIDTH 20
+  AXI_ID_WIDTH 3
+} {
   S_AXIS conv_0/M_AXIS
-  M_AXI ps_0/S_AXI_HP0
-  cfg_data const_1/dout
+  M_AXI ps_0/S_AXI_ACP
+  cfg_data slice_4/dout
   aclk pll_0/clk_out1
   aresetn slice_3/dout
 }
 
-assign_bd_address [get_bd_addr_segs ps_0/S_AXI_HP0/HP0_DDR_LOWOCM]
+assign_bd_address [get_bd_addr_segs ps_0/S_AXI_ACP/ACP_DDR_LOWOCM]
