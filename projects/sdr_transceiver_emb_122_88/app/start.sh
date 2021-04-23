0fb792e94560e7d69498f904a6afa98afee4b580
Pavel Demin <pavel-demin@outlook.com>
2021-04-23 16:35:59 +0000

    update wdsp-fftw-wisdom.txt in sdr_transceiver_emb_122_88

--- projects/sdr_transceiver_emb_122_88/app/start.sh
+++ projects/sdr_transceiver_emb_122_88/app/start.sh
@@ -6,9 +6,13 @@ source $apps_dir/stop.sh
 
 cat $apps_dir/sdr_transceiver_emb_122_88/sdr_transceiver_emb_122_88.bit > /dev/xdevcfg
 
+cd $apps_dir/sdr_transceiver_emb_122_88
+
 if aplay --list-devices | grep 'card 0:'
 then
   arecord -fFLOAT_LE -c2 -r48000 | $apps_dir/sdr_transceiver_emb_122_88/sdr-transceiver-emb | aplay -fFLOAT_LE -c2 -r48000 &
 else
   cat /dev/zero | $apps_dir/sdr_transceiver_emb_122_88/sdr-transceiver-emb > /dev/null &
 fi
+
+cd -
