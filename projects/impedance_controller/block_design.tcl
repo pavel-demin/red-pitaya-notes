698cb107510b293dbaf5c217df6f235cf26e9e80
Pavel Demin <pavel-demin@outlook.com>
2021-04-23 10:35:30 +0000

    add impedance_controller

---
+++ projects/impedance_controller/block_design.tcl
@@ -0,0 +1,444 @@
+# Create clk_wiz
+cell xilinx.com:ip:clk_wiz pll_0 {
+  PRIMITIVE PLL
+  PRIM_IN_FREQ.VALUE_SRC USER
+  PRIM_IN_FREQ 125.0
+  PRIM_SOURCE Differential_clock_capable_pin
+  CLKOUT1_USED true
+  CLKOUT1_REQUESTED_OUT_FREQ 125.0
+  CLKOUT2_USED true
+  CLKOUT2_REQUESTED_OUT_FREQ 250.0
+  CLKOUT2_REQUESTED_PHASE 157.5
+  CLKOUT3_USED true
+  CLKOUT3_REQUESTED_OUT_FREQ 250.0
+  CLKOUT3_REQUESTED_PHASE 202.5
+  USE_RESET false
+} {
+  clk_in1_p adc_clk_p_i
+  clk_in1_n adc_clk_n_i
+}
+
+# Create processing_system7
+cell xilinx.com:ip:processing_system7 ps_0 {
+  PCW_IMPORT_BOARD_PRESET cfg/red_pitaya.xml
+} {
+  M_AXI_GP0_ACLK pll_0/clk_out1
+}
+
+# Create all required interconnections
+apply_bd_automation -rule xilinx.com:bd_rule:processing_system7 -config {
+  make_external {FIXED_IO, DDR}
+  Master Disable
+  Slave Disable
+} [get_bd_cells ps_0]
+
+# Create xlconstant
+cell xilinx.com:ip:xlconstant const_0
+
+# Create proc_sys_reset
+cell xilinx.com:ip:proc_sys_reset rst_0 {} {
+  ext_reset_in const_0/dout
+}
+
+# ADC
+
+# Create axis_red_pitaya_adc
+cell pavel-demin:user:axis_red_pitaya_adc adc_0 {
+  ADC_DATA_WIDTH 14
+} {
+  aclk pll_0/clk_out1
+  adc_dat_a adc_dat_a_i
+  adc_dat_b adc_dat_b_i
+  adc_csn adc_csn_o
+}
+
+# DAC
+
+# Create axis_red_pitaya_dac
+cell pavel-demin:user:axis_red_pitaya_dac dac_0 {
+  DAC_DATA_WIDTH 14
+} {
+  aclk pll_0/clk_out1
+  ddr_clk pll_0/clk_out2
+  wrt_clk pll_0/clk_out3
+  locked pll_0/locked
+  dac_clk dac_clk_o
+  dac_rst dac_rst_o
+  dac_sel dac_sel_o
+  dac_wrt dac_wrt_o
+  dac_dat dac_dat_o
+}
+
+# GPIO
+
+# Delete input/output port
+delete_bd_objs [get_bd_ports exp_p_tri_io]
+
+# Create output port
+create_bd_port -dir O -from 7 -to 0 exp_p_tri_io
+
+# CFG
+
+# Create axi_cfg_register
+cell pavel-demin:user:axi_cfg_register cfg_0 {
+  CFG_DATA_WIDTH 96
+  AXI_ADDR_WIDTH 32
+  AXI_DATA_WIDTH 32
+}
+
+# Create port_slicer
+cell pavel-demin:user:port_slicer slice_0 {
+  DIN_WIDTH 96 DIN_FROM 0 DIN_TO 0
+} {
+  din cfg_0/cfg_data
+}
+
+# Create port_slicer
+cell pavel-demin:user:port_slicer slice_1 {
+  DIN_WIDTH 96 DIN_FROM 1 DIN_TO 1
+} {
+  din cfg_0/cfg_data
+}
+
+# Create port_slicer
+cell pavel-demin:user:port_slicer slice_2 {
+  DIN_WIDTH 96 DIN_FROM 15 DIN_TO 8
+} {
+  din cfg_0/cfg_data
+  dout exp_p_tri_io
+}
+
+# Create port_slicer
+cell pavel-demin:user:port_slicer slice_3 {
+  DIN_WIDTH 96 DIN_FROM 31 DIN_TO 16
+} {
+  din cfg_0/cfg_data
+}
+
+# Create port_slicer
+cell pavel-demin:user:port_slicer slice_4 {
+  DIN_WIDTH 96 DIN_FROM 63 DIN_TO 32
+} {
+  din cfg_0/cfg_data
+}
+
+# Create port_slicer
+cell pavel-demin:user:port_slicer slice_5 {
+  DIN_WIDTH 96 DIN_FROM 79 DIN_TO 64
+} {
+  din cfg_0/cfg_data
+}
+
+# DDS
+
+# Create axis_constant
+cell pavel-demin:user:axis_constant phase_0 {
+  AXIS_TDATA_WIDTH 32
+} {
+  cfg_data slice_4/dout
+  aclk pll_0/clk_out1
+}
+
+# Create dds_compiler
+cell xilinx.com:ip:dds_compiler dds_0 {
+  DDS_CLOCK_RATE 125
+  SPURIOUS_FREE_DYNAMIC_RANGE 138
+  FREQUENCY_RESOLUTION 0.2
+  PHASE_INCREMENT Streaming
+  HAS_ARESETN true
+  HAS_PHASE_OUT false
+  PHASE_WIDTH 30
+  OUTPUT_WIDTH 24
+  DSP48_USE Minimal
+  NEGATIVE_SINE true
+} {
+  S_AXIS_PHASE phase_0/M_AXIS
+  aclk pll_0/clk_out1
+  aresetn slice_0/dout
+}
+
+# Create axis_constant
+cell pavel-demin:user:axis_constant phase_1 {
+  AXIS_TDATA_WIDTH 32
+} {
+  cfg_data slice_4/dout
+  aclk pll_0/clk_out1
+}
+
+# Create dds_compiler
+cell xilinx.com:ip:dds_compiler dds_1 {
+  DDS_CLOCK_RATE 125
+  SPURIOUS_FREE_DYNAMIC_RANGE 138
+  FREQUENCY_RESOLUTION 0.2
+  PHASE_INCREMENT Streaming
+  HAS_ARESETN true
+  HAS_PHASE_OUT false
+  PHASE_WIDTH 30
+  OUTPUT_WIDTH 24
+  DSP48_USE Minimal
+  OUTPUT_SELECTION Sine
+} {
+  S_AXIS_PHASE phase_1/M_AXIS
+  aclk pll_0/clk_out1
+  aresetn slice_0/dout
+}
+
+# TX
+
+# Create axis_lfsr
+cell pavel-demin:user:axis_lfsr lfsr_0 {} {
+  aclk pll_0/clk_out1
+  aresetn slice_0/dout
+}
+
+# Create xbip_dsp48_macro
+cell xilinx.com:ip:xbip_dsp48_macro mult_4 {
+  INSTRUCTION1 RNDSIMPLE(A*B+CARRYIN)
+  A_WIDTH.VALUE_SRC USER
+  B_WIDTH.VALUE_SRC USER
+  OUTPUT_PROPERTIES User_Defined
+  A_WIDTH 24
+  B_WIDTH 16
+  P_WIDTH 15
+} {
+  A dds_1/m_axis_data_tdata
+  B slice_5/dout
+  CARRYIN lfsr_0/m_axis_tdata
+  CLK pll_0/clk_out1
+}
+
+# Create c_shift_ram
+cell xilinx.com:ip:c_shift_ram delay_0 {
+  WIDTH.VALUE_SRC USER
+  WIDTH 1
+  DEPTH 4
+} {
+  D dds_1/m_axis_data_tvalid
+  CLK pll_0/clk_out1
+}
+
+# Create axis_zeroer
+cell pavel-demin:user:axis_zeroer zeroer_0 {
+  AXIS_TDATA_WIDTH 32
+} {
+  s_axis_tdata mult_4/P
+  s_axis_tvalid delay_0/Q
+  M_AXIS dac_0/S_AXIS
+  aclk pll_0/clk_out1
+}
+
+# RX
+
+for {set i 0} {$i <= 1} {incr i} {
+
+  # Create port_slicer
+  cell pavel-demin:user:port_slicer dds_slice_$i {
+    DIN_WIDTH 48 DIN_FROM [expr 24 * $i + 23] DIN_TO [expr 24 * $i]
+  } {
+    din dds_0/m_axis_data_tdata
+  }
+
+  # Create port_slicer
+  cell pavel-demin:user:port_slicer adc_slice_$i {
+    DIN_WIDTH 32 DIN_FROM [expr 16 * $i + 13] DIN_TO [expr 16 * $i]
+  } {
+    din adc_0/m_axis_tdata
+  }
+
+}
+
+for {set i 0} {$i <= 3} {incr i} {
+
+  # Create xbip_dsp48_macro
+  cell xilinx.com:ip:xbip_dsp48_macro mult_$i {
+    INSTRUCTION1 RNDSIMPLE(A*B+CARRYIN)
+    A_WIDTH.VALUE_SRC USER
+    B_WIDTH.VALUE_SRC USER
+    OUTPUT_PROPERTIES User_Defined
+    A_WIDTH 24
+    B_WIDTH 14
+    P_WIDTH 25
+  } {
+    A dds_slice_[expr $i % 2]/dout
+    B adc_slice_[expr $i / 2]/dout
+    CARRYIN lfsr_0/m_axis_tdata
+    CLK pll_0/clk_out1
+  }
+
+  # Create axis_variable
+  cell pavel-demin:user:axis_variable rate_$i {
+    AXIS_TDATA_WIDTH 16
+  } {
+    cfg_data slice_3/dout
+    aclk pll_0/clk_out1
+    aresetn slice_0/dout
+  }
+
+  # Create cic_compiler
+  cell xilinx.com:ip:cic_compiler cic_$i {
+    INPUT_DATA_WIDTH.VALUE_SRC USER
+    FILTER_TYPE Decimation
+    NUMBER_OF_STAGES 6
+    SAMPLE_RATE_CHANGES Programmable
+    MINIMUM_RATE 4
+    MAXIMUM_RATE 8192
+    FIXED_OR_INITIAL_RATE 4
+    INPUT_SAMPLE_FREQUENCY 125
+    CLOCK_FREQUENCY 125
+    INPUT_DATA_WIDTH 24
+    QUANTIZATION Truncation
+    OUTPUT_DATA_WIDTH 32
+    USE_XTREME_DSP_SLICE false
+    HAS_ARESETN true
+  } {
+    s_axis_data_tdata mult_$i/P
+    s_axis_data_tvalid const_0/dout
+    S_AXIS_CONFIG rate_$i/M_AXIS
+    aclk pll_0/clk_out1
+    aresetn slice_0/dout
+  }
+
+}
+
+# Create axis_combiner
+cell  xilinx.com:ip:axis_combiner comb_0 {
+  TDATA_NUM_BYTES.VALUE_SRC USER
+  TDATA_NUM_BYTES 4
+  NUM_SI 4
+} {
+  S00_AXIS cic_3/M_AXIS_DATA
+  S01_AXIS cic_2/M_AXIS_DATA
+  S02_AXIS cic_1/M_AXIS_DATA
+  S03_AXIS cic_0/M_AXIS_DATA
+  aclk pll_0/clk_out1
+  aresetn slice_0/dout
+}
+
+# Create axis_dwidth_converter
+cell xilinx.com:ip:axis_dwidth_converter conv_0 {
+  S_TDATA_NUM_BYTES.VALUE_SRC USER
+  S_TDATA_NUM_BYTES 16
+  M_TDATA_NUM_BYTES 4
+} {
+  S_AXIS comb_0/M_AXIS
+  aclk pll_0/clk_out1
+  aresetn slice_0/dout
+}
+
+# Create cic_compiler
+cell xilinx.com:ip:cic_compiler cic_4 {
+  INPUT_DATA_WIDTH.VALUE_SRC USER
+  FILTER_TYPE Decimation
+  NUMBER_OF_STAGES 6
+  SAMPLE_RATE_CHANGES Fixed
+  FIXED_OR_INITIAL_RATE 3125
+  INPUT_SAMPLE_FREQUENCY 31.25
+  CLOCK_FREQUENCY 125
+  NUMBER_OF_CHANNELS 4
+  INPUT_DATA_WIDTH 32
+  QUANTIZATION Truncation
+  OUTPUT_DATA_WIDTH 32
+  USE_XTREME_DSP_SLICE false
+  HAS_ARESETN true
+} {
+  S_AXIS_DATA conv_0/M_AXIS
+  aclk pll_0/clk_out1
+  aresetn slice_0/dout
+}
+
+# Create floating_point
+cell xilinx.com:ip:floating_point fp_0 {
+  OPERATION_TYPE Fixed_to_float
+  A_PRECISION_TYPE.VALUE_SRC USER
+  C_A_EXPONENT_WIDTH.VALUE_SRC USER
+  C_A_FRACTION_WIDTH.VALUE_SRC USER
+  A_PRECISION_TYPE Custom
+  C_A_EXPONENT_WIDTH 2
+  C_A_FRACTION_WIDTH 30
+  RESULT_PRECISION_TYPE Single
+  HAS_ARESETN true
+} {
+  S_AXIS_A cic_4/M_AXIS_DATA
+  aclk pll_0/clk_out1
+  aresetn slice_0/dout
+}
+
+# Create axis_dwidth_converter
+cell xilinx.com:ip:axis_dwidth_converter conv_1 {
+  S_TDATA_NUM_BYTES.VALUE_SRC USER
+  S_TDATA_NUM_BYTES 4
+  M_TDATA_NUM_BYTES 16
+} {
+  S_AXIS fp_0/M_AXIS_RESULT
+  aclk pll_0/clk_out1
+  aresetn slice_0/dout
+}
+
+# Create fifo_generator
+cell xilinx.com:ip:fifo_generator fifo_generator_0 {
+  PERFORMANCE_OPTIONS First_Word_Fall_Through
+  INPUT_DATA_WIDTH 128
+  INPUT_DEPTH 256
+  OUTPUT_DATA_WIDTH 32
+  OUTPUT_DEPTH 1024
+  READ_DATA_COUNT true
+  READ_DATA_COUNT_WIDTH 11
+} {
+  clk pll_0/clk_out1
+  srst slice_1/dout
+}
+
+# Create axis_fifo
+cell pavel-demin:user:axis_fifo fifo_0 {
+  S_AXIS_TDATA_WIDTH 128
+  M_AXIS_TDATA_WIDTH 32
+} {
+  S_AXIS conv_1/M_AXIS
+  FIFO_READ fifo_generator_0/FIFO_READ
+  FIFO_WRITE fifo_generator_0/FIFO_WRITE
+  aclk pll_0/clk_out1
+}
+
+# Create axi_axis_reader
+cell pavel-demin:user:axi_axis_reader reader_0 {
+  AXI_DATA_WIDTH 32
+} {
+  S_AXIS fifo_0/M_AXIS
+  aclk pll_0/clk_out1
+  aresetn rst_0/peripheral_aresetn
+}
+
+# STS
+
+# Create dna_reader
+cell pavel-demin:user:dna_reader dna_0 {} {
+  aclk pll_0/clk_out1
+  aresetn rst_0/peripheral_aresetn
+}
+
+# Create xlconcat
+cell xilinx.com:ip:xlconcat concat_3 {
+  NUM_PORTS 3
+  IN0_WIDTH 32
+  IN1_WIDTH 64
+  IN2_WIDTH 16
+} {
+  In0 const_0/dout
+  In1 dna_0/dna_data
+  In2 fifo_generator_0/rd_data_count
+}
+
+# Create axi_sts_register
+cell pavel-demin:user:axi_sts_register sts_0 {
+  STS_DATA_WIDTH 128
+  AXI_ADDR_WIDTH 32
+  AXI_DATA_WIDTH 32
+} {
+  sts_data concat_3/dout
+}
+
+addr 0x40000000 4K sts_0/S_AXI /ps_0/M_AXI_GP0
+
+addr 0x40001000 4K cfg_0/S_AXI /ps_0/M_AXI_GP0
+
+addr 0x40002000 4K reader_0/S_AXI /ps_0/M_AXI_GP0
---
+++ projects/impedance_controller/server/impedance-controller.c
@@ -0,0 +1,188 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <math.h>
+#include <pthread.h>
+#include <sys/mman.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+volatile uint16_t *cntr;
+volatile float *data;
+
+int sock_thread = -1;
+
+void *read_handler(void *arg);
+
+int main(int argc, char *argv[])
+{
+  int fd, sock_server, sock_client;
+  struct sched_param param;
+  pthread_attr_t attr;
+  pthread_t thread;
+  volatile void *cfg, *sts;
+  volatile uint32_t *freq;
+  volatile uint16_t *rate;
+  volatile int16_t *level;
+  volatile uint8_t *rst, *gpio;
+  struct sockaddr_in addr;
+  uint32_t command;
+  int32_t value;
+  int yes = 1;
+
+  if((fd = open("/dev/mem", O_RDWR)) < 0)
+  {
+    perror("open");
+    return EXIT_FAILURE;
+  }
+
+  sts = mmap(NULL, sysconf(_SC_PAGESIZE), PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0x40000000);
+  cfg = mmap(NULL, sysconf(_SC_PAGESIZE), PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0x40001000);
+  data = mmap(NULL, sysconf(_SC_PAGESIZE), PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0x40002000);
+
+  cntr = ((uint16_t *)(sts + 12));
+
+  rst = ((uint8_t *)(cfg + 0));
+  gpio = ((uint8_t *)(cfg + 1));
+  rate = ((uint16_t *)(cfg + 2));
+  freq = ((uint32_t *)(cfg + 4));
+  level = ((int16_t *)(cfg + 8));
+
+  *rate = 4;
+  *freq = (uint32_t)floor(100000 / 125.0e6 * (1<<30) + 0.5);
+
+  *level = 0;
+
+  *rst &= ~1;
+  *rst |= 2;
+  *gpio = 0;
+
+  if((sock_server = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+  {
+    perror("socket");
+    return EXIT_FAILURE;
+  }
+
+  setsockopt(sock_server, SOL_SOCKET, SO_REUSEADDR, (void *)&yes , sizeof(yes));
+
+  /* setup listening address */
+  memset(&addr, 0, sizeof(addr));
+  addr.sin_family = AF_INET;
+  addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  addr.sin_port = htons(1001);
+
+  if(bind(sock_server, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+  {
+    perror("bind");
+    return EXIT_FAILURE;
+  }
+
+  listen(sock_server, 1024);
+
+  pthread_attr_init(&attr);
+  pthread_attr_setinheritsched(&attr, PTHREAD_EXPLICIT_SCHED);
+  pthread_attr_setschedpolicy(&attr, SCHED_FIFO);
+  param.sched_priority = 99;
+  pthread_attr_setschedparam(&attr, &param);
+
+  while(1)
+  {
+    if((sock_client = accept(sock_server, NULL, NULL)) < 0)
+    {
+      perror("accept");
+      return EXIT_FAILURE;
+    }
+
+    while(1)
+    {
+      if(recv(sock_client, (char *)&command, 4, MSG_WAITALL) <= 0) break;
+      value = command & 0xfffffff;
+      value |= (value & 0x8000000) ? 0xf0000000 : 0;
+      switch(command >> 28)
+      {
+        case 0:
+          /* set sample rate */
+          if(value < 4 || value > 8192) continue;
+          *rate = value;
+          break;
+        case 1:
+          /* set frequency */
+          if(value < 0 || value > 62500000) continue;
+          *freq = (uint32_t)floor(value / 125.0e6 * (1<<30) + 0.5);
+          break;
+        case 2:
+          /* set level */
+          if(value < -32766 || value > 32766) continue;
+          *level = value;
+          break;
+        case 3:
+          /* set gpio */
+          if(value < 0 || value > 255) continue;
+          *gpio = value;
+          break;
+        case 4:
+          /* start */
+          if(sock_thread >= 0) continue;
+          /* reset */
+          *rst &= ~1;
+          *rst |= 2;
+          sock_thread = sock_client;
+          if(pthread_create(&thread, &attr, read_handler, NULL) < 0)
+          {
+            perror("pthread_create");
+            return EXIT_FAILURE;
+          }
+          pthread_detach(thread);
+          *rst &= ~2;
+          *rst |= 1;
+          break;
+        case 5:
+          /* stop */
+          sock_thread = -1;
+          *rst &= ~1;
+          *rst |= 2;
+          break;
+      }
+    }
+
+    sock_thread = -1;
+    *rst &= ~1;
+    *rst |= 2;
+    *gpio = 0;
+    close(sock_client);
+  }
+
+  close(sock_server);
+
+  return EXIT_SUCCESS;
+}
+
+void *read_handler(void *arg)
+{
+  int i;
+  float buffer[16];
+
+  while(1)
+  {
+    if(sock_thread < 0) break;
+
+    if(*cntr < 16)
+    {
+      usleep(1000);
+      continue;
+    }
+
+    for(i = 0; i < 16; ++i)
+    {
+      buffer[i] = *data;
+    }
+
+    if(send(sock_thread, buffer, 64, MSG_NOSIGNAL) < 0) break;
+  }
+
+  return NULL;
+}
