9764ac404f1c9929bc32c8311f06db9d805d324a
Pavel Demin <pavel-demin@outlook.com>
2021-05-01 12:09:39 +0000

    replace arp_filter with arp_announce and arp_ignore

--- projects/sdr_receiver_hpsdr_122_88/app/start.sh
+++ projects/sdr_receiver_hpsdr_122_88/app/start.sh
@@ -7,9 +7,12 @@ source $apps_dir/stop.sh
 cat $apps_dir/sdr_receiver_hpsdr_122_88/sdr_receiver_hpsdr_122_88.bit > /dev/xdevcfg
 
 address=`awk -F : '$5="FF"' OFS=: /sys/class/net/eth0/address`
-ip link add mvl0 link eth0 address $address type macvlan mode passthru
-echo 1 > /proc/sys/net/ipv4/conf/all/arp_filter
+
+echo 2 > /proc/sys/net/ipv4/conf/all/arp_announce
+echo 1 > /proc/sys/net/ipv4/conf/all/arp_ignore
 echo 2 > /proc/sys/net/ipv4/conf/all/rp_filter
+
+ip link add mvl0 link eth0 address $address type macvlan mode passthru
 
 $apps_dir/sdr_receiver_hpsdr_122_88/sdr-receiver-hpsdr eth0 1 1 1 1 1 1 1 1 &
 $apps_dir/sdr_receiver_hpsdr_122_88/sdr-receiver-hpsdr mvl0 1 1 1 1 1 1 1 1 &
--- projects/sdr_receiver_hpsdr_122_88/app/stop.sh
+++ projects/sdr_receiver_hpsdr_122_88/app/stop.sh
@@ -2,6 +2,8 @@
 
 killall -q sdr-receiver-hpsdr
 
-echo 0 > /proc/sys/net/ipv4/conf/all/arp_filter
-echo 1 > /proc/sys/net/ipv4/conf/all/rp_filter
 ip link del mvl0
+
+echo 0 > /proc/sys/net/ipv4/conf/all/arp_announce
+echo 0 > /proc/sys/net/ipv4/conf/all/arp_ignore
+echo 1 > /proc/sys/net/ipv4/conf/all/rp_filter
