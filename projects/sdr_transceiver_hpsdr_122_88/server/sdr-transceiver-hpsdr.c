b4c4b21bb05978f2527c0d16d852d9901ad03c58
Pavel Demin <pavel-demin@outlook.com>
2021-06-12 19:08:17 +0000

    update CW PPT for NUCLEO-G071RB

--- projects/sdr_transceiver_hpsdr_122_88/server/sdr-transceiver-hpsdr.c
+++ projects/sdr_transceiver_hpsdr_122_88/server/sdr-transceiver-hpsdr.c
@@ -350,7 +350,7 @@ void nucleo_write()
     code[i] = code[i] % 2 ? code[i] / 2 + 1 : 0;
   }
 
-  data[0] = nucleo_data_3 << 25 | nucleo_data_2 << 20 | nucleo_data_1 << 17 | nucleo_data_0 << 1 | cw_ptt << 1 | (code[0] != code[1]);
+  data[0] = nucleo_data_3 << 25 | nucleo_data_2 << 20 | nucleo_data_1 << 17 | nucleo_data_0 << 1 | (code[0] != code[1]);
   data[1] = code[2] << 4 | code[1];
 
   if(i2c_nucleo_data[0] != data[0] || i2c_nucleo_data[1] != data[1])
@@ -854,6 +854,13 @@ int main(int argc, char *argv[])
       misc_update = 0;
       misc_write();
     };
+
+    data = nucleo_data_0 | cw_ptt;
+    if(nucleo_data_0 != data)
+    {
+      nucleo_data_0 = data;
+      nucleo_update = 1;
+    }
 
     if(nucleo_update)
     {
