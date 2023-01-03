c05471bc766a894139734a8206555546734bdf12
Pavel Demin <pavel-demin@outlook.com>
2023-01-03 21:58:21 +0100

    add DSP48E1 to axis_iir_filter

--- cores/axis_iir_filter_v1_0/axis_iir_filter.v
+++ cores/axis_iir_filter_v1_0/axis_iir_filter.v
@@ -7,19 +7,7 @@ module axis_iir_filter
   input  wire        aclk,
   input  wire        aresetn,
 
-  input  wire [31:0] cfg_data,
-
-  // DSP signals
-  output wire [15:0] dsp_a_a,
-  input  wire [32:0] dsp_a_p,
-
-  output wire [24:0] dsp_b_a,
-  output wire [41:0] dsp_b_c,
-  input  wire [41:0] dsp_b_p,
-
-  output wire [24:0] dsp_c_a,
-  output wire [41:0] dsp_c_c,
-  input  wire [41:0] dsp_c_p,
+  input  wire [79:0] cfg_data,
 
   // Slave side
   output wire        s_axis_tready,
@@ -32,29 +20,56 @@ module axis_iir_filter
   output wire        m_axis_tvalid
 );
 
-  reg [41:0] int_data_reg[1:0];
+  wire [47:0] int_p_wire [2:0];
 
-  always @(posedge aclk)
-  begin
-    if(~aresetn)
-    begin
-      int_data_reg[0] <= 42'd0;
-      int_data_reg[1] <= 42'd0;
-    end
-    else
-    begin
-      int_data_reg[0] <= dsp_b_p;
-      int_data_reg[1] <= dsp_c_p;
-    end
-  end
+  DSP48E1 #(
+    .ALUMODEREG(0), .CARRYINSELREG(0), .INMODEREG(0), .OPMODEREG(0),
+    .AREG(0), .ACASCREG(0), .BREG(0), .BCASCREG(0),
+    .CREG(0), .CARRYINREG(0), .MREG(1), .PREG(1)
+  ) dsp_0 (
+    .CLK(aclk),
+    .RSTM(~aresetn),
+    .RSTP(~aresetn),
+    .CEM(1'b1),
+    .CEP(1'b1),
+    .OPMODE(7'b0000101),
+    .A({{(5){s_axis_tdata[15]}}, s_axis_tdata, 9'd0}),
+    .B(cfg_data[15:0]),
+    .P(int_p_wire[0])
+  );
 
-  assign dsp_a_a = s_axis_tdata;
-  assign dsp_b_a = int_data_reg[0][39:15];
-  assign dsp_b_c = {dsp_a_p, 9'd0};
-  assign dsp_c_a = int_data_reg[1][39:15];
-  assign dsp_c_c = int_data_reg[0];
+  DSP48E1 #(
+    .ALUMODEREG(0), .CARRYINSELREG(0), .INMODEREG(0), .OPMODEREG(0),
+    .AREG(0), .ACASCREG(0), .BREG(0), .BCASCREG(0),
+    .CREG(0), .CARRYINREG(0), .MREG(0), .PREG(1)
+  ) dsp_1 (
+    .CLK(aclk),
+    .RSTP(~aresetn),
+    .CEP(1'b1),
+    .OPMODE(7'b0110101),
+    .A(int_p_wire[1][45:16]),
+    .B(cfg_data[31:16]),
+    .C(int_p_wire[0]),
+    .P(int_p_wire[1])
+  );
+
+  DSP48E1 #(
+    .ALUMODEREG(0), .CARRYINSELREG(0), .INMODEREG(0), .OPMODEREG(0),
+    .AREG(0), .ACASCREG(0), .BREG(0), .BCASCREG(0),
+    .CREG(0), .CARRYINREG(0), .MREG(0), .PREG(1)
+  ) dsp_2 (
+    .CLK(aclk),
+    .RSTP(~aresetn),
+    .CEP(1'b1),
+    .OPMODE(7'b0110101),
+    .A(int_p_wire[2][45:16]),
+    .B(cfg_data[47:32]),
+    .C(int_p_wire[1]),
+    .P(int_p_wire[2])
+  );
+
   assign s_axis_tready = m_axis_tready;
-  assign m_axis_tdata = $signed(int_data_reg[1][41:23]) < $signed(cfg_data[15:0]) ? cfg_data[15:0] : $signed(int_data_reg[1][41:23]) > $signed(cfg_data[31:16]) ? cfg_data[31:16] : int_data_reg[1][38:23];
+  assign m_axis_tdata = $signed(int_p_wire[2][47:25]) < $signed(cfg_data[63:48]) ? cfg_data[63:48] : $signed(int_p_wire[2][47:25]) > $signed(cfg_data[79:64]) ? cfg_data[79:64] : int_p_wire[2][40:25];
   assign m_axis_tvalid = s_axis_tvalid;
 
 endmodule
