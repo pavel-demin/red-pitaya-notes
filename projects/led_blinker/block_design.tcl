0cc17b9740de3a66c2b90d0d99e4d428d41b4264
Pavel Demin <pavel-demin@outlook.com>
2023-01-15 19:22:10 +0100

    simplify led_blinker projects

--- projects/led_blinker/block_design.tcl
+++ projects/led_blinker/block_design.tcl
@@ -1,12 +1,30 @@
-source projects/base_system/block_design.tcl
-
-# Create xlconstant
-cell xilinx.com:ip:xlconstant const_0
-
-# Create proc_sys_reset
-cell xilinx.com:ip:proc_sys_reset rst_0 {} {
-  ext_reset_in const_0/dout
+# Create clk_wiz
+cell xilinx.com:ip:clk_wiz pll_0 {
+  PRIMITIVE PLL
+  PRIM_IN_FREQ.VALUE_SRC USER
+  PRIM_IN_FREQ 125.0
+  PRIM_SOURCE Differential_clock_capable_pin
+  CLKOUT1_USED true
+  CLKOUT1_REQUESTED_OUT_FREQ 125.0
+  USE_RESET false
+} {
+  clk_in1_p adc_clk_p_i
+  clk_in1_n adc_clk_n_i
 }
+
+# Create processing_system7
+cell xilinx.com:ip:processing_system7 ps_0 {
+  PCW_IMPORT_BOARD_PRESET cfg/red_pitaya.xml
+} {
+  M_AXI_GP0_ACLK pll_0/clk_out1
+}
+
+# Create all required interconnections
+apply_bd_automation -rule xilinx.com:bd_rule:processing_system7 -config {
+  make_external {FIXED_IO, DDR}
+  Master Disable
+  Slave Disable
+} [get_bd_cells ps_0]
 
 # LED
 
@@ -24,32 +42,3 @@ cell pavel-demin:user:port_slicer slice_0 {
   din cntr_0/Q
   dout led_o
 }
-
-# STS
-
-# Create dna_reader
-cell pavel-demin:user:dna_reader dna_0 {} {
-  aclk pll_0/clk_out1
-  aresetn rst_0/peripheral_aresetn
-}
-
-# Create xlconcat
-cell xilinx.com:ip:xlconcat concat_0 {
-  NUM_PORTS 2
-  IN0_WIDTH 32
-  IN1_WIDTH 64
-} {
-  In0 const_0/dout
-  In1 dna_0/dna_data
-}
-
-# Create axi_sts_register
-cell pavel-demin:user:axi_sts_register sts_0 {
-  STS_DATA_WIDTH 96
-  AXI_ADDR_WIDTH 32
-  AXI_DATA_WIDTH 32
-} {
-  sts_data concat_0/dout
-}
-
-addr 0x40000000 4K sts_0/S_AXI /ps_0/M_AXI_GP0
--- projects/led_blinker_122_88/block_design.tcl
+++ projects/led_blinker_122_88/block_design.tcl
@@ -26,14 +26,6 @@ apply_bd_automation -rule xilinx.com:bd_rule:processing_system7 -config {
   Slave Disable
 } [get_bd_cells ps_0]
 
-# Create xlconstant
-cell xilinx.com:ip:xlconstant const_0
-
-# Create proc_sys_reset
-cell xilinx.com:ip:proc_sys_reset rst_0 {} {
-  ext_reset_in const_0/dout
-}
-
 # LED
 
 # Create c_counter_binary
@@ -50,32 +42,3 @@ cell pavel-demin:user:port_slicer slice_0 {
   din cntr_0/Q
   dout led_o
 }
-
-# STS
-
-# Create dna_reader
-cell pavel-demin:user:dna_reader dna_0 {} {
-  aclk pll_0/clk_out1
-  aresetn rst_0/peripheral_aresetn
-}
-
-# Create xlconcat
-cell xilinx.com:ip:xlconcat concat_0 {
-  NUM_PORTS 2
-  IN0_WIDTH 32
-  IN1_WIDTH 64
-} {
-  In0 const_0/dout
-  In1 dna_0/dna_data
-}
-
-# Create axi_sts_register
-cell pavel-demin:user:axi_sts_register sts_0 {
-  STS_DATA_WIDTH 96
-  AXI_ADDR_WIDTH 32
-  AXI_DATA_WIDTH 32
-} {
-  sts_data concat_0/dout
-}
-
-addr 0x40000000 4K sts_0/S_AXI /ps_0/M_AXI_GP0
