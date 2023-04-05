80fd0125666d90301041d08d184e9900711afb48
Pavel Demin <pavel-demin@outlook.com>
2023-04-05 16:04:47 +0200

    adapt scanner to axi_hub

--- projects/scanner/server/scanner.c
+++ projects/scanner/server/scanner.c
@@ -15,14 +15,13 @@ int main()
   int fd, sock_server, sock_client;
   struct sockaddr_in addr;
   int i, j, n, counter, position, size, yes = 1;
+  uint8_t buffer[8192];
   uint32_t command, code, data, period, pulses, shdelay, shtime;
   uint32_t *coordinates;
-  uint64_t buffer[1024], tmp;
   volatile void *cfg, *sts;
-  volatile uint8_t *rst;
+  volatile uint8_t *cfg8;
   volatile uint16_t *rd_cntr, *wr_cntr;
-  volatile uint32_t *dac;
-  volatile uint64_t *adc;
+  volatile uint32_t *cfg32, *fifo;
 
   size = 0;
   coordinates = malloc(4194304);
@@ -34,61 +33,61 @@ int main()
   }
 
   cfg = mmap(NULL, sysconf(_SC_PAGESIZE), PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0x40000000);
-  sts = mmap(NULL, sysconf(_SC_PAGESIZE), PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0x40001000);
-  dac = mmap(NULL, 16*sysconf(_SC_PAGESIZE), PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0x40010000);
-  adc = mmap(NULL, 32*sysconf(_SC_PAGESIZE), PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0x40020000);
+  sts = mmap(NULL, sysconf(_SC_PAGESIZE), PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0x41000000);
+  fifo = mmap(NULL, 32*sysconf(_SC_PAGESIZE), PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0x42000000);
 
   wr_cntr = (uint16_t *)(sts + 0);
   rd_cntr = (uint16_t *)(sts + 2);
 
-  rst = (uint8_t *)(cfg + 0);
+  cfg8 = (uint8_t *)(cfg + 0);
+  cfg32 = (uint32_t *)(cfg + 0);
 
   /* stop pulse generators */
-  *rst &= ~1;
+  cfg8[0] &= ~1;
 
   /* configure trigger edge (0 for negative, 1 for positive) */
-  *(uint8_t *)(cfg + 1) = 1;
+  cfg8[1] = 1;
 
   /* set trigger mask */
-  *(uint8_t *)(cfg + 2) = 1;
+  cfg8[2] = 1;
 
   /* set trigger level */
-  *(uint8_t *)(cfg + 3) = 1;
+  cfg8[3] = 1;
 
   /* set number of ADC samples per pulse */
-  *(uint8_t *)(cfg + 4) = 32 - 1;
+  cfg8[4] = 32 - 1;
 
   /* set number of pulses per pixel */
   pulses = 2;
-  *(uint8_t *)(cfg + 5) = pulses - 1;
+  cfg8[5] = pulses - 1;
 
   period = 25000;
 
   /* trigger pulse generator */
   /* number of clocks before 1 */
-  *(uint32_t *)(cfg + 8) = 0;
+  cfg32[2] = 0;
   /* number of clocks before 0 */
-  *(uint32_t *)(cfg + 12) = 200;
+  cfg32[3] = 200;
   /* total number of clocks (200e-6 * 125e6) */
-  *(uint32_t *)(cfg + 16) = period - 1;
+  cfg32[4] = period - 1;
 
   /* S&H pulse generator */
   /* number of clocks before 1 */
   shdelay = 400;
-  *(uint32_t *)(cfg + 20) = shdelay;
+  cfg32[5] = shdelay;
   /* number of clocks before 0 */
   shtime = 30;
-  *(uint32_t *)(cfg + 24) = shdelay + shtime;
+  cfg32[6] = shdelay + shtime;
   /* total number of clocks (200e-6 * 143e6) */
-  *(uint32_t *)(cfg + 28) = period - 1;
+  cfg32[7] = period - 1;
 
   /* ADC pulse generator */
   /* number of clocks before 1 */
-  *(uint32_t *)(cfg + 32) = 2000;
+  cfg32[8] = 2000;
   /* number of clocks before 0 (+1) */
-  *(uint32_t *)(cfg + 36) = 2001;
+  cfg32[9] = 2001;
   /* total number of clocks */
-  *(uint32_t *)(cfg + 40) = 2001;
+  cfg32[10] = 2001;
 
   if((sock_server = socket(AF_INET, SOCK_STREAM, 0)) < 0)
   {
@@ -130,48 +129,48 @@ int main()
         case 0:
           /* set trigger period */
           period = data;
-          *(uint32_t *)(cfg + 16) = period - 1;
-          *(uint32_t *)(cfg + 28) = period - 1;
+          cfg32[4] = period - 1;
+          cfg32[7] = period - 1;
           break;
         case 1:
           /* set trigger duration */
-          *(uint32_t *)(cfg + 12) = data;
+          cfg32[3] = data;
           break;
         case 2:
           /* set trigger polarity */
-          if(data == 0) *rst &= ~4;
-          else if(data == 1) *rst |= 4;
+          if(data == 0) cfg8[0] &= ~4;
+          else if(data == 1) cfg8[0] |= 4;
           break;
         case 3:
           /* set S&H delay */
           shdelay = data;
-          *(uint32_t *)(cfg + 20) = shdelay;
-          *(uint32_t *)(cfg + 24) = shdelay + shtime;
+          cfg32[5] = shdelay;
+          cfg32[6] = shdelay + shtime;
           break;
         case 4:
           /* set S&H duration */
           shtime = data;
-          *(uint32_t *)(cfg + 24) = shdelay + shtime;
+          cfg32[6] = shdelay + shtime;
           break;
         case 5:
           /* set S&H polarity */
-          if(data == 0) *rst &= ~8;
-          else if(data == 1) *rst |= 8;
+          if(data == 0) cfg8[0] &= ~8;
+          else if(data == 1) cfg8[0] |= 8;
           break;
         case 6:
           /* set acquisition delay */
-          *(uint32_t *)(cfg + 32) = data;
-          *(uint32_t *)(cfg + 36) = data + 1;
-          *(uint32_t *)(cfg + 40) = data + 1;
+          cfg32[8] = data;
+          cfg32[9] = data + 1;
+          cfg32[10] = data + 1;
           break;
         case 7:
           /* set number of ADC samples per pulse */
-          *(uint8_t *)(cfg + 4) = data - 1;
+          cfg8[4] = data - 1;
           break;
         case 8:
           /* set number of pulses per pixel */
           pulses = data;
-          *(uint8_t *)(cfg + 5) = pulses - 1;
+          cfg8[5] = pulses - 1;
           break;
         case 9:
           /* clear coordinates */
@@ -185,7 +184,7 @@ int main()
           break;
         case 11:
           /* start pulse generators */
-          *rst |= 1;
+          cfg8[0] |= 1;
           break;
         case 12:
           /* start scanning */
@@ -194,10 +193,10 @@ int main()
           n = 1024;
 
           /* stop pulse generators */
-          *rst &= ~1;
+          cfg8[0] &= ~1;
 
           /* reset DAC and ADC FIFO */
-          *rst |= 2; *rst &= ~2;
+          cfg8[0] &= ~2; cfg8[0] |= 2;
 
           while(counter < size)
           {
@@ -206,7 +205,7 @@ int main()
             if(*rd_cntr < n * 2) usleep(500);
             if(*rd_cntr >= n * 2 && counter < size)
             {
-              for(j = 0; j < n; ++j) buffer[j] = *adc;
+              memcpy(buffer, fifo, n * 8);
               if(send(sock_client, buffer, n * 8, MSG_NOSIGNAL) < 0) break;
               counter += n;
             }
@@ -214,12 +213,12 @@ int main()
             /* write OUT1 and OUT2 samples to DAC FIFO */
             while(*wr_cntr < 16384 - pulses && position < size)
             {
-              for(j = 0; j < pulses; ++j) *dac = coordinates[position];
+              for(j = 0; j < pulses; ++j) *fifo = coordinates[position];
               ++position;
             }
 
             /* start pulse generators */
-            *rst |= 1;
+            cfg8[0] |= 1;
           }
 
           break;
@@ -227,7 +226,7 @@ int main()
     }
 
     /* stop pulse generators */
-    *rst &= ~1;
+    cfg8[0] &= ~1;
 
     close(sock_client);
   }
