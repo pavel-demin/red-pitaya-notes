e02b59f5b52c231dd7e159e458a28be5396462fc
Pavel Demin <pavel-demin@outlook.com>
2023-04-26 16:57:30 +0200

    remove kitgen

--- kitgen/files/tk8.6-pkgIndex.tcl
+++
@@ -1,8 +0,0 @@
-package ifneeded Tk $::tcl_patchLevel \
-  [string map [list @@ [file join $dir .. libtk$::tcl_version[info sharedlibext]]] {
-    if {[lsearch -exact [info loaded] {{} Tk}] >= 0} {
-      load "" Tk
-    } else {
-      load @@ Tk
-    }
-  }]
