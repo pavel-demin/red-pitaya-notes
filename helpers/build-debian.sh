1c4dc1b62e2a8dd7009f1c0104179290cad632b9
Pavel Demin <pavel-demin@outlook.com>
2023-06-25 19:03:59 +0200

    update build-debian.sh

--- helpers/build-debian.sh
+++ helpers/build-debian.sh
@@ -1,16 +1,8 @@
-DATE=`date +%Y%m%d`
-
 source /opt/Xilinx/Vitis/2023.1/settings64.sh
+
+DATE=`date +%Y%m%d`
 
 make NAME=led_blinker all
 
-sudo sh scripts/image.sh scripts/debian.sh red-pitaya-debian-11.3-armhf-$DATE.img 1024
-zip red-pitaya-debian-11.3-armhf-$DATE.zip red-pitaya-debian-11.3-armhf-$DATE.img
-
-sudo sh scripts/image.sh scripts/debian-ecosystem.sh red-pitaya-ecosystem-0.95-debian-11.3-armhf-$DATE.img 1024
-zip red-pitaya-ecosystem-0.95-debian-11.3-armhf-$DATE.zip red-pitaya-ecosystem-0.95-debian-11.3-armhf-$DATE.img
-
-make NAME=sdr_transceiver_emb all
-
-sudo sh scripts/image.sh scripts/debian-gnuradio.sh red-pitaya-gnuradio-debian-11.3-armhf-$DATE.img 2048
-zip red-pitaya-gnuradio-debian-11.3-armhf-$DATE.zip red-pitaya-gnuradio-debian-11.3-armhf-$DATE.img
+sudo sh scripts/image.sh scripts/debian.sh red-pitaya-debian-11.7-armhf-$DATE.img 1024
+zip red-pitaya-debian-11.7-armhf-$DATE.zip red-pitaya-debian-11.7-armhf-$DATE.img
