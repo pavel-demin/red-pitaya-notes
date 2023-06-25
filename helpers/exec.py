0da10634b4eb824be8382b383f7752b592190b3d
Pavel Demin <pavel-demin@outlook.com>
2023-06-25 12:15:51 +0200

    improve formatting in exec.py and pyside2-uic.py

--- helpers/exec.py
+++ helpers/exec.py
@@ -1,4 +1,5 @@
 import os
 import sys
-file_name = os.path.splitext(sys.argv[0])[0] + '.py'
+
+file_name = os.path.splitext(sys.argv[0])[0] + ".py"
 exec(open(file_name).read())
--- helpers/pyside2-uic.py
+++ helpers/pyside2-uic.py
@@ -1,4 +1,5 @@
 import sys
 import subprocess as sp
-cmd = ['uic', '-g', 'python'] + sys.argv[1:]
-sys.exit(sp.run(cmd, stderr = sp.PIPE, shell = True).returncode)
+
+cmd = ["uic", "-g", "python"] + sys.argv[1:]
+sys.exit(sp.run(cmd, stderr=sp.PIPE, shell=True).returncode)
