dffd8027dd3e9a95179662ae0663dd4f8e886a66
Pavel Demin <pavel-demin@outlook.com>
2024-02-18 16:49:30 +0100

    set implementation strategy to Performance_ExploreWithRemap

--- scripts/bitstream.tcl
+++ scripts/bitstream.tcl
@@ -4,7 +4,7 @@ set project_name [lindex $argv 0]
 open_project tmp/$project_name.xpr
 
 if {[get_property PROGRESS [get_runs impl_1]] != "100%"} {
-  launch_runs impl_1 -to_step route_design
+  launch_runs impl_1
   wait_on_run impl_1
 }
 
