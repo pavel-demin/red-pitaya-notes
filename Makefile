67242df98a51add20f7fe59264b9220db60df963
Pavel Demin <pavel-demin@outlook.com>
2024-05-30 13:10:23 +0200

    switch to Alpine 3.20

--- Makefile
+++ Makefile
@@ -19,7 +19,7 @@ VIVADO = vivado -nolog -nojournal -mode batch
 XSCT = xsct
 RM = rm -rf
 
-INITRAMFS_TAG = 3.18
+INITRAMFS_TAG = 3.20
 LINUX_TAG = 6.1
 DTREE_TAG = xilinx_v2023.1
 
--- scripts/alpine-project.sh
+++ scripts/alpine-project.sh
@@ -1,9 +1,9 @@
-alpine_url=http://dl-cdn.alpinelinux.org/alpine/v3.18
+alpine_url=http://dl-cdn.alpinelinux.org/alpine/v3.20
 
-tools_tar=apk-tools-static-2.14.0-r2.apk
+tools_tar=apk-tools-static-2.14.4-r0.apk
 tools_url=$alpine_url/main/armv7/$tools_tar
 
-firmware_tar=linux-firmware-other-20230515-r6.apk
+firmware_tar=linux-firmware-other-20240513-r0.apk
 firmware_url=$alpine_url/main/armv7/$firmware_tar
 
 linux_dir=tmp/linux-6.1
@@ -19,7 +19,7 @@ test -f $tools_tar || curl -L $tools_url -o $tools_tar
 
 test -f $firmware_tar || curl -L $firmware_url -o $firmware_tar
 
-for tar in linux-firmware-ath9k_htc-20230515-r6.apk linux-firmware-brcm-20230515-r6.apk linux-firmware-cypress-20230515-r6.apk linux-firmware-rtlwifi-20230515-r6.apk
+for tar in linux-firmware-ath9k_htc-20240513-r0.apk linux-firmware-brcm-20240513-r0.apk linux-firmware-cypress-20240513-r0.apk linux-firmware-rtlwifi-20240513-r0.apk
 do
   url=$alpine_url/main/armv7/$tar
   test -f $tar || curl -L $url -o $tar
@@ -38,7 +38,7 @@ depmod -a -b alpine-modloop $linux_ver
 
 tar -zxf $firmware_tar --directory=alpine-modloop/lib/modules --warning=no-unknown-keyword --strip-components=1 --wildcards lib/firmware/ar* lib/firmware/rt*
 
-for tar in linux-firmware-ath9k_htc-20230515-r6.apk linux-firmware-brcm-20230515-r6.apk linux-firmware-cypress-20230515-r6.apk linux-firmware-rtlwifi-20230515-r6.apk
+for tar in linux-firmware-ath9k_htc-20240513-r0.apk linux-firmware-brcm-20240513-r0.apk linux-firmware-cypress-20240513-r0.apk linux-firmware-rtlwifi-20240513-r0.apk
 do
   tar -zxf $tar --directory=alpine-modloop/lib/modules --warning=no-unknown-keyword --strip-components=1
 done
@@ -156,6 +156,6 @@ hostname -F /etc/hostname
 
 rm -rf $root_dir alpine-apk
 
-zip -r red-pitaya-alpine-3.18-armv7-`date +%Y%m%d`-$project.zip apps boot.bin cache modloop red-pitaya.apkovl.tar.gz start.sh wifi
+zip -r red-pitaya-alpine-3.20-armv7-`date +%Y%m%d`-$project.zip apps boot.bin cache modloop red-pitaya.apkovl.tar.gz start.sh wifi
 
 rm -rf apps cache modloop red-pitaya.apkovl.tar.gz start.sh wifi
