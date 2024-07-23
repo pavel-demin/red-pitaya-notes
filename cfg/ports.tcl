c59de58888e88c9b8375384fcd4d3dc79439060c
JerryTech <1194781887@qq.com>
2024-07-24 00:24:56 +0800

    Port redpitaya sdr_receiver to web888

--- cfg/ports.tcl
+++ cfg/ports.tcl
@@ -2,42 +2,23 @@
 ### ADC
 
 create_bd_port -dir I -from 15 -to 0 adc_dat_a_i
-create_bd_port -dir I -from 15 -to 0 adc_dat_b_i
 
 create_bd_port -dir I adc_clk_p_i
 create_bd_port -dir I adc_clk_n_i
 
-create_bd_port -dir O adc_enc_p_o
-create_bd_port -dir O adc_enc_n_o
+create_bd_port -dir I adc_ofl_i
 
-create_bd_port -dir O adc_csn_o
+create_bd_port -dir O adc_dith_o
+create_bd_port -dir O adc_pga_o
 
-### DAC
+### Misc
 
-create_bd_port -dir O -from 13 -to 0 dac_dat_o
+create_bd_port -dir I pps_i
 
-create_bd_port -dir O dac_clk_o
-create_bd_port -dir O dac_rst_o
-create_bd_port -dir O dac_sel_o
-create_bd_port -dir O dac_wrt_o
-
-### PWM
-
-create_bd_port -dir O -from 3 -to 0 dac_pwm_o
-
-### XADC
-
-create_bd_intf_port -mode Slave -vlnv xilinx.com:interface:diff_analog_io_rtl:1.0 Vp_Vn
-create_bd_intf_port -mode Slave -vlnv xilinx.com:interface:diff_analog_io_rtl:1.0 Vaux0
-create_bd_intf_port -mode Slave -vlnv xilinx.com:interface:diff_analog_io_rtl:1.0 Vaux1
-create_bd_intf_port -mode Slave -vlnv xilinx.com:interface:diff_analog_io_rtl:1.0 Vaux9
-create_bd_intf_port -mode Slave -vlnv xilinx.com:interface:diff_analog_io_rtl:1.0 Vaux8
-
-### Expansion connector
-
-create_bd_port -dir IO -from 7 -to 0 exp_p_tri_io
-create_bd_port -dir IO -from 7 -to 0 exp_n_tri_io
+### Expand
+create_bd_port -dir IO -from 2 -to 0 exp_p_tri_io
+create_bd_port -dir IO -from 2 -to 0 exp_n_tri_io
 
 ### LED
 
-create_bd_port -dir O -from 7 -to 0 led_o
+create_bd_port -dir O led_o
