cc8fa24e21964e5a38ec28bfdc19ccc25acf7861
Pavel Demin <pavel-demin@outlook.com>
2024-12-06 16:45:13 +0100

    remove unnecessary commands from devicetree.tcl

--- scripts/devicetree.tcl
+++ scripts/devicetree.tcl
@@ -5,8 +5,6 @@ set proc_name [lindex $argv 1]
 
 set repo_path [lindex $argv 2]
 
-set boot_args {console=ttyPS0,115200 earlyprintk}
-
 set hard_path tmp/$project_name.hard
 set tree_path tmp/$project_name.tree
 
@@ -17,9 +15,6 @@ hsi set_repo_path $repo_path
 
 hsi open_hw_design $hard_path/$project_name.xsa
 hsi create_sw_design -proc $proc_name -os device_tree devicetree
-
-hsi set_property CONFIG.kernel_version {2023.1} [hsi get_os]
-hsi set_property CONFIG.bootargs $boot_args [hsi get_os]
 
 hsi generate_target -dir $tree_path
 
