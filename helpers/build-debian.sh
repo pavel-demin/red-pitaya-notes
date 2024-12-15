0777a9e5203c67b97ab6e7731f2ad581307b9a36
Pavel Demin <pavel-demin@outlook.com>
2024-12-15 23:14:42 +0100

    replace 11.7 with 12.8 in build-debian.sh

--- helpers/build-debian.sh
+++ helpers/build-debian.sh
@@ -4,5 +4,5 @@ DATE=`date +%Y%m%d`
 
 make NAME=led_blinker all
 
-sudo sh scripts/image.sh scripts/debian.sh red-pitaya-debian-11.7-armhf-$DATE.img 1024
-zip red-pitaya-debian-11.7-armhf-$DATE.zip red-pitaya-debian-11.7-armhf-$DATE.img
+sudo sh scripts/image.sh scripts/debian.sh red-pitaya-debian-12.8-armhf-$DATE.img 1024
+zip red-pitaya-debian-12.8-armhf-$DATE.zip red-pitaya-debian-12.8-armhf-$DATE.img
