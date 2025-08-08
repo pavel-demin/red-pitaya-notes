8315297ae280692a677b24600c02967eebbcba3c
Pavel Demin <pavel-demin@outlook.com>
2025-08-08 21:59:49 +0200

    replace PySide2 with PySide6 in freeze.py

--- helpers/freeze.py
+++ helpers/freeze.py
@@ -1,41 +1,44 @@
 from py2exe import freeze
 
 includes = [
-    "PySide2.QtCore",
-    "PySide2.QtGui",
-    "PySide2.QtNetwork",
-    "PySide2.QtMultimedia",
-    "PySide2.QtPrintSupport",
-    "PySide2.QtUiTools",
-    "PySide2.QtWebSockets",
-    "PySide2.QtWidgets",
-    "PySide2.QtXml",
+    "PySide6.QtCore",
+    "PySide6.QtGui",
+    "PySide6.QtNetwork",
+    "PySide6.QtMultimedia",
+    "PySide6.QtPrintSupport",
+    "PySide6.QtUiTools",
+    "PySide6.QtWebSockets",
+    "PySide6.QtWidgets",
+    "PySide6.QtXml",
     "numpy",
-    "matplotlib.backends.backend_qt5agg",
+    "matplotlib.backends.backend_qtagg",
 ]
 
 freeze(
-    windows=[{"script": "exec.py"}, {"script": "pyside2-uic.py"}],
+    windows=[{"script": "exec.py"}],
     data_files=[
         (
             "",
             [
-                "c:\\Python38\\Lib\\site-packages\\PySide2\\Qt5Designer.dll",
-                "c:\\Python38\\Lib\\site-packages\\PySide2\\Qt5DesignerComponents.dll",
-                "c:\\Python38\\Lib\\site-packages\\PySide2\\designer.exe",
-                "c:\\Python38\\Lib\\site-packages\\PySide2\\uic.exe",
+                "c:\\Python313\\Lib\\site-packages\\shiboken6\\msvcp140.dll",
+                "c:\\Python313\\Lib\\site-packages\\PySide6\\Qt6OpenGL.dll",
+                "c:\\Python313\\Lib\\site-packages\\PySide6\\Qt6Designer.dll",
+                "c:\\Python313\\Lib\\site-packages\\PySide6\\Qt6DesignerComponents.dll",
+                "c:\\Python313\\Lib\\site-packages\\PySide6\\designer.exe",
+                "c:\\Python313\\Lib\\site-packages\\PySide6\\uic.exe",
+                "c:\\Python313\\Scripts\\pyside6-uic.exe",
             ],
         ),
         (
             "platforms",
             [
-                "c:\\Python38\\Lib\\site-packages\\PySide2\\plugins\\platforms\\qwindows.dll"
+                "c:\\Python313\\Lib\\site-packages\\PySide6\\plugins\\platforms\\qwindows.dll"
             ],
         ),
         (
             "styles",
             [
-                "c:\\Python38\\Lib\\site-packages\\PySide2\\plugins\\styles\\qwindowsvistastyle.dll"
+                "c:\\Python313\\Lib\\site-packages\\PySide6\\plugins\\styles\\qmodernwindowsstyle.dll"
             ],
         ),
     ],
