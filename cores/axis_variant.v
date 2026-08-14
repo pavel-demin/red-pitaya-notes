7f95c4f3e91cce368b27dc75daf0a0c6d14f0a45
Pavel Demin <pavel-demin@outlook.com>
2026-08-14 21:28:49 +0200

    use output_buffer for the valid/ready handshake in axis_variant.v

--- cores/axis_variant.v
+++ cores/axis_variant.v
@@ -21,27 +21,16 @@ module axis_variant #
   output wire                        m_axis_tvalid
 );
 
-  reg [AXIS_TDATA_WIDTH-1:0] int_tdata_reg;
-  reg int_tvalid_reg;
-  wire [AXIS_TDATA_WIDTH-1:0] int_tdata_wire;
+  wire [AXIS_TDATA_WIDTH-1:0] int_data_wire;
 
-  assign int_tdata_wire = cfg_flag ? cfg_data1 : cfg_data0;
+  assign int_data_wire = cfg_flag ? cfg_data1 : cfg_data0;
 
-  always @(posedge aclk)
-  begin
-    if(~aresetn)
-    begin
-      int_tdata_reg <= {(AXIS_TDATA_WIDTH){1'b0}};
-      int_tvalid_reg <= 1'b0;
-    end
-    else if(~int_tvalid_reg | m_axis_tready)
-    begin
-      int_tdata_reg <= int_tdata_wire;
-      int_tvalid_reg <= int_tdata_reg != int_tdata_wire;
-    end
-  end
-
-  assign m_axis_tdata = int_tdata_reg;
-  assign m_axis_tvalid = int_tvalid_reg;
+  output_buffer #(
+    .DATA_WIDTH(AXIS_TDATA_WIDTH)
+  ) buf_0 (
+    .aclk(aclk), .aresetn(aresetn),
+    .in_data(int_data_wire), .in_valid(m_axis_tdata != int_data_wire), .in_ready(),
+    .out_data(m_axis_tdata), .out_valid(m_axis_tvalid), .out_ready(m_axis_tready)
+  );
 
 endmodule
