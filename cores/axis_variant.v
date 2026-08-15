3a768ccedab92d9318e2d094f66e2bdf81491d1b
Pavel Demin <pavel-demin@outlook.com>
2026-08-15 23:30:12 +0200

    fix initial transfer after reset in axis_variant.v

--- cores/axis_variant.v
+++ cores/axis_variant.v
@@ -21,16 +21,24 @@ module axis_variant #
   output wire                        m_axis_tvalid
 );
 
+  reg  int_init_reg;
+
   wire [AXIS_TDATA_WIDTH-1:0] int_data_wire;
 
   assign int_data_wire = cfg_flag ? cfg_data1 : cfg_data0;
+
+  always @(posedge aclk)
+  begin
+    int_init_reg <= ~aresetn;
+  end
 
   output_buffer #(
     .DATA_WIDTH(AXIS_TDATA_WIDTH)
   ) buf_0 (
     .aclk(aclk), .aresetn(aresetn),
-    .in_data(int_data_wire), .in_valid(m_axis_tdata != int_data_wire), .in_ready(),
+    .in_data(int_data_wire), .in_valid(int_init_reg | (m_axis_tdata != int_data_wire)), .in_ready(),
     .out_data(m_axis_tdata), .out_valid(m_axis_tvalid), .out_ready(m_axis_tready)
   );
+
 
 endmodule
