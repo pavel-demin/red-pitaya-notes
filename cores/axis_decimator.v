a110bb9a8996c4b4e12bbb4676fcffd1e4c73cd3
Pavel Demin <pavel-demin@outlook.com>
2026-08-16 11:49:42 +0200

    use output_buffer for valid/ready handshake in axis_decimator.v

--- cores/axis_decimator.v
+++ cores/axis_decimator.v
@@ -24,67 +24,30 @@ module axis_decimator #
   output wire                        m_axis_tvalid
 );
 
-  reg [AXIS_TDATA_WIDTH-1:0] int_tdata_reg, int_tdata_next;
-  reg [CNTR_WIDTH-1:0] int_cntr_reg, int_cntr_next;
-  reg int_tvalid_reg, int_tvalid_next;
-  reg int_tready_reg, int_tready_next;
+  reg [CNTR_WIDTH-1:0] int_cntr_reg;
 
-  wire int_comp_wire, int_tvalid_wire;
+  wire int_last_wire;
+
+  assign int_last_wire = int_cntr_reg >= cfg_data;
 
   always @(posedge aclk)
   begin
     if(~aresetn)
     begin
-      int_tdata_reg <= {(AXIS_TDATA_WIDTH){1'b0}};
-      int_tvalid_reg <= 1'b0;
-      int_tready_reg <= 1'b0;
       int_cntr_reg <= {(CNTR_WIDTH){1'b0}};
     end
-    else
+    else if(s_axis_tvalid & s_axis_tready)
     begin
-      int_tdata_reg <= int_tdata_next;
-      int_tvalid_reg <= int_tvalid_next;
-      int_tready_reg <= int_tready_next;
-      int_cntr_reg <= int_cntr_next;
+      int_cntr_reg <= int_last_wire ? {(CNTR_WIDTH){1'b0}} : int_cntr_reg + 1'b1;
     end
   end
 
-  assign int_comp_wire = int_cntr_reg < cfg_data;
-  assign int_tvalid_wire = s_axis_tready & s_axis_tvalid;
-
-  always @*
-  begin
-    int_tdata_next = int_tdata_reg;
-    int_tvalid_next = int_tvalid_reg;
-    int_tready_next = int_tready_reg;
-    int_cntr_next = int_cntr_reg;
-
-    if(~int_tready_reg & int_comp_wire)
-    begin
-      int_tready_next = 1'b1;
-    end
-
-    if(m_axis_tready & int_tvalid_reg)
-    begin
-      int_tvalid_next = 1'b0;
-    end
-
-    if(int_tvalid_wire & int_comp_wire)
-    begin
-      int_cntr_next = int_cntr_reg + 1'b1;
-    end
-
-    if(int_tvalid_wire & ~int_comp_wire)
-    begin
-      int_cntr_next = {(CNTR_WIDTH){1'b0}};
-      int_tdata_next = s_axis_tdata;
-      int_tvalid_next = 1'b1;
-    end
-
-  end
-
-  assign s_axis_tready = int_tready_reg & (~int_tvalid_reg | m_axis_tready);
-  assign m_axis_tdata = int_tdata_reg;
-  assign m_axis_tvalid = int_tvalid_reg;
+  output_buffer #(
+    .DATA_WIDTH(AXIS_TDATA_WIDTH)
+  ) buf_0 (
+    .aclk(aclk), .aresetn(aresetn),
+    .in_data(s_axis_tdata), .in_valid(s_axis_tvalid & int_last_wire), .in_ready(s_axis_tready),
+    .out_data(m_axis_tdata), .out_valid(m_axis_tvalid), .out_ready(m_axis_tready)
+  );
 
 endmodule
