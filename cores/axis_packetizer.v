3b4e906973d7e39583cb7a49c2b12a5745bfd62e
Pavel Demin <pavel-demin@outlook.com>
2026-08-29 20:55:05 +0200

    add output buffer to axis_packetizer

--- cores/axis_packetizer.v
+++ cores/axis_packetizer.v
@@ -27,80 +27,56 @@ module axis_packetizer #
   output wire                        m_axis_tlast
 );
 
-  reg [CNTR_WIDTH-1:0] int_cntr_reg, int_cntr_next;
-  reg [CNTR_WIDTH-1:0] int_data_reg;
-  reg int_enbl_reg, int_enbl_next;
+  reg [CNTR_WIDTH-1:0] int_cntr_reg;
 
-  wire int_comp_wire, int_tvalid_wire, int_tlast_wire;
+  wire int_gate_wire, int_last_wire, int_valid_wire, int_ready_wire;
+
+  assign int_valid_wire = s_axis_tvalid & int_gate_wire;
+  assign int_last_wire = int_cntr_reg >= cfg_data;
+
+  generate
+    if(CONTINUOUS == "TRUE")
+    begin : CONTINUE
+      assign int_gate_wire = 1'b1;
+    end
+    else
+    begin : STOP
+      reg int_gate_reg;
+
+      always @(posedge aclk)
+      begin
+        if(~aresetn)
+        begin
+          int_gate_reg <= 1'b1;
+        end
+        else if(int_valid_wire & int_ready_wire & int_last_wire)
+        begin
+          int_gate_reg <= 1'b0;
+        end
+      end
+
+      assign int_gate_wire = int_gate_reg;
+    end
+  endgenerate
 
   always @(posedge aclk)
   begin
     if(~aresetn)
     begin
       int_cntr_reg <= {(CNTR_WIDTH){1'b0}};
-      int_data_reg <= {(CNTR_WIDTH){1'b0}};
-      int_enbl_reg <= 1'b0;
     end
-    else
+    else if(int_valid_wire & int_ready_wire)
     begin
-      int_cntr_reg <= int_cntr_next;
-      int_data_reg <= cfg_data;
-      int_enbl_reg <= int_enbl_next;
+      if(int_last_wire)
+      begin
+        int_cntr_reg <= {(CNTR_WIDTH){1'b0}};
+      end
+      else
+      begin
+        int_cntr_reg <= int_cntr_reg + 1'b1;
+      end
     end
   end
-
-  assign int_comp_wire = int_cntr_reg < int_data_reg;
-  assign int_tvalid_wire = int_enbl_reg & s_axis_tvalid;
-  assign int_tlast_wire = ~int_comp_wire;
-
-  generate
-    if(CONTINUOUS == "TRUE")
-    begin : CONTINUE
-      always @*
-      begin
-        int_cntr_next = int_cntr_reg;
-        int_enbl_next = int_enbl_reg;
-
-        if(~int_enbl_reg & int_comp_wire)
-        begin
-          int_enbl_next = 1'b1;
-        end
-
-        if(m_axis_tready & int_tvalid_wire & int_comp_wire)
-        begin
-          int_cntr_next = int_cntr_reg + 1'b1;
-        end
-
-        if(m_axis_tready & int_tvalid_wire & int_tlast_wire)
-        begin
-          int_cntr_next = {(CNTR_WIDTH){1'b0}};
-        end
-      end
-    end
-    else
-    begin : STOP
-      always @*
-      begin
-        int_cntr_next = int_cntr_reg;
-        int_enbl_next = int_enbl_reg;
-
-        if(~int_enbl_reg & int_comp_wire)
-        begin
-          int_enbl_next = 1'b1;
-        end
-
-        if(m_axis_tready & int_tvalid_wire & int_comp_wire)
-        begin
-          int_cntr_next = int_cntr_reg + 1'b1;
-        end
-
-        if(m_axis_tready & int_tvalid_wire & int_tlast_wire)
-        begin
-          int_enbl_next = 1'b0;
-        end
-      end
-    end
-  endgenerate
 
   generate
     if(ALWAYS_READY == "TRUE")
@@ -109,12 +85,16 @@ module axis_packetizer #
     end
     else
     begin : BLOCKING
-      assign s_axis_tready = int_enbl_reg & m_axis_tready;
+      assign s_axis_tready = int_ready_wire & int_gate_wire;
     end
   endgenerate
 
-  assign m_axis_tdata = s_axis_tdata;
-  assign m_axis_tvalid = int_tvalid_wire;
-  assign m_axis_tlast = int_enbl_reg & int_tlast_wire;
+  output_buffer #(
+    .DATA_WIDTH(AXIS_TDATA_WIDTH + 1)
+  ) buf_0 (
+    .aclk(aclk), .aresetn(aresetn),
+    .in_data({int_last_wire, s_axis_tdata}), .in_valid(int_valid_wire), .in_ready(int_ready_wire),
+    .out_data({m_axis_tlast, m_axis_tdata}), .out_valid(m_axis_tvalid), .out_ready(m_axis_tready)
+  );
 
 endmodule
