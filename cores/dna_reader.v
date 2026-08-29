aef4ed278be27a1670363f91e814c0fd2d3a9c18
Pavel Demin <pavel-demin@outlook.com>
2026-08-29 21:11:02 +0200

    simplify dna_reader

--- cores/dna_reader.v
+++ cores/dna_reader.v
@@ -12,15 +12,13 @@ module dna_reader
   localparam integer CNTR_WIDTH = 16;
   localparam integer DATA_WIDTH = 57;
 
-  reg int_enbl_reg, int_enbl_next;
-  reg int_read_reg, int_read_next;
-  reg int_shift_reg, int_shift_next;
-  reg [CNTR_WIDTH-1:0] int_cntr_reg, int_cntr_next;
-  reg [DATA_WIDTH-1:0] int_data_reg, int_data_next;
+  reg [CNTR_WIDTH-1:0] int_cntr_reg;
+  reg [DATA_WIDTH-1:0] int_data_reg;
+  reg int_read_reg, int_shift_reg;
 
-  wire int_comp_wire, int_data_wire;
+  wire int_data_wire, int_last_wire;
 
-  assign int_comp_wire = int_cntr_reg < 64*DATA_WIDTH;
+  assign int_last_wire = int_cntr_reg >= 64*DATA_WIDTH;
 
   DNA_PORT dna_0 (
     .DOUT(int_data_wire),
@@ -34,52 +32,33 @@ module dna_reader
   begin
     if(~aresetn)
     begin
-      int_enbl_reg <= 1'b0;
-      int_read_reg <= 1'b0;
-      int_shift_reg <= 1'b0;
       int_cntr_reg <= {(CNTR_WIDTH){1'b0}};
       int_data_reg <= {(DATA_WIDTH){1'b0}};
+      int_read_reg <= 1'b0;
+      int_shift_reg <= 1'b0;
     end
     else
     begin
-      int_enbl_reg <= int_enbl_next;
-      int_read_reg <= int_read_next;
-      int_shift_reg <= int_shift_next;
-      int_cntr_reg <= int_cntr_next;
-      int_data_reg <= int_data_next;
-    end
-  end
+      if(~|int_cntr_reg)
+      begin
+        int_read_reg <= 1'b1;
+      end
 
-  always @*
-  begin
-    int_enbl_next = int_enbl_reg;
-    int_read_next = int_read_reg;
-    int_shift_next = int_shift_reg;
-    int_cntr_next = int_cntr_reg;
-    int_data_next = int_data_reg;
+      if(&int_cntr_reg[5:0])
+      begin
+        int_read_reg <= 1'b0;
+        int_shift_reg <= 1'b1;
+        int_data_reg <= {int_data_reg[DATA_WIDTH-2:0], int_data_wire};
+      end
 
-    if(~int_enbl_reg & int_comp_wire)
-    begin
-      int_enbl_next = 1'b1;
-      int_read_next = 1'b1;
-    end
-
-    if(int_enbl_reg)
-    begin
-      int_cntr_next = int_cntr_reg + 1'b1;
-    end
-
-    if(&int_cntr_reg[5:0])
-    begin
-      int_read_next = 1'b0;
-      int_shift_next = 1'b1;
-      int_data_next = {int_data_reg[DATA_WIDTH-2:0], int_data_wire};
-    end
-
-    if(~int_comp_wire)
-    begin
-      int_enbl_next = 1'b0;
-      int_shift_next = 1'b0;
+      if(int_last_wire)
+      begin
+        int_shift_reg <= 1'b0;
+      end
+      else
+      begin
+        int_cntr_reg <= int_cntr_reg + 1'b1;
+      end
     end
   end
 
